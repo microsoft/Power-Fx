d11a1ff53f3b806a69f2586df19a471462d65ba5
Greg Lindhorst <gregli@microsoft.com>
2021-03-01 19:14:46 -0800

    more updates

--- docs/tools/grammar.ps1
+++ docs/tools/grammar.ps1
@@ -19,188 +19,177 @@ $prods = @{}
 $original = Get-Content -Path $mdpath
 $out = ""
 
-#try 
-#{
-	$grammar = Get-Content -Path $gpath
-	$grammar = $grammar + "`r`n"
+$grammar = Get-Content -Path $gpath
+$grammar = $grammar + "`r`n"
 
-	$names = @{}
-	$used = @{}
+$names = @{}
+$used = @{}
 
-	# =========================================================================================
-	# first pass, register names
-	#
-	ForEach( $line in $($grammar -split "`r`n"))
+# =========================================================================================
+# first pass, register names
+#
+ForEach( $line in $($grammar -split "`r`n"))
+{
+	if( $line -match '^(\w+)\s*:' )
 	{
-		if( $line -match '^(\w+)\s*:' )
+		if( $names[$matches[1]] )
 		{
-			if( $names[$matches[1]] )
-			{
-				write-host "grammar: duplicate name:" $matches[1] -ForegroundColor Red
-			}
-			$names[$matches[1]] = $true
+			write-host "grammar: duplicate name:" $matches[1] -ForegroundColor Red
 		}
+		$names[$matches[1]] = $true
 	}
+}
 
-	# =========================================================================================
-	# second pass, read productions
-	#
-	$md = ""
-	ForEach ($line in $($grammar -split "`r`n"))
+# =========================================================================================
+# second pass, read productions
+#
+$md = ""
+ForEach ($line in $($grammar -split "`r`n"))
+{
+	$line = if( $line -match '^(.*[^``])\/\/' ) { $matches[1] } else { $line }
+
+	if( $line -match '^(\w+)\s*:\s*(one\s+of)?' )
 	{
-		$line = if( $line -match '^(.*[^``])\/\/' ) { $matches[1] } else { $line }
-
-		if( $line -match '^(\w+)\s*:\s*(one\s+of)?' )
-		{
-			$name = $matches[1]
-			$after = if( $matches[2].length -gt 4 ) { " **one of**"} else { "" }
-			$md = '&emsp;&emsp;<a name="' + $name + '"></a>*' + $name + "* **:**" + $after + "<br>`r`n"
-		}
-		elseif( $line -match '^\s*$' )
-		{
-			if( $md -ne "" )
-			{	
-				$prods[ $name ] = $md
-			}
-			$md = ""
-		}
-		elseif( $line -match '^\s+>\s*(.*)$') 
-		{
-			$line = $matches[1]
-			$md = $md + '&emsp;&emsp;&emsp;&emsp;'
-
-			$spacing = ""
-			ForEach ($word in $($line -split "\s+"))
-			{
-				if( $names[$word] )
-				{
-					$md = $md + $spacing + '*[' + $word + '](#' + $word + ')*'
-					$used[$word]++
-				}
-				else 
-				{
-					$md = $md + $spacing + $word	
-				}
-				$spacing = " "
-			}			
-			$md = $md + "<br>`r`n"
-		}
-		elseif( $line -match '^\s+[^\s]' )
-		{
-			$md = $md + '&emsp;&emsp;&emsp;'
-			$spacing = "&emsp;"
-			ForEach ($word in $($line -split "\s+"))
-			{
-				if( $word -match '^\w' )
-				{
-					if( $word -eq "but" -or $word -eq "not" -or $word -eq "or" -or $word -eq "one" )
-					{
-						$md = $md + ' **' + $word + '**'
-						$spacing = " "
-					}
-					else 
-					{
-						if( $word -match '^(.*)\?$')
-						{
-							$word = $matches[1]
-							$opt = "<sub>opt</sub>"
-						}
-						else 
-						{
-							$opt = ""
-						}
-						if( -not $names[$word] )
-						{
-							write-host "grammar: name not found:" $word -ForegroundColor Red
-						}
-						$used[$word]++
-						$md = $md + $spacing + '*[' + $word + '](#' + $word + ')*' + $opt
-					}
-				}
-				elseif( $word -match '[^\s]')
-				{
-					$md = $md + '&emsp;' + $word 
-				}
-			}
-			$md = $md + "<br>`r`n"
-		}
-	}	
-
-	# =========================================================================================
-	# scan for productions not used
-	#
-	ForEach( $word in $names.Keys )
-	{
-		if( $used[$word] -lt 1 )
-		{
-			write-host "grammar: name not used:" $word -ForegroundColor Red
-		}
+		$name = $matches[1]
+		$after = if( $matches[2].length -gt 4 ) { " **one of**"} else { "" }
+		$md = '&emsp;&emsp;<a name="' + $name + '"></a>*' + $name + "* **:**" + $after + "<br>`r`n"
 	}
-
-	# =========================================================================================
-	# replace in md file
-	#
-	$eating = $false
-	$mdused = @{}
-	ForEach ($line in $($original -split "`r`n"))
+	elseif( $line -match '^\s*$' )
 	{
-		if( $line -imatch '^\#.*conventions' )
-		{
-			$conventions = $true
+		if( $md -ne "" )
+		{	
+			$prods[ $name ] = $md
 		}
-		elseif( $line -match '^\#')
-		{
-			$conventions = $false
-		}
+		$md = ""
+	}
+	elseif( $line -match '^\s+>\s*(.*)$') 
+	{
+		$line = $matches[1]
+		$md = $md + '&emsp;&emsp;&emsp;&emsp;'
 
-		if( $line -imatch '^&emsp;&emsp;.*\*(\w+)\*[\s\*]*:' -and -not $conventions )
+		$spacing = ""
+		ForEach ($word in $($line -split "\s+"))
 		{
-			$name = $matches[1]
-			if( $names[ $name ] )
+			if( $names[$word] )
 			{
-				$out += $prods[ $name ] + "`r`n"
-				$eating = $true
-				$mdused[ $name ]++
+				$md = $md + $spacing + '*[' + $word + '](#' + $word + ')*'
+				$used[$word]++
 			}
 			else 
 			{
-				write-host "md: name not found:" $name -ForegroundColor Red
+				$md = $md + $spacing + $word	
+			}
+			$spacing = " "
+		}			
+		$md = $md + "<br>`r`n"
+	}
+	elseif( $line -match '^\s+[^\s]' )
+	{
+		$md = $md + '&emsp;&emsp;&emsp;'
+		$spacing = "&emsp;"
+		ForEach ($word in $($line -split "\s+"))
+		{
+			if( $word -match '^\w' )
+			{
+				if( $word -eq "but" -or $word -eq "not" -or $word -eq "or" -or $word -eq "one" )
+				{
+					$md = $md + ' **' + $word + '**'
+					$spacing = " "
+				}
+				else 
+				{
+					if( $word -match '^(.*)\?$')
+					{
+						$word = $matches[1]
+						$opt = "<sub>opt</sub>"
+					}
+					else 
+					{
+						$opt = ""
+					}
+					if( -not $names[$word] )
+					{
+						write-host "grammar: name not found:" $word -ForegroundColor Red
+					}
+					$used[$word]++
+					$md = $md + $spacing + '*[' + $word + '](#' + $word + ')*' + $opt
+				}
+			}
+			elseif( $word -match '[^\s]')
+			{
+				$md = $md + '&emsp;' + $word 
 			}
 		}
-		elseif( $eating -and $line -match '^[^&]' )
-		{
-			$eating = $false
-			$out += "$line`r`n"			
-		}
-		elseif( -not $eating )
-		{
-			$out += "$line`r`n"
-		}
+		$md = $md + "<br>`r`n"
+	}
+}	
+
+# =========================================================================================
+# scan for productions not used
+#
+ForEach( $word in $names.Keys )
+{
+	if( $used[$word] -lt 1 )
+	{
+		write-host "grammar: name not used:" $word -ForegroundColor Red
+	}
+}
+
+# =========================================================================================
+# replace in md file
+#
+$eating = $false
+$mdused = @{}
+ForEach ($line in $($original -split "`r`n"))
+{
+	if( $line -imatch '^\#.*conventions' )
+	{
+		$conventions = $true
+	}
+	elseif( $line -match '^\#')
+	{
+		$conventions = $false
 	}
 
-	# =========================================================================================
-	# scan md used
-	#
-	ForEach( $word in $names.Keys )
+	if( $line -imatch '^&emsp;&emsp;.*\*(\w+)\*[\s\*]*:' -and -not $conventions )
 	{
-		if( $mdused[$word] -gt 1 )
+		$name = $matches[1]
+		if( $names[ $name ] )
 		{
-			write-host "md: name used more than once:" $word -ForegroundColor Red
+			$out += $prods[ $name ] + "`r`n"
+			$eating = $true
+			$mdused[ $name ]++
 		}
-		elseif( $mdused[$word] -lt 1 )
+		else 
 		{
-			write-host "md: name not used:" $word -ForegroundColor Red
+			write-host "md: name not found:" $name -ForegroundColor Red
 		}
 	}
-#} 
-#catch
-#{
-#	write-host "Caught an exception:" -ForegroundColor Red
-	#write-host "  Type: $($_.Exception.GetType().FullName)" -ForegroundColor Red
-	#write-host "  Message: $($_.Exception.Message)" -ForegroundColor Red
-	#write-host "  StackTrace: $($_.Exception.StackTrace)" -ForegroundColor Red
-	#break;
-#}
+	elseif( $eating -and $line -match '^[^&]' )
+	{
+		$eating = $false
+		$out += "$line`r`n"			
+	}
+	elseif( -not $eating )
+	{
+		$out += "$line`r`n"
+	}
+}
+
+# =========================================================================================
+# scan md used
+#
+ForEach( $word in $names.Keys )
+{
+	if( $mdused[$word] -gt 1 )
+	{
+		write-host "md: name used more than once:" $word -ForegroundColor Red
+	}
+	elseif( $mdused[$word] -lt 1 )
+	{
+		write-host "md: name not used:" $word -ForegroundColor Red
+	}
+}
 
 if( $backup )
 {
