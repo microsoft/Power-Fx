86f1a4684afab665e0c6910e2c99c274fbbd9e71
Mike Stall <mikestall@hotmail.com>
2021-12-06 09:28:41 -0800

    Add feature flag concept. (#78)
    Add feature flag concept.

---
+++ src/libraries/Microsoft.PowerFx.Core/FeatureFlags.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.PowerFx
+{
+    /// <summary>
+    /// Hosts can enable these flags to try out early features. 
+    /// Any flags will eventually default to true and then get removed once the feature is finalized. 
+    /// </summary>
+    public static class FeatureFlags
+    {
+        /// <summary>
+        /// Enable String Interpolation feature. 
+        /// Added 12/3/2021
+        /// </summary>
+        public static bool StringInterpolation = false;
+    }
+}
