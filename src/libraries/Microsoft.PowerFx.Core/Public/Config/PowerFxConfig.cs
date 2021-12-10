f6c7282e5af604bb30dba7afbf38665a81304a70
bryancroteau-MSFT <84751645+bryancroteau-MSFT@users.noreply.github.com>
2021-12-10 13:30:53 -0800

    Example implementation of configurable Enums (#82)
    * Example implementation of configurable Enums
    
    * Mark EnumStore methods as internal
    
    * Move Config to the Core namespace
    
    * Fix failing public surface test
    
    * Add visibility annotations to RecalcEngine config
    
    * Add unit tests for an empty enum provider
    
    * Make EnumStore internal again

---
+++ src/libraries/Microsoft.PowerFx.Core/Public/Config/PowerFxConfig.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Globalization;
+using Microsoft.PowerFx.Core.Types.Enums;
+
+namespace Microsoft.PowerFx.Core
+{
+    /// <summary>
+    /// A container that allows for compiler customization.
+    /// </summary>
+    public sealed class PowerFxConfig
+    {
+        private readonly EnumStore _enumStore;
+        private readonly CultureInfo _cultureInfo;
+
+        internal EnumStore EnumStore => _enumStore;
+        public CultureInfo CultureInfo => _cultureInfo;
+
+        public PowerFxConfig()
+        {
+            _enumStore = new EnumStore();
+            _cultureInfo = CultureInfo.CurrentCulture;
+        }
+
+        public PowerFxConfig(CultureInfo cultureInfo)
+        {
+            _enumStore = new EnumStore();
+            _cultureInfo = cultureInfo;
+        }
+
+        internal PowerFxConfig(EnumStore enumStore, CultureInfo cultureInfo)
+        {
+            _enumStore = enumStore;
+            _cultureInfo = cultureInfo;
+        }
+    }
+}
