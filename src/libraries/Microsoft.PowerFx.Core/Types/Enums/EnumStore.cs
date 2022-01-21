93e2dd95b838a946005c316cfbb45bb956c9ff52
Carlos Figueira <carlosff@microsoft.com>
2022-01-20 16:36:59 -0800

    Revert "Add new error kind enum value (canceled)" (#110)
    * Revert "Add new error kind enum value (canceled) (#108)"
    
    This reverts commit ab65b7a4062a8e984628061a9ff63ea3b46b32c5.
    
    * Added missing enum value

--- src/libraries/Microsoft.PowerFx.Core/Types/Enums/EnumStore.cs
+++ src/libraries/Microsoft.PowerFx.Core/Types/Enums/EnumStore.cs
@@ -198,10 +198,8 @@ namespace Microsoft.PowerFx.Core.Types.Enums
                 // TASK 4620228: Connect to Data: Need Final design on Error enum returned by Errors function.
                 {
                     EnumConstants.ErrorKindEnumString,
-                    "%n[None:0, Sync:1, MissingRequired:2, CreatePermission:3, EditPermission:4, DeletePermission:5, Conflict:6, NotFound:7, ConstraintViolated:8, " +
-                    "GeneratedValue:9, ReadOnlyValue:10, Validation: 11, Unknown: 12, Div0: 13, BadLanguageCode: 14, BadRegex: 15, InvalidFunctionUsage: 16, " +
-                    "FileNotFound: 17, AnalysisError: 18, ReadPermission: 19, NotSupported: 20, InsufficientMemory: 21, QuotaExceeded: 22, Network: 23, Numeric: 24, " +
-                    "InvalidArgument: 25, Canceled: 26]"
+                    "%n[None:0, Sync:1, MissingRequired:2, CreatePermission:3, EditPermission:4, DeletePermission:5, Conflict:6, NotFound:7, ConstraintViolated:8, GeneratedValue:9, ReadOnlyValue:10, Validation: 11, Unknown: 12, " +
+                    "Div0: 13, BadLanguageCode: 14, BadRegex: 15, InvalidFunctionUsage: 16, FileNotFound: 17, AnalysisError: 18, ReadPermission: 19, NotSupported: 20, InsufficientMemory: 21, QuotaExceeded: 22, Network: 23, Numeric: 24, InvalidArgument: 25]"
                 },
                 {
                     EnumConstants.ZoomEnumString,
