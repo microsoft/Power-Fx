415453513dc6a826e2d988cebcb7abe45f337833
Emily H <emhomm4@gmail.com>
2022-01-25 14:00:14 -0800

    Added named formula class (#107)
    * Added named formula class
    
    * made getsubscript method
    
    * Added unit tests
    
    * updated unit test, and subsscript method
    
    * Updated trygetsubscript method
    
    * A space
    
    * formatting
    
    * Fixed header
    
    * Added inline data, added HasErrors function
    
    * Renamed NamedFormula to NamedFormulas, added more tests
    
    * Changed helper method, updated unit tests
    
    * Updated varibles
    
    * Updated unit test

---
+++ src/libraries/Microsoft.PowerFx.Core/Syntax/NamedFormulas.cs
@@ -0,0 +1,104 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.PowerFx.Core.Errors;
+using Microsoft.PowerFx.Core.Localization;
+using Microsoft.PowerFx.Core.Parser;
+using Microsoft.PowerFx.Core.Syntax.Nodes;
+using Microsoft.PowerFx.Core.Utils;
+
+namespace Microsoft.PowerFx.Core.Syntax
+{
+    /// <summary>
+    /// This encapsulates a named formula: its original script, the parsed result, and any parse errors.
+    /// </summary>
+    internal class NamedFormulas
+    {
+        /// <summary>
+        /// A script containing one or more named formulas.
+        /// </summary>
+        public readonly string Script;
+
+        // The language settings used for parsing this script.
+        // May be null if the script is to be parsed in the current locale.
+        public readonly ILanguageSettings Loc;
+
+        public bool IsParsed => _formulasResult != null;
+
+        public bool HasParseErrors { get; private set; }
+
+        private Dictionary<DName, TexlNode> _formulasResult;
+
+        private List<TexlError> _errors;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NamedFormulas"/> class.
+        /// </summary>
+        /// <param name="script"></param>
+        /// <param name="loc"></param>
+        public NamedFormulas(string script, ILanguageSettings loc = null)
+        {
+            Contracts.AssertValue(script);
+            Contracts.AssertValueOrNull(loc);
+
+            Script = script;
+            Loc = loc;
+        }
+
+        /// <summary>
+        /// Ensures that the named formulas have been parsed and if not, parses them.
+        /// </summary>
+        /// <returns></returns>
+        public bool EnsureParsed()
+        {
+            if (_formulasResult == null)
+            {
+                Contracts.AssertValue(Script);
+                Contracts.AssertValueOrNull(Loc);
+                var result = TexlParser.ParseFormulasScript(Script, loc: Loc);
+                _formulasResult = result.NamedFormulas;
+                _errors = result.Errors;
+                HasParseErrors = result.HasError;
+                Contracts.AssertValue(_formulasResult);
+            }
+
+            return _errors == null;
+        }
+
+        /// <summary>
+        /// Returns any parse errors.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<TexlError> GetParseErrors()
+        {
+            Contracts.AssertValue(Script);
+            Contracts.Assert(IsParsed, "Should call EnsureParsed() first!");
+            return _errors ?? Enumerable.Empty<TexlError>();
+        }
+
+        /// <summary>
+        /// Returns a Formula object for each named formula.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Formula> GetFormulas()
+        {
+            var formulas = new List<Formula>();
+            if (_formulasResult != null)
+            {
+                foreach (var kvp in _formulasResult)
+                {
+                    formulas.Add(GetFormula(kvp.Value));
+                }
+            }
+
+            return formulas;
+        }
+
+        private Formula GetFormula(TexlNode node)
+        {
+            return new Formula(node.GetCompleteSpan().GetFragment(Script), node);
+        }
+    }
+}
---
+++ src/tests/Microsoft.PowerFx.Core.Tests/NamedFormulasTests.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System.Linq;
+using Microsoft.PowerFx.Core.Syntax;
+using Xunit;
+
+namespace Microsoft.PowerFx.Core.Tests
+{
+    public class NamedFormulasTests
+    {
+        [Theory]
+        [InlineData("x=1;y=2;")]
+        public void NamedFormulaTest(string script)
+        {
+            var namedFormula = new NamedFormulas(script);
+            Assert.Equal(script, namedFormula.Script);
+        }
+
+        [Theory]
+        [InlineData("x=1;y=2;")]
+        public void EnsureParsedTest(string script)
+        {
+            var namedFormula = new NamedFormulas(script);
+            Assert.True(namedFormula.EnsureParsed());
+        }
+
+        [Theory]
+        [InlineData("x=")]
+        public void EnsureParsedWithErrorsTest(string script)
+        {
+            var namedFormula = new NamedFormulas(script);
+            Assert.False(namedFormula.EnsureParsed());            
+        }
+
+        [Theory]
+        [InlineData("x=")]
+        public void GetParseErrorsTest(string script)
+        {
+            var namedFormula = new NamedFormulas(script);
+            namedFormula.EnsureParsed();
+
+            var errors = namedFormula.GetParseErrors();
+            Assert.NotEmpty(errors);
+        }
+
+        [Theory]
+        [InlineData("x=1;")]
+        public void GetParseErrorsNoErrorsTest(string script)
+        {
+            var namedFormula = new NamedFormulas(script);
+            namedFormula.EnsureParsed();
+
+            var errors = namedFormula.GetParseErrors();
+            Assert.Empty(errors);
+        }
+
+        [Theory]
+        [InlineData("x=1;y=2;", "1", "2")]
+        public void GetFormulasTest(string script, string expectedX, string expectedY)
+        {
+            var namedFormula = new NamedFormulas(script);
+            namedFormula.EnsureParsed();
+            var formulas = namedFormula.GetFormulas();
+            formulas.OrderBy(formula => formula.Script);
+
+            Assert.NotNull(formulas);
+
+            Assert.Equal(expectedX, formulas.ElementAt(0).Script);
+            Assert.Equal(expectedY, formulas.ElementAt(1).Script);
+        }
+    }
+}
