4f284ec2a44cf5e070861e54a15bd759cd3bac50
Bryan Croteau <bryancroteau@microsoft.com>
2022-01-26 14:14:15 -0800

    Add comments, negative tests

--- src/libraries/Microsoft.PowerFx.Core/Public/Types/ExternalType.cs
+++ src/libraries/Microsoft.PowerFx.Core/Public/Types/ExternalType.cs
@@ -6,12 +6,22 @@ using Microsoft.PowerFx.Core.Types;
 
 namespace Microsoft.PowerFx.Core.Public.Types
 {
+    /// <summary>
+    /// This enum represents types which are not supported by PowerFx
+    /// but which may be supported by external data formats.
+    /// </summary>
     public enum ExternalTypeKind
     {
         Array,
         Object
     }
 
+    /// <summary>
+    /// FormulaType that can be used by CustomObject instances to
+    /// indicate that the type of the data does not exist in PowerFx,
+    /// such as Arrays (PowerFx only supports Tables) or schema-less
+    /// objects.
+    /// </summary>
     public class ExternalType : FormulaType
     {
         public ExternalTypeKind Kind { get; }
