b9ecc0aa1d969d16e36778781ae6564346179fc2
Carlos Figueira <carlosfigueirafilho@hotmail.com>
2022-03-09 09:12:32 -0800

    Allow numeric enum variables to be used in the Kind property for the Error function

--- src/tests/Microsoft.PowerFx.Interpreter.Tests/FunctionCompilationTests.cs
+++ src/tests/Microsoft.PowerFx.Interpreter.Tests/FunctionCompilationTests.cs
@@ -1,8 +1,11 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+using Microsoft.PowerFx.Core.IR;
 using Microsoft.PowerFx.Core.Public.Types;
+using Microsoft.PowerFx.Core.Public.Values;
 using Microsoft.PowerFx.Core.Types;
+using Microsoft.PowerFx.Core.Types.Enums;
 using Microsoft.PowerFx.Interpreter;
 using Xunit;
 
@@ -21,6 +24,47 @@ namespace Microsoft.PowerFx.Tests
             Assert.True(check.IsSuccess);
             Assert.Null(check.Errors);
             Assert.Equal(FormulaType.String, check.ReturnType);
+        }
+
+        private class ErrorKindEnumFormulaType : FormulaType
+        {
+            private static readonly DType _errorKindEnumType = new EnumStore().GetEnum("ErrorKind");
+
+            public ErrorKindEnumFormulaType()
+                : base(_errorKindEnumType)
+            {
+            }
+
+            public override void Visit(ITypeVistor vistor)
+            {
+            }
+        }
+
+        private class ErrorKindEnumFormulaValue : FormulaValue
+        {
+            internal ErrorKindEnumFormulaValue()
+                : base(IRContext.NotInSource(new ErrorKindEnumFormulaType()))
+            {
+            }
+
+            public override object ToObject()
+            {
+                return "<ErrorKindEnum>";
+            }
+
+            public override void Visit(IValueVisitor visitor)
+            {
+            }
+        }
+
+        [Fact]
+        public void ErrorKindCanBeTypedEnumKind()
+        {
+            var engine = new RecalcEngine();
+            engine.UpdateVariable("A", new ErrorKindEnumFormulaValue());
+            var expression = "Error({Kind:A})";
+            var check = engine.Check(expression);
+            Assert.Null(check.Errors);
         }
     }
 }
