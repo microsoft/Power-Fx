27493e7001c9ecbc54780592abee451e2280848d
Ian Legler <60012924+ian-legler@users.noreply.github.com>
2022-03-10 14:50:16 -0800

    Add message key to expression error, allow custom name sanitization (#193)
    * Add message key to expression error, allow custom name sanitization
    
    * Update name provider contract to handle lookups

---
+++ src/libraries/Microsoft.PowerFx.Core/Logging/ISanitizedNameProvider.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using Microsoft.PowerFx.Core.Syntax;
+using Microsoft.PowerFx.Core.Syntax.Nodes;
+
+namespace Microsoft.PowerFx.Core.Logging
+{
+    internal interface ISanitizedNameProvider
+    {
+        /// <summary>
+        /// Attempt to sanitize an identifier using a custom sanitization scheme.
+        /// </summary>
+        /// <param name="identifier">The identifer.</param>
+        /// <param name="sanitizedName">The sanitized name output.</param>
+        /// <param name="dottedNameNode">The dotted name node, optional.</param>
+        /// <returns>Whether the custom sanitization should be used.</returns>
+        bool TrySanitizeIdentifier(Identifier identifier, out string sanitizedName, DottedNameNode dottedNameNode = null);
+    }
+}
