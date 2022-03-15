6fc5f5425314a9fc2412488dc0ffa723e71f023e
Om Prakaash <omsuseela@gmail.com>
2022-03-15 08:45:38 +0530

    Added file header to IRTranslatorTests

--- src/tests/Microsoft.PowerFx.Core.Tests/IRTranslatorTests.cs
+++ src/tests/Microsoft.PowerFx.Core.Tests/IRTranslatorTests.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
 using Microsoft.PowerFx.Core.Binding;
 using Microsoft.PowerFx.Core.Glue;
 using Microsoft.PowerFx.Core.IR;
