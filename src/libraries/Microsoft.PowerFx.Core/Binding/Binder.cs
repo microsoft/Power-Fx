cb933e8e0ebc647e7145370896581e3482649b56
rick-nguyen <97256426+rick-nguyen@users.noreply.github.com>
2022-04-05 12:33:50 -0700

    Add name of invoked unknown function to error message (#252)
    * initial commit
    
    * add tests, namespace to error message

--- src/libraries/Microsoft.PowerFx.Core/Binding/Binder.cs
+++ src/libraries/Microsoft.PowerFx.Core/Binding/Binder.cs
@@ -4626,7 +4626,15 @@ namespace Microsoft.PowerFx.Core.Binding
                 var overloads = LookupFunctions(funcNamespace, node.Head.Name.Value);
                 if (!overloads.Any())
                 {
-                    _txb.ErrorContainer.Error(node, TexlStrings.ErrUnknownFunction);
+                    if (funcNamespace.ToString() != string.Empty)
+                    {
+                        _txb.ErrorContainer.Error(node, TexlStrings.ErrUnknownNamespaceFunction, node.Head.Name.Value, funcNamespace.ToString());
+                    }
+                    else
+                    {
+                        _txb.ErrorContainer.Error(node, TexlStrings.ErrUnknownFunction, node.Head.Name.Value);
+                    }
+
                     _txb.SetInfo(node, new CallInfo(node));
                     _txb.SetType(node, DType.Error);
 
