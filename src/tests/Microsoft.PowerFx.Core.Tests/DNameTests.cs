d3c0b7ff9479418309bdae08c5591b408afd2bbd
Jesus Lopez Felix <jelopezf@microsoft.com>
2022-04-07 15:25:08 -0700

    test

---
+++ src/tests/Microsoft.PowerFx.Core.Tests/DNameTests.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.PowerFx.Core.Utils;
+using Xunit;
+
+namespace Microsoft.PowerFx.Core.Tests
+{
+    public sealed class DNameTests
+    {
+        [Fact]
+        public void TestMakeValid()
+        {
+            DName name;
+            bool modified;
+
+            name = DName.MakeValid(" Name   ", out modified);
+            Assert.False(modified);
+            Assert.Equal(" Name   ", name);
+
+            name = DName.MakeValid("Name\bAbcd", out modified);
+            Assert.False(modified);
+            Assert.Equal("Name\bAbcd", name);
+
+            name = DName.MakeValid("Name\nAbcd", out modified);
+            Assert.True(modified);
+            Assert.Equal("Name Abcd", name);
+
+            name = DName.MakeValid("   ", out modified);
+            Assert.True(modified);
+            Assert.Equal("_   ", name);
+
+            name = DName.MakeValid(" \t ", out modified);
+            Assert.True(modified);
+            Assert.Equal("_   ", name);
+        }
+    }
+}
