8a8ba2dab9cea043fbeec1948859ee2ac4004e25
McCall <lesaltzm@microsoft.com>
2022-04-07 14:59:52 -0700

    Symbols know their own type

--- src/libraries/Microsoft.PowerFx.Core/Entities/External/IExternalEntity.cs
+++ src/libraries/Microsoft.PowerFx.Core/Entities/External/IExternalEntity.cs
@@ -4,6 +4,7 @@
 using System.Collections.Generic;
 using Microsoft.PowerFx.Core.App.Controls;
 using Microsoft.PowerFx.Core.Errors;
+using Microsoft.PowerFx.Core.Types;
 using Microsoft.PowerFx.Core.Utils;
 
 namespace Microsoft.PowerFx.Core.Entities
@@ -11,5 +12,7 @@ namespace Microsoft.PowerFx.Core.Entities
     internal interface IExternalEntity
     {
         DName EntityName { get; }
+
+        DType Type { get; }
     }
 }
