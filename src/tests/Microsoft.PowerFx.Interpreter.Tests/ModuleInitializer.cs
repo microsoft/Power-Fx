f2c85120735553645d9c29fa41c4bb5e50be44d7
Luc Genetier <lucgen@microsoft.com>
2022-04-09 23:55:45 +0200

    Ensure tests are run using EN-US locale

---
+++ src/tests/Microsoft.PowerFx.Interpreter.Tests/ModuleInitializer.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace Microsoft.PowerFx.Interpreter.Tests
+{
+    internal static class ModuleInitializer
+    {
+        [ModuleInitializer]
+        internal static void SetLocaleForTests()
+        {
+            // Ensure all tests are run with English US locale
+            // Even if the local machine is using a different one
+            var testCulture = CultureInfo.GetCultureInfo("en-US");
+
+            Thread.CurrentThread.CurrentCulture = testCulture;
+            Thread.CurrentThread.CurrentUICulture = testCulture;
+            CultureInfo.DefaultThreadCurrentCulture = testCulture;
+            CultureInfo.DefaultThreadCurrentUICulture = testCulture;
+        }
+    }
+}
