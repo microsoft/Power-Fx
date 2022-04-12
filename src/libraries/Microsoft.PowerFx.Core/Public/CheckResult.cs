dcf19f2f431634a41959b476649de76188ef52d3
Ivan Radicek <ivradice@microsoft.com>
2022-04-12 15:12:16 +0200

    Expose a method to test whether a node is constant

--- src/libraries/Microsoft.PowerFx.Core/Public/CheckResult.cs
+++ src/libraries/Microsoft.PowerFx.Core/Public/CheckResult.cs
@@ -75,6 +75,13 @@ namespace Microsoft.PowerFx.Core.Public
         /// <returns></returns>
         public FormulaType GetNodeType(TexlNode node) => FormulaType.Build(_binding.GetType(node));
 
+        /// <summary>
+        /// CWhether the node is a constant node.
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public bool IsNodeConstant(TexlNode node) => _binding.IsConstant(node);
+
         internal IReadOnlyDictionary<string, TokenResultType> GetTokens(GetTokensFlags flags) => GetTokensUtils.GetTokens(_binding, flags);
 
         internal CheckResult SetErrors(IEnumerable<IDocumentError> errors)
