fc515ada5ae7a23b7e162e38ed56685aab391e13
Mike Stall <jmstall@microsoft.com>
2022-04-19 10:35:15 -0700

    Add 6000+ new .txt tests written by Mitu.
    Many of these don't pass yet, so putting in a "NotYetReady" directory and we'll migrate to ExpressionTestCase dir as we verify they pass.
    
    Add unit test to verify they at least all parse, and fix some parser issues.

--- src/tests/Microsoft.PowerFx.Interpreter.Tests/FileExpressionEvaluationTests.cs
+++ src/tests/Microsoft.PowerFx.Interpreter.Tests/FileExpressionEvaluationTests.cs
@@ -3,6 +3,7 @@
 
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using Microsoft.PowerFx.Core.Tests;
 using Microsoft.PowerFx.Interpreter.Tests.XUnitExtensions;
 using Xunit;
@@ -40,7 +41,7 @@ namespace Microsoft.PowerFx.Interpreter.Tests
                     Skip.If(true, prefix + msg);
                     break;
             }
-        }
+        } 
 
         // Since test discovery runs in a separate process, run a dedicated 
         // parse pass as a single unit test to verify all the .txt will parse. 
@@ -51,8 +52,35 @@ namespace Microsoft.PowerFx.Interpreter.Tests
             var method = GetType().GetMethod(nameof(InterpreterTestCase));
             var attr = (TxtFileDataAttribute)method.GetCustomAttributes(typeof(TxtFileDataAttribute), false)[0];
 
-            // Verify this runs wtihout throwing an exception.
+            // Verify this runs without throwing an exception.
             var list = attr.GetData(method);
+
+            // And doesn't report back any test failures. 
+            foreach (var batch in list)
+            {
+                var item = (ExpressionTestCase)batch[0];
+                Assert.Null(item.FailMessage);
+            }
+        }
+
+        // Scan the "Not Yet Ready" directory to ensure the tests all parse.
+        [Fact]
+        public void ScanNotYetReadyForTxtParseErrors()
+        {
+            var path = Path.Combine("ExpressionTestCases", "NotYetReady");
+
+            // For testing, you can also set path to a full path. 
+            // var path = @"D:\dev\pa2\Power-Fx\src\tests\Microsoft.PowerFx.Core.Tests\ExpressionTestCases\NotYetReady";
+
+            path = TxtFileDataAttribute.GetDefaultTestDir(path);
+
+            var runner = new TestRunner();
+
+            // Verify this runs without throwing an exception.
+            runner.AddDir(path);
+
+            // Ensure that we actually found tests and not pointed to an empty directory
+            Assert.True(runner.Tests.Count > 10);
         }
     }
 }
