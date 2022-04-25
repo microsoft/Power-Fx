525902e80cb654a1b496242fd5389e9ae7c72dee
McCall Saltzman <69215460+lesaltzm@users.noreply.github.com>
2022-04-25 16:36:37 -0700

    Move config option on INameResolver to a new BindingConfig type (#313)
    * Start
    
    * Move flags to config

---
+++ src/libraries/Microsoft.PowerFx.Core/Binding/BindingConfig.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.PowerFx.Core.Binding
+{
+    /// <summary>
+    /// Configuration for an invocation of the binder.
+    /// </summary>
+    [ThreadSafeImmutable]
+    internal class BindingConfig
+    {
+        public static readonly BindingConfig Default = new BindingConfig(allowsSideEffects: false);
+
+        public bool AllowsSideEffects { get; }
+
+        public BindingConfig(bool allowsSideEffects)
+        {
+            AllowsSideEffects = allowsSideEffects;
+        }
+    }
+}
