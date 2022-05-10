c3fc47ecf65c9b2557d513502cf5c8b526d8c16b
Ivan Radicek <ivradice@microsoft.com>
2022-05-10 20:13:42 +0200

    Added intellisense operations

---
+++ src/libraries/Microsoft.PowerFx.Core/Public/IntellisenseOperations.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.PowerFx.Intellisense
+{
+    /// <summary>
+    /// Various IntelliSense-like operations.
+    /// </summary>
+    public class IntellisenseOperations
+    {
+        private readonly CheckResult _checkResult;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IntellisenseOperations"/> class.
+        /// </summary>
+        /// <param name="result"></param>
+        public IntellisenseOperations(CheckResult result)
+        {
+            _checkResult = result;
+        }
+    }
+}
