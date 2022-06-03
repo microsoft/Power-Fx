b84b108d0383f30a100ab8741afbfa339a707375
Mike Stall <mikestall@hotmail.com>
2022-06-03 07:28:18 -0700

    Basic connector support  (#397)

---
+++ src/tests/Microsoft.PowerFx.Connectors.Tests/CacheTests.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System.Threading.Tasks;
+using Microsoft.PowerFx.Connectors;
+using Microsoft.PowerFx.Core.Tests;
+using Microsoft.PowerFx.Types;
+using Xunit;
+
+namespace Microsoft.PowerFx.Tests
+{
+    public class CacheTests : PowerFxTest
+    {
+        private readonly ICachingHttpClient _cache = new CachingHttpClient();
+
+        [Fact]
+        public async Task TestCache()
+        {            
+            var scope1 = "scope1";
+            var scope2 = "scope2";
+
+            Assert.Equal(5.0, Test(scope1, "key1", 5));
+            Assert.Equal(5.0, Test(scope1, "key1", -999)); // already cached
+
+            Assert.Equal(7.0, Test(scope1, "key2", 7));
+
+            Assert.Equal(33.0, Test(scope2, "key1", 33)); // new scope, cache miss
+
+            _cache.Reset(scope1);
+            Assert.Equal(6.0, Test(scope1, "key1", 6)); // reset, gets new value
+            Assert.Equal(33.0, Test(scope2, "key1", -999)); // unchanged
+        }
+
+        private double Test(string cacheScope, string requestKey, double callback)
+        {
+            var r1 = _cache.TryGetAsync(cacheScope, requestKey, () => New(callback)).Result;
+            return (double)r1.ToObject();
+        }
+
+        public async Task<FormulaValue> New(double n)
+        {
+            return FormulaValue.New(n);
+        }
+    }
+}
---
+++ src/tests/Microsoft.PowerFx.Connectors.Tests/Helpers/Helpers.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System.IO;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Readers;
+using Xunit;
+
+namespace Microsoft.PowerFx.Tests
+{
+    internal static class Helpers
+    {
+        public static Stream GetStream(string name)
+        {
+            var assemblyNamespace = "Microsoft.PowerFx.Connectors.Tests";
+            var fullName = assemblyNamespace + "." + name.Replace('\\', '.');
+
+            var assembly = typeof(BasicRestTests).Assembly;
+            var stream = assembly.GetManifestResourceStream(fullName);
+
+            Assert.NotNull(stream);
+            return stream;
+        }
+
+        public static string ReadAllText(string name)
+        {
+            using (var stream = GetStream(name))
+            using (var textReader = new StreamReader(stream))
+            {
+                return textReader.ReadToEnd();
+            }
+        }
+
+        // Get a swagger file from the embedded resources. 
+        public static OpenApiDocument ReadSwagger(string name)
+        {
+            using (var stream = GetStream(name))
+            {
+                var doc = new OpenApiStreamReader().Read(stream, out var diag);
+                return doc;
+            }
+        }
+    }
+}
