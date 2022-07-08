78dd7dbc6fb953a03a8e1012541800dd1b170805
Luc Genetier <69138830+LucGenetier@users.noreply.github.com>
2022-07-08 09:23:42 +0200

    [ { a: 1 } ] - [Breaking Change] Do not add "Value:" for arrays of records (#444)

--- src/libraries/Microsoft.PowerFx.Core/Public/Engine.cs
+++ src/libraries/Microsoft.PowerFx.Core/Public/Engine.cs
@@ -139,7 +139,8 @@ namespace Microsoft.PowerFx
                 parse.Root,
                 resolver,
                 bindingConfig,
-                ruleScope: parameterType._type);
+                ruleScope: parameterType._type,
+                features: Config.Features);
 
             var result = new CheckResult(parse, binding);
 
