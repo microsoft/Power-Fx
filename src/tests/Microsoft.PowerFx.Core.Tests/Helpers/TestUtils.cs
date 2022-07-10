d583c4380e6d6adcfb64d53f66c80d2d40baadd8
McCall Saltzman <69215460+lesaltzm@users.noreply.github.com>
2022-07-10 14:37:00 -0700

    Remove DType unused members and port tests (#468)

---
+++ src/tests/Microsoft.PowerFx.Core.Tests/Helpers/TestUtils.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.PowerFx.Core.Types;
+using Xunit;
+
+namespace Microsoft.PowerFx.Core.Tests.Helpers
+{
+    internal class TestUtils
+    {        
+        // Parse a type in string form to a DType
+        public static DType DT(string type)
+        {
+            Assert.True(DType.TryParse(type, out DType dtype));
+            Assert.True(dtype.IsValid);
+            return dtype;
+        }
+    }
+}
