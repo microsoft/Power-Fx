cc61da6d23ca6b66a24149ad57d9cdb6eb5d6f69
Luc Genetier <lucgen@microsoft.com>
2022-07-15 09:34:35 +0200

    wip

--- src/libraries/Microsoft.PowerFx.Core/Texl/Builtins/Sqrt.cs
+++ src/libraries/Microsoft.PowerFx.Core/Texl/Builtins/Sqrt.cs
@@ -1,8 +1,13 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+using System.Collections.Generic;
+using Microsoft.PowerFx.Core.App.ErrorContainers;
+using Microsoft.PowerFx.Core.Binding;
 using Microsoft.PowerFx.Core.Localization;
 using Microsoft.PowerFx.Core.Types;
+using Microsoft.PowerFx.Core.Utils;
+using Microsoft.PowerFx.Syntax;
 
 namespace Microsoft.PowerFx.Core.Texl.Builtins
 {
@@ -13,7 +18,7 @@ namespace Microsoft.PowerFx.Core.Texl.Builtins
         public SqrtFunction()
             : base("Sqrt", TexlStrings.AboutSqrt, FunctionCategories.MathAndStat)
         {
-        }
+        }      
     }
 
     // Sqrt(E:*[n])
@@ -23,6 +28,23 @@ namespace Microsoft.PowerFx.Core.Texl.Builtins
         public SqrtTableFunction()
             : base("Sqrt", TexlStrings.AboutSqrtT, FunctionCategories.Table)
         {
+        }
+
+        public override bool CheckInvocation(TexlNode[] args, DType[] argTypes, IErrorContainer errors, out DType returnType, out Dictionary<TexlNode, DType> nodeToCoercedTypeMap)
+        {
+            var fValid = base.CheckInvocation(args, argTypes, errors, out returnType, out nodeToCoercedTypeMap);
+            
+            if (returnType.IsTable && returnType != DType.EmptyRecord.Add(new TypedName(DType.Number, ColumnName_Value)).ToTable())
+            {
+                var arg = args[0];
+                var argType = argTypes[0];
+                fValid &= CheckStringColumnType(argType, arg, errors, ref nodeToCoercedTypeMap);
+
+                var rowType = DType.EmptyRecord.Add(new TypedName(DType.Number, ColumnName_Value));
+                returnType = rowType.ToTable();
+            }
+
+            return fValid;
         }
     }
 }
