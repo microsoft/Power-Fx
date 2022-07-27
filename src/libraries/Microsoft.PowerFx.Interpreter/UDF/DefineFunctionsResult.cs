9ba3b21d68b6182eed5d8c13226037daa2c60742
Malek <pocmalek@gmail.com>
2022-07-27 16:24:41 -0700

    Integration changes working (#496)
    * Integration changes working
    
    * added change
    
    * changed naming and added FunctionInfo returns for UDFs
    
    * changed return type of define funciton
    
    * changed to property for texlfunction in lazybinder
    
    * Changed some small things
    
    * fixed public surface tests
    
    * changed errors

---
+++ src/libraries/Microsoft.PowerFx.Interpreter/UDF/DefineFunctionsResult.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.PowerFx.Interpreter.UDF
+{
+    public class DefineFunctionsResult : IOperationStatus
+    {
+        private readonly IEnumerable<ExpressionError> _expressionErrors;
+        private readonly IEnumerable<FunctionInfo> _functionInfos;
+
+        public IEnumerable<FunctionInfo> FunctionInfo => _functionInfos;
+        
+        public DefineFunctionsResult(IEnumerable<ExpressionError> expressionErrors, IEnumerable<FunctionInfo> functionInfos)
+        {
+            _expressionErrors = expressionErrors;
+            _functionInfos = functionInfos;
+        }
+
+        public IEnumerable<ExpressionError> Errors => _expressionErrors;
+
+        public bool IsSuccess => !_expressionErrors.Any();
+    }
+}
