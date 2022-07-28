f4356496b2652472867c802a0fa08bee1d2356d0
Akshar <aksharpatel@microsoft.com>
2022-07-28 16:49:48 -0400

    adding dropped curly braces back in interpolated string when formatting

--- src/libraries/Microsoft.PowerFx.Core/Utils/CharacterUtils.cs
+++ src/libraries/Microsoft.PowerFx.Core/Utils/CharacterUtils.cs
@@ -158,7 +158,7 @@ namespace Microsoft.PowerFx.Core.Utils
             return InternalEscapeString(value, length, lengthForBuilder, ref sb, finalizeBuilder: true);
         }
 
-        public static string ExcelEscapeString(string value)
+        public static string ExcelEscapeString(string value, bool isInterpolatedString = false)
         {
             Contracts.AssertValue(value);
 
@@ -173,6 +173,10 @@ namespace Microsoft.PowerFx.Core.Utils
                 {
                     case '\"':
                         UpdateEscapeInternals("\"\"", value, lengthForBuilder, i, ref charsToAdd, ref sb);
+                        break;
+                    case '{':
+                    case '}':
+                        HandleEscapedCurlyBraces(value[i], value, isInterpolatedString, lengthForBuilder, i, ref charsToAdd, ref sb);
                         break;
                     default:
                         charsToAdd++;
@@ -378,6 +382,18 @@ namespace Microsoft.PowerFx.Core.Utils
             }
 
             sb.Append(escapedValue);
+        }
+
+        private static void HandleEscapedCurlyBraces(char curlyBrace, string input, bool isInterpolatedInput, int estimatedLength, int currentPosition, ref int charsToAdd, ref StringBuilder sb)
+        {
+            Contracts.AssertOneOfValueType(curlyBrace, '{', '}');
+
+            if (!isInterpolatedInput)
+            {
+                return;
+            }
+
+            UpdateEscapeInternals($"{curlyBrace}{curlyBrace}", input, estimatedLength, currentPosition, ref charsToAdd, ref sb);
         }
 
         // If a string is going to be inserted into a string which is then used as a format string, it needs to be escaped first
