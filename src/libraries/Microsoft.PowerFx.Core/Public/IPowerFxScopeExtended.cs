5fbae91eb6facd85c7940b3c7e7032498cd06436
Luc Genetier <lucgen@microsoft.com>
2022-08-11 18:47:18 +0200

    Revert "WIP"
    This reverts commit 722225dd8fe96ee8b033d4d8ce1deb7453752110.

---
+++ src/libraries/Microsoft.PowerFx.Core/Public/IPowerFxScopeExtended.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+namespace Microsoft.PowerFx.Intellisense
+{
+    internal interface IPowerFxScopeExtended : IPowerFxScope
+    {
+        /// <summary>
+        /// Check for errors in the given expression, with parser options. 
+        /// </summary>
+        /// <param name="expression">The expression to validate.</param>
+        /// <param name="options">Parser options to use.</param>
+        /// <returns>Validation result.</returns>
+        CheckResult Check(string expression, ParserOptions options);
+
+        /// <summary>
+        /// Provide intellisense for expression, from CheckResult.
+        /// </summary>
+        IIntellisenseResult Suggest(string expression, CheckResult checkResult, int cursorPosition);
+    }
+}
