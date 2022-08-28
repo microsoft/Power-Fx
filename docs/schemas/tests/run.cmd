4cfcf845afb48f85ba93119651924c61f81d6e6f
Greg Lindhorst <gregli@microsoft.com>
2022-08-28 10:22:14 -0700

    more

---
+++ docs/schemas/tests/run.cmd
@@ -0,0 +1,25 @@
+@echo off
+
+rem install ajv with "npm install -g ajv-cli"
+rem source at https://github.com/ajv-validator/ajv-cli and https://github.com/ajv-validator/ajv
+rem filename parsing drives the tests, in the format "schema-result-....yaml"
+
+set total=0
+set fail=0
+for %%f in (*.yaml) do call :each %%f
+echo %fail% failed, %total% total
+goto :EOF
+
+:each
+for /f "tokens=1,2,* delims=-" %%a in ("%1") do call :parse %%a %%b %%c 
+cmd /c "ajv test -s ..\%type%.schema.json -d %1 --spec=draft2019 --%result% --errors=no"
+if errorlevel 1 set /a fail=fail+1
+set /a total=total+1
+goto :EOF
+
+:parse
+set type=%1
+set result=%2
+set test=%3
+
+
