microsoft/Power-Fx

commit 7fd2d9addd4d35f15e5e94f7071f508e778ab4ed

Add missing NoOp case to ToTable (#599)
fixes #519

src/libraries/Microsoft.PowerFx.Core/Types/DType.cs
@@ -1067,6 +1067,7 @@ namespace Microsoft.PowerFx.Core.Types
 
             switch (Kind)
             {
+                case DKind.LazyTable:
                 case DKind.Table:
                     return this;
                 case DKind.LazyRecord:
@@ -1214,23 +1215,7 @@ namespace Microsoft.PowerFx.Core.Types
                 return true;
             }
 
-            if (!type.IsAggregate)
-            {
-                type = Invalid;
-                return false;
-            }
-
-            switch (type.Kind)
-            {
-                case DKind.Record:
-                case DKind.Table:
-                case DKind.OptionSet:
-                case DKind.View:
-                    return type.TypeTree.TryGetValue(path.Name, out type);
-                default:
-                    Contracts.Assert(false);
-                    return false;
-            }
+            return TryGetTypeCore(path.Name, out type);
         }
 
         // Get the type of a member field specified by path.
@@ -1664,6 +1649,9 @@ namespace Microsoft.PowerFx.Core.Types
                 case DKind.Enum:
                     var supertype = new DType(type.EnumSuperkind);
                     return type.ValueTree.GetPairs().Select(kvp => new TypedName(supertype, new DName(kvp.Key)));
+                case DKind.LazyRecord:
+                case DKind.LazyTable:
+                    return type.GetRootFieldNames().Select(name => new TypedName(type.GetType(name), name));
                 default:
                     return Enumerable.Empty<TypedName>();
             }
@@ -1772,6 +1760,8 @@ namespace Microsoft.PowerFx.Core.Types
                 case DKind.DataEntity:
                     Contracts.AssertValue(ExpandInfo);
                     return type.ExpandInfo.Identity == ExpandInfo.Identity;
+                case DKind.LazyRecord:
+                case DKind.LazyTable:
                 case DKind.Table:
                 case DKind.Record:
                     if (type.ExpandInfo != null && type.ExpandInfo.Identity != ExpandInfo.Identity)

src/tests/Microsoft.PowerFx.Core.Tests/BindingEngineTests.cs
@@ -309,6 +309,21 @@ namespace Microsoft.PowerFx.Tests
             Assert.True(lazyTypeInstance.EnumerableIterated);
         }
 
+        [Fact]
+        public void CheckShuffleLazyTable()
+        {
+            var config = new PowerFxConfig();
+            var engine = new Engine(config);
+
+            var lazyTypeInstance = new LazyRecursiveRecordType().ToTable();
+
+            var result = engine.Check("Shuffle(Table)", RecordType.Empty().Add("Table", lazyTypeInstance));
+            Assert.True(result.IsSuccess);
+
+            var tableType = Assert.IsType<TableType>(result.ReturnType);
+            Assert.IsType<LazyRecursiveRecordType>(tableType.ToRecord());
+        }
+
         /// <summary>
         /// A function with behavior/side-effects used in testing.
         /// </summary>

src/tests/Microsoft.PowerFx.Core.Tests/LazyTypeTests.cs
@@ -261,6 +261,12 @@ namespace Microsoft.PowerFx.Core.Tests
 
             Assert.NotEqual(_lazyRecord1, _lazyRecord2.ToTable().ToRecord());
             Assert.NotEqual(_lazyTable2, _lazyTable1.ToRecord().ToTable());
+
+            Assert.Equal(_lazyTable2._type, _lazyTable2._type.ToRecord().ToTable());
+            Assert.Equal(_lazyRecord1._type, _lazyRecord1._type.ToTable().ToRecord());
+
+            Assert.Equal(_lazyTable2._type, _lazyTable2._type.ToTable());
+            Assert.Equal(_lazyRecord1._type, _lazyRecord1._type.ToRecord());
         }
 
         [Fact]

commit ee96d3cb31a600e2e8e486284312701a449088fa

Fix TryGetType DPath overload (#612)
This DType overload is rarely used, and appeared untested in this repo. Fixes a recent break caused by #599 and adds test

src/libraries/Microsoft.PowerFx.Core/Types/DType.cs
@@ -1180,7 +1180,7 @@ namespace Microsoft.PowerFx.Core.Types
                 return true;
             }
 
-            return TryGetTypeCore(path.Name, out type);
+            return type.TryGetTypeCore(path.Name, out type);
         }
 
         // Get the type of a member field specified by path.

src/tests/Microsoft.PowerFx.Core.Tests/DTypeTests.cs
@@ -633,6 +633,21 @@ namespace Microsoft.PowerFx.Tests
             Assert.True(AttachmentRecordType.IsAttachment);
             Assert.NotNull(AttachmentRecordType.AttachmentType);
         }
+
+        [Fact]
+        public void TryGetTypePathTest()
+        {
+            var type = TestUtils.DT("*[A:n, B:*[D:s, E:*[F:b]], C:n]");
+            Assert.True(
+                type.TryGetType(
+                    DPath.Root
+                        .Append(new DName("B"))
+                        .Append(new DName("E"))
+                        .Append(new DName("F")),
+                    out var result));
+
+            Assert.Equal(DType.Boolean, result);
+        }
                 
         [Fact]
         public void RecordAndTableDTypeTests()