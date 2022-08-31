66ed7ece8f056c9e0427f83a1a5a85cf3f202804
Bryan Croteau <bryancroteau@microsoft.com>
2022-08-31 14:23:46 -0700

    Improve null safety

--- src/libraries/Microsoft.PowerFx.Core/Binding/BinderCheckTypeResult.cs
+++ src/libraries/Microsoft.PowerFx.Core/Binding/BinderCheckTypeResult.cs
@@ -9,12 +9,30 @@ namespace Microsoft.PowerFx.Core.Binding
 {
     internal readonly struct BinderCheckTypeResult
     {
+        private readonly IReadOnlyCollection<BinderCoercionResult> _coercions;
+
         public bool Success { get; init; }
 
         // The binder has an imperative function called SetCoercedType,
         // which may be called multiple times in a given type checking function
         // This collection represents repeated calls to that function
-        public IReadOnlyCollection<BinderCoercionResult> Coercions { get; init; }
+        public IReadOnlyCollection<BinderCoercionResult> Coercions
+        {
+            get
+            {
+                if (_coercions == null)
+                {
+                    return new List<BinderCoercionResult>();
+                }
+
+                return _coercions;
+            }
+
+            init
+            {
+                _coercions = value;
+            }
+        }
 
         public TexlNode Node { get; init; }
 
