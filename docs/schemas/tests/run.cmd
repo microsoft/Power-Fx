93a05af70916b3a4d5189b398b1581d73ea38565
Greg Lindhorst <gregli@microsoft.com>
2022-08-31 00:10:24 -0700

    more

--- docs/schemas/tests/run.cmd
+++ docs/schemas/tests/run.cmd
@@ -5,6 +5,11 @@ rem install ajv with "npm install -g ajv-cli"
 rem source at https://github.com/ajv-validator/ajv-cli and https://github.com/ajv-validator/ajv
 rem filename parsing drives the tests, in the format "schema-result-....yaml"
 
+if not "%1" == "" call :each %1
+if not "%1" == "" goto :EOF
+
+set errors=--errors=no
+
 set total=0
 set fail=0
 for %%f in (*.yaml) do call :each %%f
@@ -17,7 +22,7 @@ set rfunction=
 set rtype=
 if not "%type%"=="function" set rfunction=-r ..\function.schema.json
 if not "%type%"=="type" set rtype=-r ..\type.schema.json
-cmd /c "ajv test -s ..\%type%.schema.json -d %1 --spec=draft2019 --%result% --errors=no --strict=false %rtype% %rfunction%
+cmd /c "ajv test -s ..\%type%.schema.json -d %1 --spec=draft2019 --%result% %errors% %rtype% %rfunction% --strict=false"
 if errorlevel 1 set /a fail=fail+1
 set /a total=total+1
 goto :EOF
