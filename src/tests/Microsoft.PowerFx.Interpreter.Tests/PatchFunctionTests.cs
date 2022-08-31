f715e9009da70f5b826c4489d5a37a5dd4a99706
Anderson Silva <joyle@outlook.com>
2022-08-31 16:38:41 -0500

    Patch function (#595)
    This is work in progress. Two Patch implementations for now:
    
    > Patch(record, record)
    > Patch(dataSource, record, arg1, arg2, ...)

---
+++ src/tests/Microsoft.PowerFx.Interpreter.Tests/PatchFunctionTests.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.PowerFx.Core.App.ErrorContainers;
+using Microsoft.PowerFx.Core.Binding;
+using Microsoft.PowerFx.Core.Errors;
+using Microsoft.PowerFx.Core.Functions;
+using Microsoft.PowerFx.Core.Functions.FunctionArgValidators;
+using Microsoft.PowerFx.Core.Localization;
+using Microsoft.PowerFx.Core.Tests;
+using Microsoft.PowerFx.Core.Types;
+using Microsoft.PowerFx.Core.Utils;
+using Microsoft.PowerFx.Functions;
+using Microsoft.PowerFx.Interpreter;
+using Microsoft.PowerFx.Syntax;
+using Microsoft.PowerFx.Types;
+using Xunit;
+using static Microsoft.PowerFx.Core.Localization.TexlStrings;
+
+namespace Microsoft.PowerFx.Interpreter.Tests
+{
+    public class PatchFunctionTests : PowerFxTest
+    {
+        [Theory]
+        [InlineData(typeof(PatchFunction))]
+        [InlineData(typeof(PatchRecordFunction))]
+        public async Task CheckArgsTestAsync(Type type)
+        {
+            var expressionError = new ExpressionError()
+            {
+                Kind = ErrorKind.ReadOnlyValue,
+                Severity = ErrorSeverity.Critical,
+                Message = "Something went wrong"
+            };
+
+            FormulaValue[] args = new[]
+            {
+                FormulaValue.NewError(expressionError)
+            };
+
+            var function = Activator.CreateInstance(type) as IAsyncTexlFunction;
+            var result = await function.InvokeAsync(args, CancellationToken.None);
+
+            Assert.IsType<ErrorValue>(result);
+        }
+    }
+}
