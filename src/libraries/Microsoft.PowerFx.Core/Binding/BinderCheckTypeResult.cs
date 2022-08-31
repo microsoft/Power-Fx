b50fe408dcc225329ec28585387810e570b56c72
Bryan Croteau <bryancroteau@microsoft.com>
2022-08-30 18:07:02 -0700

    PR feedback

--- src/libraries/Microsoft.PowerFx.Core/Binding/BinderCheckTypeResult.cs
+++ src/libraries/Microsoft.PowerFx.Core/Binding/BinderCheckTypeResult.cs
@@ -7,14 +7,17 @@ using Microsoft.PowerFx.Syntax;
 
 namespace Microsoft.PowerFx.Core.Binding
 {
-    internal class BinderCheckTypeResult
+    internal readonly struct BinderCheckTypeResult
     {
         public bool Success { get; init; }
 
+        // The binder has an imperative function called SetCoercedType,
+        // which may be called multiple times in a given type checking function
+        // This collection represents repeated calls to that function
         public IReadOnlyCollection<BinderCoercionResult> Coercions { get; init; }
     }
 
-    internal class BinderCoercionResult
+    internal readonly struct BinderCoercionResult
     {
         public TexlNode Node { get; init; }
 
