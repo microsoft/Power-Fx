3436202cf2eb59809ec3960e8659b7e31cd8fb38
McCall Saltzman <69215460+lesaltzm@users.noreply.github.com>
2022-09-08 10:11:30 -0700

    Revert #465 conditioned on flag (#634)
    Partially Reverts #465 which introduced a significant performance
    regression.
    This change reverts it, conditioned on a new flag added to
    DocumentProperties. We'd like to remove this flag, and will once we've
    adjusted the core Select/Expand behavior.

---
+++ src/libraries/Microsoft.PowerFx.Core/App/IExternalUserFlags.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+namespace Microsoft.PowerFx.Core.App
+{
+    /// <summary>
+    /// All hosts should use <see cref="DefaultUserFlags"/> except for Canvas Apps (legacy flag support)
+    /// This interface and the flags in it should be deprecated once the below flags are removed from Canvas Apps
+    /// DO NOT add flags to this clas without very strong justification. We do not want to allow PowerFx
+    /// behavior to be different between target platforms. 
+    /// </summary>
+    internal interface IExternalUserFlags
+    {
+        bool EnforceSelectPropagationLimit { get; }
+    }
+
+    internal sealed class DefaultUserFlags : IExternalUserFlags
+    {
+        public bool EnforceSelectPropagationLimit => true;
+    }
+}
