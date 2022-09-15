7fd5ce982d1dcdb6922f9359fdacb99c0805b1d4
Greg Lindhorst <gregli@microsoft.com>
2022-09-15 15:30:41 -0700

    Extra ParseTrivia if the seimicolon is followed by whitespace (or other trivia) befoe the Eof

--- src/libraries/Microsoft.PowerFx.Core/Parser/TexlParser.cs
+++ src/libraries/Microsoft.PowerFx.Core/Parser/TexlParser.cs
@@ -89,6 +89,8 @@ namespace Microsoft.PowerFx.Core.Parser
                 {
                     break;
                 }
+
+                ParseTrivia();
             }
 
             return new ParseUDFsResult(udfs, _errors);
