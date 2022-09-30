81f8e5fda2930f1c18608977735ac92012015de1
Jas Valgotar <32079188+jas-valgotar@users.noreply.github.com>
2022-09-30 15:54:49 -0400

    Porting TexlTests (#685)
    -Ports Texl Tests from [PAClient
    ](https://msazure.visualstudio.com/OneAgile/_git/PowerApps-Client?path=/src/Cloud/unittests/DocumentServer.Core.LargeTests/TexlTest.cs&_a=contents&version=GBmaster)to
    PowerFX

--- src/tests/Microsoft.PowerFx.Core.Tests/Helpers/TestUtils.cs
+++ src/tests/Microsoft.PowerFx.Core.Tests/Helpers/TestUtils.cs
@@ -6,7 +6,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
+using Microsoft.PowerFx.Core.App.ErrorContainers;
+using Microsoft.PowerFx.Core.Errors;
+using Microsoft.PowerFx.Core.Functions;
+using Microsoft.PowerFx.Core.Localization;
+using Microsoft.PowerFx.Core.Texl.Builtins;
 using Microsoft.PowerFx.Core.Types;
+using Microsoft.PowerFx.Core.Utils;
+using Microsoft.PowerFx.Syntax;
 using Xunit;
 
 namespace Microsoft.PowerFx.Core.Tests.Helpers
@@ -99,6 +106,205 @@ namespace Microsoft.PowerFx.Core.Tests.Helpers
                     Assert.True(false, $"Unexpected JsonValueKind: JsonValueKind.{valueKind}.");
                     break;
             }
+        }
+
+        public sealed class MockFunctionThatSupportsCoercedParamsWithOverride : BuiltinFunction
+        {
+            private readonly string _runtimeFunctionNameSuffix;
+            private readonly bool _supportsParamCoercion;
+
+            // Be explicit about wanting to support param coercion.
+            public override bool SupportsParamCoercion => _supportsParamCoercion;
+
+            public override bool IsSelfContained => true;
+
+            public bool CheckNumericTableOverload { get; set; }
+
+            public bool CheckStringTableOverload { get; set; }
+
+            public MockFunctionThatSupportsCoercedParamsWithOverride(string name, string runtimeFunctionNameSuffix, bool supportsParamCoercion, DType returnType, System.Numerics.BigInteger maskLambdas, int arityMin, int arityMax, params DType[] argTypes)
+                : base(name, (l) => name, FunctionCategories.Text, returnType, maskLambdas, arityMin, arityMax, argTypes)
+            {
+                _runtimeFunctionNameSuffix = runtimeFunctionNameSuffix;
+                _supportsParamCoercion = supportsParamCoercion;
+            }
+
+            public override IEnumerable<TexlStrings.StringGetter[]> GetSignatures()
+            {
+                yield break;
+            }
+
+            public override bool CheckInvocation(TexlNode[] args, DType[] argTypes, IErrorContainer errors, out DType returnType, out Dictionary<TexlNode, DType> coercedArgs)
+            {
+                var isValid = base.CheckInvocation(args, argTypes, errors, out returnType, out coercedArgs);
+
+                // explicitly blocking coercion
+                var wasCoerced = false;
+                if (CheckNumericTableOverload)
+                {
+                    if (CheckColumnType(argTypes[0], args[0], DType.Number, errors, TexlStrings.ErrInvalidSchemaNeedNumCol_Col, ref wasCoerced))
+                    {
+                        if (wasCoerced)
+                        {
+                            CollectionUtils.Add(ref coercedArgs, args[0], DType.EmptyTable.Add(new DName("Value"), DType.Number), allowDupes: true);
+                        }
+                    }
+                    else
+                    {
+                        isValid = false;
+                        if (coercedArgs != null)
+                        {
+                            coercedArgs.Clear();
+                        }
+                    }
+
+                    return isValid;
+                }
+
+                if (CheckStringTableOverload)
+                {
+                    if (!CheckStringColumnType(argTypes[0], args[0], errors, ref coercedArgs))
+                    {
+                        isValid = false;
+                        if (coercedArgs != null)
+                        {
+                            coercedArgs.Clear();
+                        }
+                    }
+
+                    return isValid;
+                }
+
+                if (!DType.Number.Accepts(argTypes[0]))
+                {
+                    isValid = false;
+                    errors.EnsureError(DocumentErrorSeverity.Severe, args[0], TexlStrings.ErrNumberExpected);
+                    if (coercedArgs != null)
+                    {
+                        coercedArgs.Clear();
+                    }
+                }
+
+                return isValid;
+            }
+
+            private bool CheckColumnType(DType type, TexlNode arg, DType expectedType, IErrorContainer errors, ErrorResourceKey errKey, ref bool wasCoerced)
+            {
+                Contracts.Assert(type.IsValid);
+                Contracts.AssertValue(arg);
+                Contracts.Assert(expectedType.IsValid);
+                Contracts.AssertValue(errors);
+
+                IEnumerable<TypedName> columns;
+                if (!type.IsTable || (columns = type.GetNames(DPath.Root)).Count() != 1)
+                {
+                    errors.EnsureError(DocumentErrorSeverity.Severe, arg, TexlStrings.ErrInvalidSchemaNeedCol);
+                    return false;
+                }
+                else if (!(expectedType.Accepts(columns.Single().Type) || columns.Single().Type.CoercesTo(expectedType)))
+                {
+                    errors.EnsureError(DocumentErrorSeverity.Severe, arg, errKey, columns.Single().Name.Value);
+                    return false;
+                }
+
+                if (!expectedType.Accepts(columns.Single().Type))
+                {
+                    wasCoerced = true;
+                }
+
+                return true;
+            }
+
+            public override string GetUniqueTexlRuntimeName(bool isPrefetching = false)
+            {
+                return base.GetUniqueTexlRuntimeName(isPrefetching) + _runtimeFunctionNameSuffix;
+            }
+        }
+
+        public class MockFunction : BuiltinFunction
+        {
+            private readonly FunctionFlags _flags;
+            private readonly string _runtimeFunctionNameSuffix;
+
+            public override bool IsAsync => _flags.HasFlag(FunctionFlags.IsAsync);
+
+            public override bool IsBehaviorOnly => _flags.HasFlag(FunctionFlags.IsBehaviorOnly);
+            
+            public override bool IsStateless => _flags.HasFlag(FunctionFlags.IsStateless);
+            
+            public override bool IsSelfContained => _flags.HasFlag(FunctionFlags.IsSelfContained);
+            
+            public override bool ManipulatesCollections => _flags.HasFlag(FunctionFlags.ManipulatesCollections);
+            
+            public override bool AffectsCollectionSchemas => _flags.HasFlag(FunctionFlags.AffectsCollectionSchemas);
+            
+            public override bool IsStrict => _flags.HasFlag(FunctionFlags.IsStrict);
+            
+            public override bool SupportsParamCoercion => _flags.HasFlag(FunctionFlags.SupportsParamCoercion);
+
+            public MockFunction(string name, string runtimeFunctionNameSuffix, FunctionCategories category, FunctionFlags flags, DType returnType, System.Numerics.BigInteger maskLambdas, int arityMin, int arityMax, params DType[] argTypes)
+                : base(name, (l) => "MockFunction", category, returnType, maskLambdas, arityMin, arityMax, argTypes)
+            {
+                _flags = flags;
+                _runtimeFunctionNameSuffix = runtimeFunctionNameSuffix;
+            }
+
+            public override IEnumerable<TexlStrings.StringGetter[]> GetSignatures()
+            {
+                yield break;
+            }
+
+            public override string GetUniqueTexlRuntimeName(bool isPrefetching = false)
+            {
+                return base.GetUniqueTexlRuntimeName() + _runtimeFunctionNameSuffix;
+            }
+        }
+
+        public sealed class MockFunctionWithScope : FunctionWithTableInput
+        {
+            private readonly FunctionFlags _flags;
+            private readonly string _runtimeFunctionNameSuffix;
+
+            public override bool IsAsync => _flags.HasFlag(FunctionFlags.IsAsync);
+            
+            public override bool IsBehaviorOnly => _flags.HasFlag(FunctionFlags.IsBehaviorOnly);
+            
+            public override bool SupportsParamCoercion => _flags.HasFlag(FunctionFlags.SupportsParamCoercion);
+
+            public override bool IsSelfContained => true;
+
+            public MockFunctionWithScope(string name, string runtimeFunctionNameSuffix, FunctionCategories category, FunctionFlags flags, DType returnType, System.Numerics.BigInteger maskLambdas, int arityMin, int arityMax, params DType[] argTypes)
+                : base(name, (l) => "MockFunction", category, returnType, maskLambdas, arityMin, arityMax, argTypes)
+            {
+                _flags = flags;
+                _runtimeFunctionNameSuffix = runtimeFunctionNameSuffix;
+                ScopeInfo = new FunctionScopeInfo(this, supportsAsyncLambdas: _flags.HasFlag(FunctionFlags.SupportsAsyncLambdas));
+            }
+
+            public override IEnumerable<TexlStrings.StringGetter[]> GetSignatures()
+            {
+                yield break;
+            }
+
+            public override string GetUniqueTexlRuntimeName(bool isPrefetching = false)
+            {
+                return base.GetUniqueTexlRuntimeName() + _runtimeFunctionNameSuffix;
+            }
+        }
+
+        [Flags]
+        public enum FunctionFlags : uint
+        {
+            None = 0x0,
+            IsAsync = 0x1,
+            IsBehaviorOnly = 0x2,
+            IsStateless = 0x4,
+            IsSelfContained = 0x8,
+            ManipulatesCollections = 0x20,
+            AffectsCollectionSchemas = 0x40,
+            IsStrict = 0x80,
+            SupportsParamCoercion = 0x100,
+            SupportsAsyncLambdas = 0x200,
         }
     }
 }
