8cec5d8817afc00fc3d6e4321233775c4766752b
Joris de Gruyter <jorisde@microsoft.com>
2022-09-30 12:03:09 -0700

    remove regex using

--- src/libraries/Microsoft.PowerFx.Interpreter/Functions/LibraryUnary.cs
+++ src/libraries/Microsoft.PowerFx.Interpreter/Functions/LibraryUnary.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.PowerFx.Core.IR;
 using Microsoft.PowerFx.Core.IR.Nodes;
