846ae985104c74bdddb673739f1c561aec0bed7c
Carlos Figueira <carlosfigueirafilho@hotmail.com>
2022-10-26 12:28:48 -0700

    Updating tests and tabular result schema for Dec2Hex

--- src/libraries/Microsoft.PowerFx.Core/Texl/Builtins/Dec2Hex.cs
+++ src/libraries/Microsoft.PowerFx.Core/Texl/Builtins/Dec2Hex.cs
@@ -164,9 +164,7 @@ namespace Microsoft.PowerFx.Core.Texl.Builtins
                     // Ensure we have a one-column table of numerics
                     fValid &= CheckNumericColumnType(type0, args[0], errors, ref nodeToCoercedTypeMap);
 
-                    returnType = context.Features.HasFlag(Features.ConsistentOneColumnTableResult)
-                        ? DType.CreateTable(new TypedName(DType.Number, new DName(ColumnName_ValueStr)))
-                        : type0;
+                    returnType = DType.CreateTable(new TypedName(DType.String, new DName(ColumnName_ValueStr)));
 
                     // Check arg1 below.
                     otherArg = args[1];
@@ -177,9 +175,9 @@ namespace Microsoft.PowerFx.Core.Texl.Builtins
                     // Ensure we have a one-column table of numerics
                     fValid &= CheckNumericColumnType(type1, args[1], errors, ref nodeToCoercedTypeMap);
 
-                    // Since the 1st arg is not a table, make a new table return type *[Result:n] or
+                    // Since the 1st arg is not a table, make a new table return type
                     // *[Value:n] if the consistent return schema flag is enabled
-                    returnType = DType.CreateTable(new TypedName(DType.Number, GetOneColumnTableResultName(context.Features)));
+                    returnType = DType.CreateTable(new TypedName(DType.String, new DName(ColumnName_ValueStr)));
 
                     // Check arg0 below.
                     otherArg = args[0];
