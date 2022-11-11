3a2670658e8ff0b022e6f69639993c1fc775870b
jas-valgotar <jasvalgotar@microsoft.com>
2022-11-11 14:01:55 -0500

    Added a comment for new enum

--- src/libraries/Microsoft.PowerFx.Core/Syntax/IRPreProcessor.cs
+++ src/libraries/Microsoft.PowerFx.Core/Syntax/IRPreProcessor.cs
@@ -1,12 +1,14 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.PowerFx.Core.Functions;
 
 namespace Microsoft.PowerFx.Syntax
 {
+    /// <summary>
+    /// <see cref="TexlFunction"/> can override <see cref="TexlFunction.GetIRPreProcessors"/> to return one of these preprocessor 
+    /// that IR can leverage to add preprocessing for arguments of function call.
+    /// </summary>
     internal enum IRPreProcessor
     {
         None,
