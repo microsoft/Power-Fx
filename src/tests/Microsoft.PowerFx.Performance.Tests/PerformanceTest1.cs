490a9df16d1c6ec6731d95546156c73b635c4f2a
Luc Genetier <lucgen@microsoft.com>
2022-12-07 14:08:18 +0100

    Add Benchmarck.Net profiling project

---
+++ src/tests/Microsoft.PowerFx.Performance.Tests/PerformanceTest1.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System.Globalization;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnostics.Windows;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+using Microsoft.PowerFx.Core.Tests;
+using Microsoft.PowerFx.Types;
+
+namespace Microsoft.PowerFx.Performance.Tests
+{
+    [EtwProfiler] // https://benchmarkdotnet.org/articles/features/etwprofiler.html
+    [CsvExporter] // https://benchmarkdotnet.org/articles/configs/exporters.html
+    [MinColumn, Q1Column, MeanColumn, Q3Column, MaxColumn]
+    public class PerformanceTest1 : PowerFxTest
+    {
+        private PowerFxConfig powerFxConfig;
+        private Engine engine;
+        private RecalcEngine recalcEngine;
+        private ParserOptions parserOptions;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            powerFxConfig = new PowerFxConfig(new CultureInfo("en-US"), Features.All);
+            engine = new Engine(powerFxConfig);
+            parserOptions = new ParserOptions() { AllowsSideEffects = true, Culture = new CultureInfo("en-US") };
+            recalcEngine = new RecalcEngine(powerFxConfig);
+        }
+
+        [Params(1, 2, 5, 10, 20, 50, 100)]
+        public int N { get; set; }
+
+        [Benchmark]
+        public ParseResult Parse()
+        {
+            var expr = string.Join(" + ", Enumerable.Repeat("Sum(1)", N));
+
+            var parse = engine.Parse(expr, parserOptions);
+            return parse;
+        }
+
+        [Benchmark]
+        public CheckResult Check()
+        {
+            var expr = string.Join(" + ", Enumerable.Repeat("Sum(1)", N));
+
+            var check = engine.Check(expr);
+            return check;
+        }
+
+        [Benchmark]
+        public FormulaValue Eval()
+        {
+            var expr = string.Join(" + ", Enumerable.Repeat("Sum(1)", N));
+
+            var result = recalcEngine.Eval(expr);
+            return result;
+        }
+    }
+}
