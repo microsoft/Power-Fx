d033e0e7a9f3b7eeb88646eb22106dfb5e2de2b6
Luc Genetier <69138830+LucGenetier@users.noreply.github.com>
2022-12-09 22:44:44 +0100

    Add Benchmarck.Net profiling project (#887)
    Add Benchmarck.Net profiling project to measure performance of key
    PowerFx APIs.
    This console app needs to be compiled in release mode and will produce
    results like the example below.
    Notice that these results will depend on the machine used.
    
    | Method | N | Mean | Error | StdDev | Min | Q1 | Q3 | Max |
    |------- |----
    |-------------:|-----------:|-----------:|-------------:|-------------:|-------------:|-------------:|
    | Parse | 1 | 5.634 us | 0.0343 us | 0.0304 us | 5.579 us | 5.625 us |
    5.652 us | 5.677 us |
    | Check | 1 | 56.129 us | 1.0138 us | 0.8465 us | 55.568 us | 55.673 us
    | 56.153 us | 58.825 us |
    | Eval | 1 | 57.456 us | 0.3150 us | 0.2947 us | 56.932 us | 57.324 us |
    57.697 us | 57.764 us |
    | Parse | 2 | 12.029 us | 0.0651 us | 0.0577 us | 11.950 us | 11.996 us
    | 12.065 us | 12.159 us |
    | Check | 2 | 114.498 us | 1.0578 us | 0.9377 us | 112.682 us | 113.873
    us | 115.152 us | 116.451 us |
    | Eval | 2 | 112.719 us | 0.9399 us | 0.8792 us | 111.661 us | 111.926
    us | 113.149 us | 114.603 us |
    | Parse | 5 | 30.931 us | 0.5996 us | 0.8208 us | 29.922 us | 30.253 us
    | 31.317 us | 32.828 us |
    | Check | 5 | 283.300 us | 2.9594 us | 2.7682 us | 279.459 us | 281.388
    us | 284.453 us | 288.856 us |
    | Eval | 5 | 270.327 us | 2.9469 us | 2.6123 us | 265.346 us | 268.750
    us | 271.924 us | 275.122 us |
    | Parse | 10 | 59.969 us | 0.2200 us | 0.1951 us | 59.685 us | 59.838 us
    | 60.144 us | 60.262 us |
    | Check | 10 | 553.311 us | 3.4061 us | 3.0194 us | 546.914 us | 551.101
    us | 554.823 us | 558.592 us |
    | Eval | 10 | 527.913 us | 2.8641 us | 2.5390 us | 523.217 us | 526.081
    us | 529.657 us | 532.359 us |
    | Parse | 20 | 126.709 us | 2.5166 us | 5.5239 us | 119.027 us | 120.950
    us | 131.278 us | 136.713 us |
    | Check | 20 | 1,182.082 us | 23.3672 us | 37.0628 us | 1,121.656 us |
    1,151.371 us | 1,213.736 us | 1,245.586 us |
    | Eval | 20 | 1,023.810 us | 7.3008 us | 6.8292 us | 1,016.347 us |
    1,017.929 us | 1,028.714 us | 1,037.119 us |
    | Parse | 50 | 304.723 us | 2.3317 us | 2.0670 us | 301.125 us | 303.259
    us | 306.117 us | 307.844 us |
    | Check | 50 | 2,776.818 us | 6.5057 us | 6.0854 us | 2,767.250 us |
    2,772.776 us | 2,781.323 us | 2,786.738 us |
    | Eval | 50 | 2,688.011 us | 52.6876 us | 86.5673 us | 2,578.290 us |
    2,609.024 us | 2,761.661 us | 2,836.030 us |
    | Parse | 100 | 628.569 us | 3.7187 us | 3.2965 us | 623.051 us |
    626.312 us | 630.026 us | 635.731 us |
    | Check | 100 | 5,537.963 us | 19.3509 us | 17.1540 us | 5,514.151 us |
    5,525.402 us | 5,549.316 us | 5,577.848 us |
    | Eval | 100 | 5,135.896 us | 60.8737 us | 56.9413 us | 5,056.144 us |
    5,097.930 us | 5,167.779 us | 5,263.991 us |

---
+++ src/tests/Microsoft.PowerFx.Performance.Tests/Program.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using BenchmarkDotNet.Running;
+
+namespace Microsoft.PowerFx.Performance.Tests
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 
+             *       --> This program needs to be run ELEVATED
+             *       
+             *       --> Also set the following env. variable before starting VS
+             *           SET MSBuildSDKsPath=C:\Program Files\dotnet\sdk\7.0.100\Sdks
+             *              
+             */
+
+            var summary = BenchmarkRunner.Run<PerformanceTest1>();
+        }
+    }
+}
