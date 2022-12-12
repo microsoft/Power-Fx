97a8aa40e256be653873dc8969ac9435257be449
Mike Stall <jmstall@microsoft.com>
2022-12-12 13:42:10 -0800

    start RuntimeConfig

---
+++ src/libraries/Microsoft.PowerFx.Interpreter/Environment/RuntimeConfig.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.PowerFx.Core.Binding;
+using Microsoft.PowerFx.Types;
+
+namespace Microsoft.PowerFx
+{
+    /// <summary>
+    /// Runtime configuration for the execution of an expression.
+    /// </summary>
+    public class RuntimeConfig
+    {
+        /// <summary>
+        /// This should match the SymbolTable provided at bind time. 
+        /// </summary>
+        public ReadOnlySymbolValues Values { get; set; }
+
+        public IServiceProvider Services { get; set; }
+
+        // Other services:
+        // CultureInfo, Timezone, Clock, 
+        // Stack depth 
+        // Max memory 
+        // Logging
+    }
+}
