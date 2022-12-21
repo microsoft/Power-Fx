271b0c18fe18930ba4652c1ff6cd31fe81bdeb5a
Luc Genetier <lucgen@microsoft.com>
2022-12-21 16:02:09 +0100

    update

--- .scripts/ReadApiCompat.ps1
+++ .scripts/ReadApiCompat.ps1
@@ -1,1 +1,1 @@
-[string]::Join(", ", (([xml](Get-Content .\suppress.xml)).Suppressions.Suppression | % { $_.DiagnosticId + ": " + $_.Target })) 
+[string]::Join(", ", (([xml](Get-Content .\suppress.xml)).Suppressions.Suppression | % { $_.DiagnosticId + ": " + $_.Target })) > supp.txt
