c9ace76513ee9304ea9dfa9f0faaf72d1a747245
Luc Genetier <lucgen@microsoft.com>
2022-12-21 14:57:04 +0100

    update

--- src/libraries/Microsoft.PowerFx.Transport.Attributes/TransportMethodAttribute.cs
+++ src/libraries/Microsoft.PowerFx.Transport.Attributes/TransportMethodAttribute.cs
@@ -11,7 +11,7 @@ namespace Microsoft.AppMagic.Transport
     [AttributeUsage(AttributeTargets.Method)]
     public class TransportMethodAttribute : Attribute
     {
-        public TransportMethodAttribute(bool supportSynchronousClient = false, TransportBatchingMode batchingMode = TransportBatchingMode.Default, bool isParallel = false, bool xxx = true)
+        public TransportMethodAttribute(bool supportSynchronousClient = false, TransportBatchingMode batchingMode = TransportBatchingMode.Default, bool isParallel = false, bool zzz = true)
         {
             SupportSynchronousClient = supportSynchronousClient;
             BatchingMode = batchingMode;
