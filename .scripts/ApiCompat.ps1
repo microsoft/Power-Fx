5dd125d2053048fe421bae7bde003eaecb023e1d
Luc Genetier <lucgen@microsoft.com>
2022-12-22 15:46:35 +0100

    Update script

--- .scripts/ApiCompat.ps1
+++ .scripts/ApiCompat.ps1
@@ -9,7 +9,7 @@ $apiTool="tool\.store\microsoft.dotnet.apicompat.tool\7.0.100\microsoft.dotnet.a
 $apiToolRightFolder="Output"
 
 [xml]$xfiles = Get-Content .\ApiCompat.Files.xml
-$e = ""
+$e = [char]0x274C
 
 foreach ($a in $xfiles.ApiCompat.Assembly)
 {
@@ -25,17 +25,17 @@ foreach ($a in $xfiles.ApiCompat.Assembly)
     dotnet $apiTool --suppression-file $s --left-assembly $apiToolLeft --right-assembly $apiToolRight --enable-rule-attributes-must-match --enable-rule-cannot-change-parameter-name --generate-suppression-file
     
     [xml]$x = Get-Content $s
-    $e += [string]::Join(" --" + [char]0x274C, ($x.Suppressions.Suppression | % { $_.DiagnosticId + " " + $_.Target })).Trim()
+    $e += [string]::Join([char]0x274C, ($x.Suppressions.Suppression | % { $_.DiagnosticId + " " + $_.Target })).Trim()
 }
 
-Write-Host "##[error] $e"
 
-if ($e.Length -gt 1)
+if ($e.Length -gt 2)
 {
+    Write-Host "##[error] $e"
     Write-Host "##vso[task.setvariable variable=ApiCompat;issecret=false]Public API changes: $e"
 }
 else
 {
+    Write-Host "No public API change"
     Write-Host "##vso[task.setvariable variable=ApiCompat;issecret=false]No public API change."
 }
-
