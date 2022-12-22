8a0ae42f360afced47387f5d3e5e9d40e9d54854
Luc Genetier <lucgen@microsoft.com>
2022-12-22 16:05:19 +0100

    Update script

--- .scripts/ApiCompat.ps1
+++ .scripts/ApiCompat.ps1
@@ -1,15 +1,11 @@
 
-param(
-	[Parameter(Mandatory = $true)] [String]$BuildConfiguration
-)
-
-Write-Host "BuildConfiguration" $BuildConfiguration
+## $BuildConfiguration="Release"
 
 $apiTool="tool\.store\microsoft.dotnet.apicompat.tool\7.0.100\microsoft.dotnet.apicompat.tool\7.0.100\tools\net6.0\any\Microsoft.DotNet.ApiCompat.Tool.dll"        
 $apiToolRightFolder="Output"
 
 [xml]$xfiles = Get-Content .\ApiCompat.Files.xml
-$e = [char]0x274C
+$e = ""
 
 foreach ($a in $xfiles.ApiCompat.Assembly)
 {
@@ -21,21 +17,21 @@ foreach ($a in $xfiles.ApiCompat.Assembly)
     
     Set-Content $s "<Suppressions/>"
 
-    Write-Host "Checking" $n "..."
+    Write-Host "Checking" $n
     dotnet $apiTool --suppression-file $s --left-assembly $apiToolLeft --right-assembly $apiToolRight --enable-rule-attributes-must-match --enable-rule-cannot-change-parameter-name --generate-suppression-file
     
     [xml]$x = Get-Content $s
-    $e += [string]::Join([char]0x274C, ($x.Suppressions.Suppression | % { $_.DiagnosticId + " " + $_.Target })).Trim()
+    $e += [string]::Join("`r`n", ($x.Suppressions.Suppression | % { $_.DiagnosticId + " " + $_.Target })).Trim()
 }
 
+Write-Host "##[error] $e"
 
-if ($e.Length -gt 2)
+if ($e.Length -gt 1)
 {
-    Write-Host "##[error] $e"
     Write-Host "##vso[task.setvariable variable=ApiCompat;issecret=false]Public API changes: $e"
 }
 else
 {
-    Write-Host "No public API change"
-    Write-Host "##vso[task.setvariable variable=ApiCompat;issecret=false]No public API change."
+    $ok = [char]0x2705
+    Write-Host "##vso[task.setvariable variable=ApiCompat;issecret=false]$($ok) No public API change."
 }
