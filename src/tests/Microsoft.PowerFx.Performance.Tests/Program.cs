d367ddea7063220b7b78a12e62bc57e68897e834
Luc Genetier <lucgen@microsoft.com>
2023-01-03 14:58:15 +0100

    Update

--- src/tests/Microsoft.PowerFx.Performance.Tests/Program.cs
+++ src/tests/Microsoft.PowerFx.Performance.Tests/Program.cs
@@ -9,13 +9,15 @@ namespace Microsoft.PowerFx.Performance.Tests
     {
         public static void Main(string[] args)
         {
-            /*
-             * 
-             *       --> This program needs to be run ELEVATED
-             *       
-             *       --> Also set the following env. variable before starting VS
-             *           SET MSBuildSDKsPath=C:\Program Files\dotnet\sdk\7.0.100\Sdks
-             *              
+            /*                                                  
+                   1. Build in Release mode
+
+                   2. Then, execute the following commands (from cmd.exe, ELEVATED)
+                        dotnet new globaljson --sdk-version 3.1.426
+                        MSBuildSDKsPath=C:\Program Files\dotnet\sdk\3.1.426\sdks
+                        cd <repo root>\src
+                        "tests\Microsoft.PowerFx.Performance.Tests\bin\Release\netcoreapp3.1\Microsoft.PowerFx.Performance.Tests.exe"
+            
              */
 
             var summary = BenchmarkRunner.Run<PerformanceTest1>();
