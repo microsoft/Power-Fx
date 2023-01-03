c5bd8500f792257359efce7df981a94bbdd914d5
Luc Genetier <lucgen@microsoft.com>
2023-01-03 15:00:32 +0100

    Update

--- src/tests/Microsoft.PowerFx.Performance.Tests/Program.cs
+++ src/tests/Microsoft.PowerFx.Performance.Tests/Program.cs
@@ -12,12 +12,13 @@ namespace Microsoft.PowerFx.Performance.Tests
             /*                                                  
                    1. Build in Release mode
 
-                   2. Then, execute the following commands (from cmd.exe, ELEVATED)
+                   2. Then, execute the following commands (from cmd.exe, ELEVATED [required for ETW generation])
                         dotnet new globaljson --sdk-version 3.1.426
                         MSBuildSDKsPath=C:\Program Files\dotnet\sdk\3.1.426\sdks
                         cd <repo root>\src
                         "tests\Microsoft.PowerFx.Performance.Tests\bin\Release\netcoreapp3.1\Microsoft.PowerFx.Performance.Tests.exe"
             
+                     Notice that version 3.1.426 is obtained wiht "dotnet --list-sdks"
              */
 
             var summary = BenchmarkRunner.Run<PerformanceTest1>();
