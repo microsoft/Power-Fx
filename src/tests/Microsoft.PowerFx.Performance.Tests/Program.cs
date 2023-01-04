ccfab69c9ef4117606ecf9f24ac6b383dab58dcc
Luc Genetier <lucgen@microsoft.com>
2023-01-04 11:39:26 +0100

    Updates

--- src/tests/Microsoft.PowerFx.Performance.Tests/Program.cs
+++ src/tests/Microsoft.PowerFx.Performance.Tests/Program.cs
@@ -12,16 +12,27 @@ namespace Microsoft.PowerFx.Performance.Tests
             /*                                                  
                    1. Build in Release mode
 
-                   2. Then, execute the following commands (from cmd.exe, ELEVATED [required for ETW generation])
-                        dotnet new globaljson --sdk-version 3.1.426
-                        MSBuildSDKsPath=C:\Program Files\dotnet\sdk\3.1.426\sdks
+                   2. Then, execute the following commands (from cmd.exe, ELEVATED [required for ETW generation])                       
+                        set MSBuildSDKsPath=C:\Program Files\dotnet\sdk\3.1.426\sdks
                         cd <repo root>\src
-                        "tests\Microsoft.PowerFx.Performance.Tests\bin\Release\netcoreapp3.1\Microsoft.PowerFx.Performance.Tests.exe"
+                        dotnet new globaljson --sdk-version 3.1.426
+                        "tests\Microsoft.PowerFx.Performance.Tests\bin\Release\netcoreapp3.1\Microsoft.PowerFx.Performance.Tests.exe" -f *
+                                
+                     Notice that version 3.1.426 or other versions are obtained with "dotnet --list-sdks"
+                     "dotnet new globaljson --sdk-version 3.1.426" shouldn't be needed
+                     
+                     Results are in 
+                       - <repo root>\src\BenchmarkDotNet.Artifacts
+                           Microsoft.PowerFx.Performance.Tests.<test class>-<date>-<time>.log
+                           Microsoft.PowerFx.Performance.Tests.<test class and name (+opt. N)>-<date>-<time>.etl
+                       - <repo root>\src\BenchmarkDotNet.Artifacts\results
+                           Microsoft.PowerFx.Performance.Tests.<test class>-report-github.md
+                           Microsoft.PowerFx.Performance.Tests.<test class>-report.html
+                           Microsoft.PowerFx.Performance.Tests.<test class>-report.csv
             
-                     Notice that version 3.1.426 is obtained wiht "dotnet --list-sdks"
              */
 
-            var summary = BenchmarkRunner.Run<PerformanceTest1>();
+            _ = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
         }
     }
 }
