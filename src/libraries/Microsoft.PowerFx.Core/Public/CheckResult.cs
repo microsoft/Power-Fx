microsoft/Power-Fx

commit 5845c5b16855a3693c9f9ac3ed65f941a629704e

CheckResult rewrite, add hooks for derived engine like SQL. (#993)
[1] Add hooks so that SqlEngine can be a real engine derived from
PowerFx.Engine. The benefits are:
- eliminates duplicated code around display name conversion and
intellisense.
- enabled SqlEngine to leverage CodeFixHandlers. 

[2] Also cleanup CheckResult. Better enforcement of invariants. Also
lets us avoid duplicate or unnecessary work; such as getting
intellisense competition suggestions (just need the _binding) without
having to compute dependency info (TopLevelIdentifiers).

[3] Enforce "Text too long" as a parser options.  SqlEngine used this.
Before, this check occurred after binding. It should occur immediately
before we even try to lex.

[4] Leverage the new work on CheckResult to remove some Obsolete and
some non-sensical APIs. For example, we shouldn't take a ParseResult and
ParseOptions, since the Result already has the options.

### New hooks on Engine
This required some new hooks for derived engines:
- ability to set rule scope to binder (SqlEngine used this to represent
the current record)
- Ability for an engine to consistently apply per-engine constraints
(also requested in #749). Sql engine has many restrictions (due to
generating as SQL) beyond just which functions are allowed. Add a
Engine.PostCheck() hook which gets invoked and allows engines to provide
additional errors.
- better granularity around CheckResult (see below). We need to
coordinate when the SQL visitor walks , and avoid double walking.

### Major refactor on CheckResult to be more granular.
Before - the class had organically grown and was unclear when certain
fields got filled out.
we have many different "stages" and each stage requires certain work.
But we don't want to repeat and duplicate the work.
For example: 

- Parsing requires input for text and ParserOptions. 
- Binding requires input for symbol info. And prereq is parsing.

src/libraries/Microsoft.PowerFx.Core/Public/CheckResult.cs
@@ -7,8 +7,10 @@ using System.Globalization;
 using System.Linq;
 using Microsoft.PowerFx.Core.Binding;
 using Microsoft.PowerFx.Core.Errors;
+using Microsoft.PowerFx.Core.IR;
 using Microsoft.PowerFx.Core.Localization;
 using Microsoft.PowerFx.Core.Public;
+using Microsoft.PowerFx.Core.Types;
 using Microsoft.PowerFx.Core.Utils;
 using Microsoft.PowerFx.Intellisense;
 using Microsoft.PowerFx.Syntax;
@@ -17,98 +19,240 @@ using Microsoft.PowerFx.Types;
 namespace Microsoft.PowerFx
 {
     /// <summary>
-    /// Result of binding an expression. 
+    /// Holds work such as parsing, binding, error checking done on a single expression. 
+    /// Different options require different work. 
+    /// Tracks which work is done so that it is not double repeated.
     /// </summary>
     public class CheckResult : IOperationStatus
     {
+        #region User Inputs
+        // Captured from user, set once via a Set*() function. 
+
+        /// <summary>
+        /// The source engine this was created against. 
+        /// This is critical for calling back to populate the rest of the results. 
+        /// </summary>
+        private readonly Engine _engine;
+       
+        // The raw expression test. 
+        private string _expression;
+        
+        private ParserOptions _parserOptions;
+
+        // Information for binding. 
+        private bool _setBindingCalled;
+        internal ReadOnlySymbolTable _symbols; // can be null
+        private VersionHash _symbolHash; // hash of _symbols at time of assignment
+
+        #endregion
+
+        [Obsolete("Use public constructor")]
+        internal CheckResult()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CheckResult"/> class.
+        /// </summary>
+        /// <param name="source">Engine used to handle Apply operations.</param>
+        public CheckResult(Engine source)
+        {
+            this._engine = source ?? throw new ArgumentNullException(nameof(source));
+        }
+
+        internal Engine Engine => _engine;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CheckResult"/> class.
+        /// Create a "failed" check around a set of errors. 
+        /// Can't do any other operations. 
+        /// </summary>
+        /// <param name="extraErrors"></param>
+        public CheckResult(IEnumerable<ExpressionError> extraErrors)
+        {
+            this._errors.AddRange(extraErrors);
+        }
+
+        #region Set info from User. 
+
+        public CheckResult SetText(ParseResult parse)
+        {
+            if (_engine == null)
+            {
+                throw new InvalidOperationException($"Can't call {nameof(SetText)} without an engine.");
+            }
+
+            if (parse == null)
+            {
+                throw new ArgumentNullException(nameof(parse));    
+            }
+
+            if (_expression != null)
+            {
+                throw new InvalidOperationException($"Can only call {nameof(SetText)} once.");
+            }
+
+            _expression = parse.Text;
+            _parserOptions = parse.Options;
+            this.Parse = parse;
+
+            return this;
+        }
+
+        public CheckResult SetText(string expression, ParserOptions parserOptions = null)
+        {
+            if (_engine == null)
+            {
+                throw new InvalidOperationException($"Can't call {nameof(SetText)} without an engine.");
+            }
+
+            expression = expression ?? throw new ArgumentNullException(nameof(expression));
+
+            if (_expression != null)
+            {
+                throw new InvalidOperationException($"Can only call {nameof(SetText)} once.");
+            }
+
+            _expression = expression;
+            _parserOptions = parserOptions ?? Engine.GetDefaultParserOptionsCopy();
+
+            return this;
+        }
+
+        // Symbols could be null if no additional symbols are provided. 
+        public CheckResult SetBindingInfo(ReadOnlySymbolTable symbols)
+        {
+            if (_engine == null)
+            {
+                throw new InvalidOperationException($"Can't call {nameof(SetText)} without an engine.");
+            }
+
+            if (_setBindingCalled)
+            {
+                throw new InvalidOperationException($"Can only call {nameof(SetBindingInfo)} once.");
+            }
+
+            _symbols = symbols;
+
+            if (_symbols != null)
+            {
+                _symbolHash = _symbols.VersionHash;
+            }
+
+            _setBindingCalled = true;
+            return this;
+        }
+
+        public CheckResult SetBindingInfo(RecordType parameterType)
+        {
+            ReadOnlySymbolTable symbolTable = null;
+            if (parameterType != null)
+            {
+                symbolTable = SymbolTable.NewFromRecord(parameterType);
+            }
+
+            return this.SetBindingInfo(symbolTable);
+        }
+
+        // No additional binding is required
+        public CheckResult SetBindingInfo()
+        {
+            return SetBindingInfo((RecordType)null);            
+        }
+        #endregion
+
+        #region Results from Parsing 
+
+        /// <summary>
+        /// Results from parsing. <see cref="ApplyParse"/>.
+        /// </summary>
+        public ParseResult Parse { get; private set; }
+        #endregion
+
+        #region Results from Binding
+
+        /// <summary>
+        /// Binding, computed from <see cref="ApplyBindingInternal"/>.
+        /// </summary>        
+        internal TexlBinding _binding;
+
         /// <summary> 
         /// Return type of the expression. Null if type can't be determined. 
         /// </summary>
         public FormulaType ReturnType { get; set; }
+
+        #endregion
+
+        #region Results from Dependencies
+
+        private HashSet<string> _topLevelIdentifiers;
 
         /// <summary>
         /// Names of fields that this formula uses. 
         /// null if unavailable.  
         /// This is only valid when <see cref="IsSuccess"/> is true.
         /// </summary>
-        public HashSet<string> TopLevelIdentifiers { get; set; }
+        public HashSet<string> TopLevelIdentifiers
+        {
+            get
+            {
+                if (_topLevelIdentifiers == null)
+                {
+                    throw new InvalidOperationException($"Call {nameof(ApplyDependencyAnalysis)} first.");
+                }
+
+                return _topLevelIdentifiers;
+            }
+        }
+
+        #endregion 
+
+        #region Results from Errors
+
+        // All errors accumulated. 
+        private readonly List<ExpressionError> _errors = new List<ExpressionError>();
+        #endregion
+
+        #region Results from IR 
+        private IRResult _irresult;
+
+        #endregion 
+
+        internal TexlBinding Binding
+        {
+            get
+            {
+                if (_binding == null)
+                {
+                    throw new InvalidOperationException($"Must call {nameof(ApplyBindingInternal)} before accessing binding.");
+                }
+
+                return _binding;
+            }
+        }
 
         /// <summary>
-        /// List of errors and warnings. Check <see cref="ExpressionError.IsWarning"/>.
+        /// List of all errors and warnings. Check <see cref="ExpressionError.IsWarning"/>.
+        /// This can include Parse, Bind, and per-engine custom errors (see <see cref="Engine.PostCheck(CheckResult)"/>, 
+        /// or any custom errors passes explicit to the ctor.
         /// Not null, but empty on success.
         /// </summary>
-        public IEnumerable<ExpressionError> Errors { get; set; }
-
-        private IEnumerable<ExpressionError> BindingErrors => ExpressionError.New(_binding?.ErrorContainer.GetErrors(), CultureInfo);
-
-        internal void SetErrors(IEnumerable<IDocumentError> errors)
+        public IEnumerable<ExpressionError> Errors
         {
-            Errors = ExpressionError.New(errors, CultureInfo);
+            get => _errors;
+
+            [Obsolete("use constructor to set errors")]
+            set => _errors.AddRange(value);
         }
 
         /// <summary>
-        /// Parsed expression for evaluation. 
-        /// Null on failure or if there is no evaluation. 
+        /// True if no errors for stages run so far. 
         /// </summary>
-        public IExpression Expression { get; set; }
+        public bool IsSuccess => !_errors.Any(x => !x.IsWarning);              
 
         /// <summary>
-        /// The source engine this was created against. 
+        /// Helper to throw if <see cref="IsSuccess"/> is false.
         /// </summary>
-        internal Engine Source { get; set; }
-
-        /// <summary>
-        /// True if no errors. 
-        /// </summary>
-        public bool IsSuccess => !Errors.Any(x => !x.IsWarning);
-
-        internal bool HasDeferredArgsWarning => Errors.Any(x => x.IsWarning && x.MessageKey.Equals(TexlStrings.WarnDeferredType.Key));
-
-        internal TexlBinding _binding;
-
-        /// <summary>
-        /// Results from parsing. 
-        /// </summary>
-        public ParseResult Parse { get; set; }
-
-        /// <summary>
-        /// Symbols passed to this binding. May be null. 
-        /// </summary>
-        public ReadOnlySymbolTable Symbols { get; set; }
-
-        /// <summary>
-        /// Parameters are the subset of symbols that must be passed in Eval() for each evaluation. 
-        /// This lets us associated the type in Check()  with the values in Eval().
-        /// </summary>
-        internal ReadOnlySymbolTable Parameters { get; set; }
-
-        /// <summary>
-        /// Culture info passed to this binding. May be null. 
-        /// </summary>
-        internal CultureInfo CultureInfo { get; set; }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CheckResult"/> class.
-        /// </summary>
-        public CheckResult()
-        {
-        }
-
-        internal CheckResult(ParseResult parse, TexlBinding binding = null) 
-            : this(parse, null, binding)
-        {
-        }
-
-        internal CheckResult(ParseResult parse, CultureInfo locale, TexlBinding binding = null)
-        {
-            Parse = parse ?? throw new ArgumentNullException(nameof(parse));
-            CultureInfo = locale;
-
-            _binding = binding;
-
-            Errors = Parse.Errors.Concat(BindingErrors);
-        }
-
         public void ThrowOnErrors()
         {
             if (!IsSuccess)
@@ -118,14 +262,185 @@ namespace Microsoft.PowerFx
             }
         }
 
+        internal bool HasDeferredArgsWarning => _errors.Any(x => x.IsWarning && x.MessageKey.Equals(TexlStrings.WarnDeferredType.Key));
+
         /// <summary>
-        /// Gets the type of a syntax node.
+        /// Full set of Symbols passed to this binding. 
+        /// Can include symbols from Config, Engine, and Parameters, 
+        /// May be null. 
+        /// </summary>
+        internal ReadOnlySymbolTable AllSymbols { get; private set; }
+
+        /// <summary>
+        /// Parameters are the subset of symbols that must be passed in Eval() for each evaluation. 
+        /// This lets us associated the type in Check()  with the values in Eval().
+        /// </summary>
+        internal ReadOnlySymbolTable Parameters => _symbols;
+
+        /// <summary>
+        /// Culture info passed to this binding. May be null. 
+        /// </summary>
+        internal CultureInfo CultureInfo => this.Engine.Config.CultureInfo;
+
+        internal void ThrowIfSymbolsChanged()
+        {
+            if (_symbols != null)
+            {
+                var endHash = _symbols.VersionHash;
+                if (_symbolHash != endHash)
+                {
+                    throw new InvalidOperationException($"SymbolTable was mutated during binding of {_expression}");
+                }
+            }
+        }
+
+        public ParseResult ApplyParse()
+        {
+            if (_expression == null)
+            {
+                throw new InvalidOperationException($"Must call {nameof(SetText)} before calling ApplyParse().");
+            }
+
+            if (this.Parse == null)
+            {
+                var result = Engine.Parse(_expression, Engine.Config.Features, _parserOptions);
+                this.Parse = result;
+
+                _errors.AddRange(this.Parse.Errors);
+            }
+
+            return this.Parse;
+        }
+
+        internal Formula GetParseFormula()
+        {
+            var parseResult = this.ApplyParse();
+
+            var expression = parseResult.Text;
+            var culture = parseResult.Options.Culture;
+            var formula = new Formula(expression, culture);
+            formula.ApplyParse(parseResult);
+
+            return formula;
+        }
+
+        /// <summary>
+        /// Call to run binding. 
+        /// This will compute types on each node, enabling calling <see cref="GetNodeType(TexlNode)"/>.
+        /// </summary>
+        public void ApplyBinding()
+        {
+            ApplyBindingInternal();
+        }
+
+        // Apply and return the binding. 
+        internal TexlBinding ApplyBindingInternal()
+        {
+            if (_binding == null)
+            {
+                if (!this._setBindingCalled)
+                {
+                    throw new InvalidOperationException($"Must call {nameof(SetBindingInfo)} before calling {nameof(ApplyBinding)}.");
+                }
+
+                (var binding, var combinedSymbols) = Engine.ComputeBinding(this);
+
+                this.ThrowIfSymbolsChanged();
+                
+                // Don't modify any fields until after we've verified the symbols haven't change.
+
+                this._binding = binding;
+                this.AllSymbols = combinedSymbols;
+
+                // Add the errors
+                IEnumerable<ExpressionError> bindingErrors = ExpressionError.New(binding.ErrorContainer.GetErrors(), CultureInfo);
+                _errors.AddRange(bindingErrors);
+
+                if (this.IsSuccess && !this.HasDeferredArgsWarning)
+                {
+                    // TODO: Fix FormulaType.Build to not throw exceptions for Enum types then remove this check
+                    if (binding.ResultType.Kind != DKind.Enum)
+                    {
+                        this.ReturnType = FormulaType.Build(binding.ResultType);
+                    }
+                }
+            }
+
+            return _binding;
+        }
+
+        /// <summary>
+        /// Compute the dependencies. Called after binding. 
+        /// </summary>
+        public void ApplyDependencyAnalysis()
+        {
+            var binding = this.Binding; // will throw if binding wasn't run
+            this._topLevelIdentifiers = DependencyFinder.FindDependencies(binding.Top, binding);
+        }
+
+        // Flag to ensure Post Checks are only invoked once. 
+        private bool _invokingPostCheck;
+
+        /// <summary>
+        /// Calculate all errors. 
+        /// Invoke Binding and any engine-specific errors via <see cref="Engine.PostCheck(CheckResult)"/>. 
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<ExpressionError> ApplyErrors()
+        {
+            if (!_invokingPostCheck)
+            {
+                _invokingPostCheck = true;
+
+                // Errors require Binding, Parse 
+                var binding = ApplyBindingInternal();
+
+                // Plus engine's may have additional constaints. 
+                // PostCheck may refer to binding. 
+                var extraErrors = Engine.InvokePostCheck(this);
+
+                _errors.AddRange(extraErrors);
+            }
+            
+            return this.Errors;
+        }
+
+        internal IRResult ApplyIR()
+        {
+            if (_irresult == null)
+            {
+                // IR should not create any new errors. 
+                var binding = this.ApplyBindingInternal();
+                this.ThrowOnErrors();
+                (var irnode, var ruleScopeSymbol) = IRTranslator.Translate(binding);
+
+                _irresult = new IRResult
+                {
+                    TopNode = irnode,
+                    RuleScopeSymbol = ruleScopeSymbol
+                };                
+            }
+
+            return _irresult;
+        }
+
+        /// <summary>
+        /// Gets the type of a syntax node. Must call <see cref="ApplyBinding"/> first. 
         /// </summary>
         /// <param name="node"></param>
         /// <returns></returns>
-        public FormulaType GetNodeType(TexlNode node) => FormulaType.Build(_binding.GetType(node));
+        public FormulaType GetNodeType(TexlNode node) 
+        {
+            if (node == null)
+            {
+                throw new ArgumentNullException(nameof(node));
+            }
 
-        internal IReadOnlyDictionary<string, TokenResultType> GetTokens(GetTokensFlags flags) => GetTokensUtils.GetTokens(_binding, flags);
+            var type = this.Binding.GetType(node);
+            return FormulaType.Build(type);            
+        }
+
+        internal IReadOnlyDictionary<string, TokenResultType> GetTokens(GetTokensFlags flags) => GetTokensUtils.GetTokens(this.Binding, flags);
     }
 
     // Internal interface to ensure that Result objects have a common contract

commit c6f7b06fd60410d686a760a40f6ddcd86ab08bc3

CheckResult.Symbol needs to stay public  (#1031)
Regressed with CheckResult refactor in #993 . It was made private and
renamed to AllSymbols.

We have clients that require this property to stay public.

Co-authored-by: Mike Stall <jmstall@microsoft.com>

src/libraries/Microsoft.PowerFx.Core/Public/CheckResult.cs
@@ -264,12 +264,26 @@ namespace Microsoft.PowerFx
 
         internal bool HasDeferredArgsWarning => _errors.Any(x => x.IsWarning && x.MessageKey.Equals(TexlStrings.WarnDeferredType.Key));
 
+        internal ReadOnlySymbolTable AllSymbols { get; private set; }
+
         /// <summary>
         /// Full set of Symbols passed to this binding. 
         /// Can include symbols from Config, Engine, and Parameters, 
         /// May be null. 
+        /// Set after binding. 
         /// </summary>
-        internal ReadOnlySymbolTable AllSymbols { get; private set; }
+        public ReadOnlySymbolTable Symbols
+        {
+            get
+            {
+                if (_binding == null)
+                {
+                    throw new InvalidOperationException($"Must call {nameof(ApplyBinding)} before accessing combined Sybmols.");
+                }
+
+                return this.AllSymbols;
+            }
+        }
 
         /// <summary>
         /// Parameters are the subset of symbols that must be passed in Eval() for each evaluation. 

src/tests/Microsoft.PowerFx.Core.Tests/CheckResultTests.cs
@@ -253,6 +253,53 @@ namespace Microsoft.PowerFx.Core.Tests
             Assert.Equal(FormulaType.Number, check.GetNodeType(check.Parse.Root));
         }
 
+        [Fact]
+        public void BindingSymbols()
+        {
+            // Test Symbol property. 
+            var config = new PowerFxConfig();
+            config.SymbolTable.AddVariable("Global1", FormulaType.Number);
+
+            var localSymbols = new SymbolTable { DebugName = "Locals" };
+            localSymbols.AddVariable("Local1", FormulaType.Number);
+
+            var check = new CheckResult(new Engine(config));
+
+            check.SetText("Global1 + Local1 +"); // has error.
+            check.SetBindingInfo(localSymbols);
+
+            Assert.Throws<InvalidOperationException>(() => check.Symbols);
+
+            check.ApplyBinding();
+            Assert.False(check.IsSuccess); // Still have symbols even on binding errors. 
+            
+            // Validate symbol table.
+            var allSymbols = check.Symbols;
+            Assert.NotNull(allSymbols);
+
+            var ok = allSymbols.TryLookupSlot("Global1", out var slotGlobal);
+            Assert.True(ok);
+            Assert.Same(config.SymbolTable, slotGlobal.Owner);
+
+            ok = allSymbols.TryLookupSlot("Local1", out var slotLocal);
+            Assert.True(ok);
+            Assert.Same(localSymbols, slotLocal.Owner);            
+        }
+
+        // Still have Symbols even if we thing it's empty 
+        [Fact]
+        public void BindingSymbolsEmpty()
+        {
+            var check = new CheckResult(new Engine());
+            check.SetText("1+2");
+            check.SetBindingInfo();
+
+            check.ApplyBinding();
+
+            var allSymbols = check.Symbols;
+            Assert.NotNull(allSymbols);
+        }
+
         [Fact] 
         public void TestIR()
         {