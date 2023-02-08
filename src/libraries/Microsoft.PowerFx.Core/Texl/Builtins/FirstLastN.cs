200502551204782180afbfcb686c09dbaf6078be
bryancroteau-MSFT <84751645+bryancroteau-MSFT@users.noreply.github.com>
2023-02-08 15:37:53 -0800

    Implement First/Last and FirstN/LastN for UntypedObject (#1068)
    First/Last for UO returns a UO containing the single item. FirstN/LastN
    returns a UO containing an array, even if only one item is requested.
    Should have similar Blank/Error handling semantics to the classic
    First(N)/Last(N) functions.

--- src/libraries/Microsoft.PowerFx.Core/Texl/Builtins/FirstLastN.cs
+++ src/libraries/Microsoft.PowerFx.Core/Texl/Builtins/FirstLastN.cs
@@ -66,5 +66,34 @@ namespace Microsoft.PowerFx.Core.Texl.Builtins
             return fArgsValid;
         }
     }
+
+    // FirstN(source:*, [count:n])
+    // LastN(source:*, [count:n])
+    internal sealed class FirstLastNFunction_UO : BuiltinFunction
+    {
+        public override bool IsSelfContained => true;
+
+        // Note this function does not inherit from FunctionWithTableInput so there cannot be a common
+        // base class with the above function
+        public FirstLastNFunction_UO(bool isFirst)
+            : base(isFirst ? "FirstN" : "LastN", isFirst ? TexlStrings.AboutFirstN : TexlStrings.AboutLastN, FunctionCategories.Table, DType.UntypedObject, 0, 2, 2, DType.UntypedObject, DType.Number)
+        {
+        }
+
+        public override bool SupportCoercionForArg(int argIndex)
+        {
+            return argIndex == 1;
+        }
+
+        public override IEnumerable<TexlStrings.StringGetter[]> GetSignatures()
+        {
+            yield return new[] { TexlStrings.FirstLastNArg1, TexlStrings.FirstLastNArg2 };
+        }
+
+        public override string GetUniqueTexlRuntimeName(bool isPrefetching = false)
+        {
+            return GetUniqueTexlRuntimeName(suffix: "_UO");
+        }
+    }
 }
 #pragma warning restore SA1649 // File name should match first type name
