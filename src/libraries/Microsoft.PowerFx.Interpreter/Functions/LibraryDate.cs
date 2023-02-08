e7b5b1adc6805eafc2dabaac4b34546b4dde96ca
Jas Valgotar <32079188+jas-valgotar@users.noreply.github.com>
2023-02-08 21:53:36 +0530

    Fixes the issue with TZ having minutes in utc offset. (#1029)
    Fixes the issue with TZ having minutes in utc offset.

--- src/libraries/Microsoft.PowerFx.Interpreter/Functions/LibraryDate.cs
+++ src/libraries/Microsoft.PowerFx.Interpreter/Functions/LibraryDate.cs
@@ -266,17 +266,15 @@ namespace Microsoft.PowerFx.Functions
                     return new NumberValue(irContext, years);
             }
 
-            // Convert to UTC to be accurate (apply DST if needed)
-
-            if (NeedToConvertToUtc(runner, start, timeUnit))
-            {
-                start = TimeZoneInfo.ConvertTimeToUtc(start, timeZoneInfo);
-            }
-
-            if (NeedToConvertToUtc(runner, end, timeUnit))
-            {
-                end = TimeZoneInfo.ConvertTimeToUtc(end, timeZoneInfo);
-            }
+            // This takes care of DST differences
+            // e.g. https://www.timeanddate.com/time/change/usa/seattle?year=2023
+            // start = DateTime(2023, 3, 12, 0, 0, 0) is in UTC-8
+            // end = DateTime(2023, 3, 12, 3, 0, 0) is in UTC-7
+            // startUTCOffset - endUTCOffset = -1
+            // so adding that utcOffset difference to the end(instead of converting both to UTC) will adjust the subtraction for DST
+            // while preserving hours, since cases having minutes offset can potentially change the hour.
+            var utcOffset = timeZoneInfo.GetUtcOffset(start) - timeZoneInfo.GetUtcOffset(end);
+            end += utcOffset;
 
             // The function DateDiff only returns a whole number of the units being subtracted, and the precision is given in the unit specified.
             switch (timeUnit)
