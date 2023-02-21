47f0b7d32977aa0cbb48c526668c38a4d70e908a
Luc Genetier <lucgen@microsoft.com>
2023-02-21 15:04:09 +0100

    update

--- src/libraries/Microsoft.PowerFx.Core/Public/Config/VersionHash.cs
+++ src/libraries/Microsoft.PowerFx.Core/Public/Config/VersionHash.cs
@@ -1,20 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-using System;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading;
-using Microsoft.PowerFx.Core;
-using Microsoft.PowerFx.Core.App;
-using Microsoft.PowerFx.Core.Binding;
-using Microsoft.PowerFx.Core.Binding.BindInfo;
-using Microsoft.PowerFx.Core.Entities;
-using Microsoft.PowerFx.Core.Functions;
-using Microsoft.PowerFx.Core.Types;
-using Microsoft.PowerFx.Core.Types.Enums;
-using Microsoft.PowerFx.Core.Utils;
-using Microsoft.PowerFx.Types;
 
 namespace Microsoft.PowerFx
 {
