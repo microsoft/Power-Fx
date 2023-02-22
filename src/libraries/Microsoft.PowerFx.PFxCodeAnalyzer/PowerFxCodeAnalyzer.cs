dd4400d00f1b1267ca9ac5d41c92c3e3b433a588
Hoa Nguyen <nguhoa@microsoft.com>
2023-02-22 10:02:43 -0800

    update

--- src/libraries/Microsoft.PowerFx.PFxCodeAnalyzer/PowerFxCodeAnalyzer.cs
+++ src/libraries/Microsoft.PowerFx.PFxCodeAnalyzer/PowerFxCodeAnalyzer.cs
@@ -18,8 +18,6 @@ namespace Microsoft.PowerFx.PFxCodeAnalyzer
     {
         public const string DiagnosticId = "PFxCodeAnalyzer";
 
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
         private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
