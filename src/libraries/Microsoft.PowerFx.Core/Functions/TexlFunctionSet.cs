f9afcc9d254eedfaa76e817a7410933a01117e83
Luc Genetier <lucgen@microsoft.com>
2023-02-23 08:44:29 +0100

    Remove Append method

--- src/libraries/Microsoft.PowerFx.Core/Functions/TexlFunctionSet.cs
+++ src/libraries/Microsoft.PowerFx.Core/Functions/TexlFunctionSet.cs
@@ -118,9 +118,7 @@ namespace Microsoft.PowerFx.Core.Functions
                     }
                 }
             }
-        }
-
-        internal TexlFunction Add(TexlFunction function) => Add(function);
+        }        
 
         internal TexlFunction Add(TexlFunction function)
         {
