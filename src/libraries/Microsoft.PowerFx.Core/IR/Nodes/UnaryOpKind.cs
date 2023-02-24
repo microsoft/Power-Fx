76508caefa6f2c4deece9d3eded53ced7071583d
Jas Valgotar <32079188+jas-valgotar@users.noreply.github.com>
2023-02-24 10:49:47 -0500

    Moves BlankToEmptyString arg preprocessing to IR Phase. (#1081)
    -Follow up to #1006 #1059 
    -This introduces a new UNaryOpKind `BlankToEmptyString`, since no
    current function satisfies the BlankToEmptyString behavior.
    -Note: Other backends must implement this new UNaryOpKind
    `BlankToEmptyString` for these functions to work.
    
    NOTE: In this PR 3 function are left out purposefully. 
    `Concatenate()` due to its frequency of use with `&` operator and
    because of that it may generate very long IR
    `StartsWith()` and `EndsWith()` due to complexity with support of these
    function in delegation.
    
    Corrsponding Dataverse PR:
    https://github.com/microsoft/Power-Fx-Dataverse/pull/81

--- src/libraries/Microsoft.PowerFx.Core/IR/Nodes/UnaryOpKind.cs
+++ src/libraries/Microsoft.PowerFx.Core/IR/Nodes/UnaryOpKind.cs
@@ -58,5 +58,13 @@ namespace Microsoft.PowerFx.Core.IR.Nodes
 
         BooleanToOptionSet,
         AggregateToDataEntity,
+
+        // Argument pre-processesor in IR Phase.
+
+        /// <summary>
+        /// Used for pre-processing function arguments from blank to empty string.
+        /// All Interpreter(backed) must implement this.
+        /// </summary>
+        BlankToEmptyString,
     }
 }
