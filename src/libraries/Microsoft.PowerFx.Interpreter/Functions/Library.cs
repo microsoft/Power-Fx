b77aba5437ccacb85a1d820ec57fec8ae4e3524b
Carlos Figueira <carlosff@microsoft.com>
2023-03-14 15:09:20 -0700

    Revert "Revert "Restrict IsEmpty arguments to tables only" (#1155)"
    This reverts commit 5ebaed055e5ea001e85cf42d5470b3404fa467b4.

--- src/libraries/Microsoft.PowerFx.Interpreter/Functions/Library.cs
+++ src/libraries/Microsoft.PowerFx.Interpreter/Functions/Library.cs
@@ -817,6 +817,17 @@ namespace Microsoft.PowerFx.Functions
                 NoErrorHandling(IsBlankOrError)
             },
             {
+                BuiltinFunctionsCore.IsEmpty,
+                StandardErrorHandling<FormulaValue>(
+                    BuiltinFunctionsCore.IsEmpty.Name,
+                    expandArguments: NoArgExpansion,
+                    replaceBlankValues: DoNotReplaceBlank,
+                    checkRuntimeTypes: DeferRuntimeTypeChecking,
+                    checkRuntimeValues: DeferRuntimeValueChecking,
+                    returnBehavior: ReturnBehavior.AlwaysEvaluateAndReturnResult,
+                    targetFunction: IsEmpty)
+            },
+            {
                 BuiltinFunctionsCore.IsError,
                 NoErrorHandling(IsError)
             },
@@ -1797,6 +1808,23 @@ namespace Microsoft.PowerFx.Functions
             }
 
             return false;
+        }
+
+        private static FormulaValue IsEmpty(IRContext irContext, FormulaValue[] args)
+        {
+            if (args[0] is BlankValue)
+            {
+                return new BooleanValue(irContext, true);
+            }
+
+            if (args[0] is TableValue tv)
+            {
+                return new BooleanValue(irContext, !tv.Rows.Any());
+            }
+            else
+            {
+                return CommonErrors.RuntimeTypeMismatch(irContext);
+            }
         }
 
         public static FormulaValue IsNumeric(EvalVisitor runner, EvalVisitorContext context, IRContext irContext, FormulaValue[] args)
