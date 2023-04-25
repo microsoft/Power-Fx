2096961f13056e9dca2a283c69cdf273a517b30c
Jas Valgotar <32079188+jas-valgotar@users.noreply.github.com>
2023-04-25 00:55:34 -0400

    Fixes IfError under V1 flag (#1387)
    - Followup to https://github.com/microsoft/Power-Fx/pull/1230
    - Fixes #1173

--- src/libraries/Microsoft.PowerFx.Interpreter/EvalVisitor.cs
+++ src/libraries/Microsoft.PowerFx.Interpreter/EvalVisitor.cs
@@ -290,7 +290,7 @@ namespace Microsoft.PowerFx
                             });
                     }
                     
-                    if (!(result.IRContext.ResultType == node.IRContext.ResultType || result is ErrorValue || result.IRContext.ResultType is BlankType || func is IfErrorFunction))
+                    if (!(result.IRContext.ResultType == node.IRContext.ResultType || result is ErrorValue || result.IRContext.ResultType is BlankType))
                     {
                         throw CommonExceptions.RuntimeMisMatch;
                     }
