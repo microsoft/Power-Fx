3ac1e2043628a71220b2f2c715c7fc9d6f768586
Mike Stall <jmstall@microsoft.com>
2023-05-19 14:09:14 -0700

    tests

--- src/tests/Microsoft.PowerFx.Interpreter.Tests/ThreadingTests.cs
+++ src/tests/Microsoft.PowerFx.Interpreter.Tests/ThreadingTests.cs
@@ -27,19 +27,43 @@ namespace Microsoft.PowerFx.Interpreter.Tests
         // $$$ Supersedes ImmutabilityTests. This is more aggressive (incldues private fields). 
         [Fact]
         public void CheckImmutableType()
-        {            
+        {
+            // https://github.com/microsoft/Power-Fx/issues/1519
+            // Add ThreadSafeImmutable and get these to pass. 
+            AnalyzeThreadSafety.VerifyThreadSafeImmutable(typeof(Core.IR.Nodes.IntermediateNode));
+            AnalyzeThreadSafety.VerifyThreadSafeImmutable(typeof(ReadOnlySymbolValues));
+            AnalyzeThreadSafety.VerifyThreadSafeImmutable(typeof(ComposedReadOnlySymbolValues));
+            AnalyzeThreadSafety.VerifyThreadSafeImmutable(typeof(ParsedExpression));
+
             var asm1 = typeof(RecalcEngine).Assembly;
             var asm = typeof(Types.FormulaType).Assembly;
 
             foreach (Type type in asm.GetTypes().Concat(asm1.GetTypes()))
             {
+                // includes base types 
                 var attr = type.GetCustomAttribute<ThreadSafeImmutableAttribute>();
                 if (attr == null)
                 {
                     continue;
                 }
-                                
+
+                // Common pattern is a writeable derived type (like Dict vs. IReadOnlyDict). 
+                var attrNotSafe = type.GetCustomAttribute<NotThreadSafeAttribute>(inherit: false);
+                if (attrNotSafe != null)
+                {
+                    attr = type.GetCustomAttribute<ThreadSafeImmutableAttribute>(inherit: false);
+                    if (attr != null)
+                    {
+                        Assert.True(false); // Class can't have both safe & unsafe together. 
+                    }
+
+                    continue;
+                }
+
                 bool ok = AnalyzeThreadSafety.VerifyThreadSafeImmutable(type);
+
+                // https://github.com/microsoft/Power-Fx/issues/1519
+                // Assert.True(ok);                
             }
         }
     }
