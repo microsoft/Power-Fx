6c4014b3710f94bafafe32a8b6137c0a6ec558b5
Akshar V Patel <aksharpatel@microsoft.com>
2023-05-31 22:12:52 -0400

    Adding ability to supply custom binding config to Engine.ComputeBinding() & Fixing Dup String Interp Tokens bug (#1531)
    This pr handles few things that are mentioned below:
    1) It adds a new virtual overload of `GetDefaultBindingConfig` in
    Engine.cs that takes in ParserOptions and optional rule scope and
    returns a binding config that is used by `Engine.ComputeBinding()`. This
    is done so that Power Apps can supply custom binding config for semantic
    tokenization. Power Apps never set
    `BindingConfig.UseThisRecordForRuleScope` to true but changes before
    this pr calculated the value of
    `BindingConfig.UseThisRecordForRuleScope` based on the value returned
    from `Engine.GetRuleScope()`. It would be true when GetRuleScope()
    returns a non null value. There are properties such as `OnErrors `on
    AppInfo control in PowerApps that returns a non null value for
    `GetRuleScope()`. In this case,` useThisRecordForRuleScope `would be
    true in` Engine.ComputeBinding() `but it should never be true when
    `Engine.ComputeBinding() `is run from PowerApps.
    
    2) This pr fixes a bug where duplicate string interpolation and island
    tokens were added when expression has nested string interpolation to the
    list of tokens returned from Tokenization.Tokenize. This pr fixes that
    logic and adds tests to validate it. A good example of this bug is an
    expression like `$"{$"{$"{$"{.12e4}"}"}"}"`. The number of tokens should
    be 17 but it is 41. Many of them are duplicate with same spans and token
    types.
    
    3) This pr also fixes one test that happened due to Visual Studio
    picking up a different line feed for C# multiline strings `@""` .
    Updates the test to avoid using multiline string. @gregli-msft is the
    only and first one so far to face this problem. Tests were failing for
    him. Confirmed with him that avoiding multiline string fixes the test.

---
+++ src/tests/Microsoft.PowerFx.Core.Tests/IntellisenseTests/TokenizationTests/NumDecLitTokenTestCases.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.PowerFx.Tests.IntellisenseTests;
+
+namespace Microsoft.PowerFx.Tests.IntellisenseTests
+{
+    internal class NumDecLitTokenTestCases
+    {
+        private static readonly IEnumerable<string> _numbers = new List<string> 
+        { 
+            "12",
+            "12.e3",
+            ".12",
+            "12.",
+            ".234",
+            "2e3",
+            "2e+2",
+            "2.e-3",
+            "2e-44",
+            ".12E-34",
+            "2.E-3",
+            ".0",
+            "000333",
+        };
+
+        public static IEnumerable<object[]> NumDecLiteralTestCasesAsObjects
+        {
+            get
+            {
+                var numLitTokens = _numbers.Select(number => TokenizationTestCase.Create(number, ExpectedToken.CreateNumLitToken(number.Length)));
+                var decLitTokens = _numbers.Select(number => TokenizationTestCase.Create(number, new ParserOptions { NumberIsFloat = false }, ExpectedToken.CreateDecLitToken(number.Length)));
+                return TokenizationTestCase.TestCasesAsObjectsArray(numLitTokens.Concat(decLitTokens));
+            }
+        }
+    }
+}
