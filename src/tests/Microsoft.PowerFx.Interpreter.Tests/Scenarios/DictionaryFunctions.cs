e42ac16238569db3d3d48dda2c59d90083d73b63
Mike Stall <jmstall@microsoft.com>
2023-06-02 16:07:27 -0700

    demo

---
+++ src/tests/Microsoft.PowerFx.Interpreter.Tests/Scenarios/DictionaryFunctions.cs
@@ -0,0 +1,94 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.PowerFx.Types;
+using Xunit;
+
+namespace Microsoft.PowerFx.Interpreter.Tests.Scenarios
+{
+#if false
+NewDict(); // returns a string/string dictionary, which acts like an opaque object. 
+
+GetValue(dict, key) // value or blank. returns string
+
+Add(dict, key, value) // no return 
+
+GetKeys(dict); // returns single column table of all keys in the dict
+
+#endif
+
+    public class DictionaryFunctionTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var dict = new Dictionary<string, string>
+            {
+                { "Key1", "Value1" },
+                { "Key2", "Value2" }
+            };
+
+            var config = new PowerFxConfig();
+            config.AddFunction(new XGetValueFunction());
+
+            var engine = new RecalcEngine(config);
+            engine.UpdateVariable("dict", new MyDictionaryValue(dict));
+
+            var result = engine.Eval("XGetValue(dict, \"Key1\") & 999").ToObject();
+            Assert.Equal("Value999", result);
+        }
+    }
+
+    // Wraps a dictionary. 
+    // Treated as an opaque type, only accessible via functions
+    public class MyDictionaryValue : RecordValue
+    {
+        public readonly Dictionary<string, string> _dict;
+
+        // We don't have a type-safe way to limit to this type. 
+        public static readonly RecordType ParamType = RecordType.Empty();
+
+        public MyDictionaryValue(Dictionary<string, string> dict)
+            : base(MyDictionaryValue.ParamType)
+        {
+            _dict = dict;
+        }
+
+        protected override bool TryGetField(FormulaType fieldType, string fieldName, out FormulaValue result)
+        {
+            result = null;
+            return false;
+        }
+    }
+
+    // We don't have a type-safe way to pass
+    public class XGetValueFunction : ReflectionFunction
+    {
+        public XGetValueFunction() 
+            : base("XGetValue", FormulaType.String, MyDictionaryValue.ParamType, FormulaType.String)
+        {
+        }
+
+        public StringValue Execute(MyDictionaryValue dict, StringValue key)
+        {
+            var val = dict._dict[key.Value];
+            return FormulaValue.New(val);
+        }
+    }
+
+    public class MyPoco
+    {
+        // Normal field, easy for marshaller. 
+        public int IntField { get; set; }
+
+        // Dynamic 
+        public Dictionary<string, string> MyProps { get; set; }
+    }
+
+    internal class DictionaryFunctions
+    {
+    }
+}
