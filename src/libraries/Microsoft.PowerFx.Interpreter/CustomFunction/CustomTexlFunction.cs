20f096b99638fe547794a6e976c77ad1e3dd639a
Hoa Nguyen <34080196+nguhoa@users.noreply.github.com>
2023-06-14 10:39:50 -0700

    Stronger enforcement for records in custom functions (#1555)
    Fix issue: https://github.com/microsoft/Power-Fx/issues/1490

--- src/libraries/Microsoft.PowerFx.Interpreter/CustomFunction/CustomTexlFunction.cs
+++ src/libraries/Microsoft.PowerFx.Interpreter/CustomFunction/CustomTexlFunction.cs
@@ -6,10 +6,14 @@ using System.Collections.Generic;
 using System.Numerics;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.PowerFx.Core.App.ErrorContainers;
+using Microsoft.PowerFx.Core.Errors;
 using Microsoft.PowerFx.Core.Functions;
 using Microsoft.PowerFx.Core.Localization;
 using Microsoft.PowerFx.Core.Types;
 using Microsoft.PowerFx.Core.Utils;
+using Microsoft.PowerFx.Functions;
+using Microsoft.PowerFx.Syntax;
 using Microsoft.PowerFx.Types;
 using static Microsoft.PowerFx.Core.Localization.TexlStrings;
 
@@ -24,17 +28,20 @@ namespace Microsoft.PowerFx
 
         internal BigInteger LamdaParamMask;
 
-        public CustomTexlFunction(string name, FormulaType returnType, params FormulaType[] paramTypes)
-            : this(name, returnType._type, Array.ConvertAll(paramTypes, x => x._type))
+        public override bool IsSelfContained => !_isBehavior;
+
+        private readonly bool _isBehavior;
+
+        public CustomTexlFunction(string name, FunctionCategories functionCategory, FormulaType returnType, params FormulaType[] paramTypes)
+            : this(name, functionCategory, returnType._type, Array.ConvertAll(paramTypes, x => x._type))
         {
         }
 
-        public CustomTexlFunction(string name, DType returnType, params DType[] paramTypes)
-            : base(DPath.Root, name, name, CustomFunctionUtility.SG("Custom func " + name), FunctionCategories.MathAndStat, returnType, 0, paramTypes.Length, paramTypes.Length, paramTypes)
+        public CustomTexlFunction(string name, FunctionCategories functionCategory, DType returnType, params DType[] paramTypes)
+            : base(DPath.Root, name, name, CustomFunctionUtility.SG("Custom func " + name), functionCategory, returnType, 0, paramTypes.Length, paramTypes.Length, paramTypes)
         {
+            _isBehavior = functionCategory == FunctionCategories.Behavior;
         }
-
-        public override bool IsSelfContained => true;
 
         public override IEnumerable<TexlStrings.StringGetter[]> GetSignatures()
         {
@@ -49,6 +56,33 @@ namespace Microsoft.PowerFx
         public override bool IsLazyEvalParam(int index)
         {
             return LamdaParamMask.TestBit(index);
+        }
+
+        public override bool CheckTypes(CheckTypesContext context, TexlNode[] args, DType[] argTypes, IErrorContainer errors, out DType returnType, out Dictionary<TexlNode, DType> nodeToCoercedTypeMap)
+        {
+            Contracts.AssertValue(args);
+            Contracts.AssertAllValues(args);
+            Contracts.AssertValue(argTypes);
+            Contracts.Assert(args.Length == argTypes.Length);
+            Contracts.AssertValue(errors);
+
+            var isValid = base.CheckTypes(context, args, argTypes, errors, out returnType, out nodeToCoercedTypeMap);
+
+            // Check if all record names of args exist against arg types and if its possible to coerce.
+            if (isValid)
+            {
+                for (var i = 0; i < args.Length; i++)
+                {
+                    DType curType = argTypes[i];
+
+                    if ((curType.IsRecord || curType.IsTable) && !curType.CheckAggregateNames(this.ParamTypes[i], args[i], errors, SupportCoercionForArg(i), context.Features.PowerFxV1CompatibilityRules))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return isValid;
         }
     }
 }
