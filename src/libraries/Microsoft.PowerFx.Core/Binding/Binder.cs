c21ba045167021f9e08a2aa21e8f78e537809584
McCall Saltzman <69215460+lesaltzm@users.noreply.github.com>
2023-06-27 15:18:01 -0700

    Ensure that the binding knows the name of the property or named formula it's on, if the name resolver had that information (#1614)
    In Canvas Apps, the binding has information about which property it
    corresponds to, if it's a control property. However, this isn't present
    for named formulas. Ensure that the property name or named formula name
    is added, along with the entity information that's already present.

--- src/libraries/Microsoft.PowerFx.Core/Binding/Binder.cs
+++ src/libraries/Microsoft.PowerFx.Core/Binding/Binder.cs
@@ -137,7 +137,10 @@ namespace Microsoft.PowerFx.Core.Binding
 
         public Features Features { get; }
 
-        // Property to which current rule is being bound to. It could be null in the absence of NameResolver.
+        // Property Name or NamedFormula Name to which current rule is being bound to. It could be null in the absence of NameResolver.
+        internal DName SinkName { get; }
+
+        // Property to which current rule is being bound to. It could be null in the absence of NameResolver, or if the current rule is a named formula.
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0025:Use expression body for properties", Justification = "n/a")]
         public IExternalControlProperty Property
         {
@@ -328,6 +331,7 @@ namespace Microsoft.PowerFx.Core.Binding
             {
                 EntityPath = resolver.CurrentEntityPath;
                 EntityName = resolver.CurrentEntity == null ? default : resolver.CurrentEntity.EntityName;
+                SinkName = resolver.CurrentProperty;
             }
 
             resolver?.TryGetCurrentControlProperty(out _property);
