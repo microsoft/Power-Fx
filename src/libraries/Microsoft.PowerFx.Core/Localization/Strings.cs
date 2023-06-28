902664d5f027982a97b566de9586b087128c3bb5
Manoj Lalavat <mlalavat@microsoft.com>
2023-06-28 15:38:36 -0700

    remove wanted change

--- src/libraries/Microsoft.PowerFx.Core/Localization/Strings.cs
+++ src/libraries/Microsoft.PowerFx.Core/Localization/Strings.cs
@@ -687,6 +687,5 @@ namespace Microsoft.PowerFx.Core.Localization
         public static ErrorResourceKey WrnDelagationOnlyPrimaryKeyField = new ErrorResourceKey("WrnDelagationOnlyPrimaryKeyField");
         public static ErrorResourceKey WrnDelagationRefersThisRecord = new ErrorResourceKey("WrnDelagationRefersThisRecord");
         public static ErrorResourceKey WrnDelagationBehaviorFunction = new ErrorResourceKey("WrnDelagationBehaviorFunction");
-        public static ErrorResourceKey OfflineSupportWarning = new ErrorResourceKey("OfflineSupportWarning");
     }
 }
