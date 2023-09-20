364861c1c90bb2b4df791d0f51d34cc9d18f8122
Luc Genetier <lucgen@microsoft.com>
2023-09-20 18:54:03 +0200

    Accept x-ms-keywords and x-ms-openai-data extensions

--- src/libraries/Microsoft.PowerFx.Connectors/OpenApiParser.cs
+++ src/libraries/Microsoft.PowerFx.Connectors/OpenApiParser.cs
@@ -115,6 +115,7 @@ namespace Microsoft.PowerFx.Connectors
 
                 // Undocumented but safe to ignore
                 infoExtensions.Remove("x-ms-connector-name");
+                infoExtensions.Remove("x-ms-keywords");                
 
                 if (infoExtensions.Any())
                 {
@@ -263,6 +264,7 @@ namespace Microsoft.PowerFx.Connectors
 
                 opExtensions.Remove("x-ms-test-value");
                 opExtensions.Remove(XMsUrlEncoding);
+                opExtensions.Remove("x-ms-openai-data");
 
                 // Not supported x-ms-no-generic-test - Present in https://github.com/microsoft/PowerPlatformConnectors but not documented
                 // Other not supported extensions:
