aa98a53332610ec41bcd8dd9548f183c3f2196dc
Anderson Silva <joyle@outlook.com>
2023-09-21 16:25:23 -0500

    Patch to check field types in advance and give a compilation error (#1858)
    Issue #1834.
    This change was also made with
    https://github.com/microsoft/Power-Fx/issues/1550 in mind.

--- src/libraries/Microsoft.PowerFx.Interpreter/Functions/Mutation/PatchFunction.cs
+++ src/libraries/Microsoft.PowerFx.Interpreter/Functions/Mutation/PatchFunction.cs
@@ -206,7 +206,7 @@ namespace Microsoft.PowerFx.Functions
                     continue;
                 }
 
-                // Checks if all record names exist against table type and if its possible to coerce.                
+                // Checks if all record names exist against table type and if its possible to coerce.
                 bool checkAggregateNames = curType.CheckAggregateNames(dataSourceType, args[i], errors, SupportsParamCoercion, context.Features.PowerFxV1CompatibilityRules);
 
                 isValid = isValid && checkAggregateNames;
