3dcc6bfd07d834d6c86778f141a2153da4ae5efe
jas-valgotar <jasvalgotar@microsoft.com>
2023-09-25 11:55:20 -0400

    Fixes sorting issue

--- src/refreshLocalNugetCache.ps1
+++ src/refreshLocalNugetCache.ps1
@@ -4,7 +4,7 @@ $localCache = $Env:USERPROFILE + "\.nuget\packages"
 Write-Host "Deleting Microsoft.PowerFx.*\0.3.0-local from nuget cache. This will cause the newly built packages to be downloaded."
 
 Get-ChildItem $localCache Microsoft.PowerFx.* | ForEach-Object { 
-    $path = ($_.FullName + "\0.3.0-local")
+    $path = ($_.FullName + "\1.3.0-local")
 
     if (Test-Path $path) {
       Remove-Item $path -Recurse 
