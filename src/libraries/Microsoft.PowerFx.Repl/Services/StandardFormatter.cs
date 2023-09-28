a13f846b84418ca4d5832dfb4931e659c08da38c
Greg Lindhorst <gregli@microsoft.com>
2023-09-27 22:46:28 -0700

    Updates

--- src/libraries/Microsoft.PowerFx.Repl/Services/StandardFormatter.cs
+++ src/libraries/Microsoft.PowerFx.Repl/Services/StandardFormatter.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.PowerFx.Types;
 
 namespace Microsoft.PowerFx.Repl.Services
