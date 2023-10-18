69f3284f07bdc0539bc47e5f067814e4d4bb290b
Jas Valgotar <32079188+jas-valgotar@users.noreply.github.com>
2023-10-18 18:26:02 -0400

    Fixes AsType() for interpreter (#1969)
    -Legacy analysis requires a hack that changes the return type to include
    ExpandInfo, this causes the runtime type mismatch in the interpreter.
    -Hence putting it behind a new flag

--- src/libraries/Microsoft.PowerFx.Interpreter/Functions/Library.cs
+++ src/libraries/Microsoft.PowerFx.Interpreter/Functions/Library.cs
@@ -137,7 +137,7 @@ namespace Microsoft.PowerFx.Functions
                     replaceBlankValues: DoNotReplaceBlank,
                     checkRuntimeTypes: ExactValueTypeOrBlank<FormulaValue>,
                     checkRuntimeValues: DeferRuntimeValueChecking,
-                    returnBehavior: ReturnBehavior.AlwaysEvaluateAndReturnResult,
+                    returnBehavior: ReturnBehavior.ReturnBlankIfAnyArgIsBlank,
                     targetFunction: AsType)
             },
             {
