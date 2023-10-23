dacd2f4b0fc787c676c9cf7129b09a8a0a4d3a4b
Luc Genetier <lucgen@microsoft.com>
2023-10-23 11:27:39 +0200

    Add test with customer and http invokers

--- src/tests/Microsoft.PowerFx.Connectors.Tests/CustomInvokerTests.cs
+++ src/tests/Microsoft.PowerFx.Connectors.Tests/CustomInvokerTests.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.PowerFx.Connectors.Execution;
@@ -42,6 +43,37 @@ namespace Microsoft.PowerFx.Connectors.Tests
             Assert.IsType<StringValue>(result);
             Assert.Equal("+33 799 999 999", (result as StringValue).Value);
         }
+
+        [Fact]
+        public async Task CustomInvokerTest2()
+        {
+            using var testConnector1 = new LoggingTestServer(@"Swagger\Office_365_Users.json");
+            using var testConnector2 = new LoggingTestServer(@"Swagger\MSNWeather.json");            
+            var apiDoc1 = testConnector1._apiDocument;
+            var apiDoc2 = testConnector2._apiDocument;
+            var config = new PowerFxConfig();
+            var logger = new ConsoleLogger(_output);
+            
+            using var httpClient2 = new HttpClient(testConnector2);
+            using var client2 = new PowerPlatformConnectorClient(apiDoc2, "839eace6-59ab-4243-97ec-a5b8fcc104e4", "shared-msnweather-8d08e763-937a-45bf-a2ea-c5ed-ecc70ca4", () => "AuthToken1", httpClient2) { SessionId = "MySessionId" };
+
+            config.AddActionConnector("Office365Users", apiDoc1, logger);
+            config.AddActionConnector("MSNWeather", apiDoc2, logger);
+            var engine = new RecalcEngine(config);
+
+            CustomInvokerRuntimeContext2 runtimeContext = new CustomInvokerRuntimeContext2();
+            runtimeContext.AddCustomInvoker("Office365Users", (cf) => new CustomFunctionInvoker(cf, runtimeContext));
+            runtimeContext.AddHttpInvoker("MSNWeather", client2);
+            RuntimeConfig runtimeConfig = new RuntimeConfig().AddRuntimeContext(runtimeContext);
+
+            testConnector1.SetResponseFromFile(@"Responses\Office365_UserProfileV2.json");
+            testConnector2.SetResponseFromFile(@"Responses\MSNWeather_Response.json");                        
+
+            var result = await engine.EvalAsync(@"Office365Users.UserProfileV2(""johndoe@microsoft.com"").mobilePhone & ""/"" & Text(MSNWeather.CurrentWeather(""Redmond"", ""Imperial"").responses.weather.current.temp)", CancellationToken.None, runtimeConfig: runtimeConfig).ConfigureAwait(false);
+
+            Assert.IsType<StringValue>(result);
+            Assert.Equal("+33 799 999 999/53", (result as StringValue).Value);
+        }
     }
 
     internal class CustomInvokerRuntimeContext : BaseRuntimeConnectorContext
@@ -63,6 +95,49 @@ namespace Microsoft.PowerFx.Connectors.Tests
             }
 
             throw new NotImplementedException("Unknown namespace");
+        }
+    }
+
+    internal class CustomInvokerRuntimeContext2 : BaseRuntimeConnectorContext
+    {
+        private readonly Dictionary<string, Func<ConnectorFunction, FunctionInvoker>> _customInvokers = new Dictionary<string, Func<ConnectorFunction, FunctionInvoker>>();
+        private readonly Dictionary<string, HttpMessageInvoker> _httpInvokers = new Dictionary<string, HttpMessageInvoker>();
+
+        public CustomInvokerRuntimeContext2()
+        {            
+        }
+
+        public void AddHttpInvoker(string @namespace, HttpMessageInvoker client)
+        {
+            _httpInvokers.Add(@namespace, client);
+        }
+
+        public void AddCustomInvoker(string @namespace, Func<ConnectorFunction, FunctionInvoker> getInvoker)
+        {
+            _customInvokers.Add(@namespace, getInvoker);
+        }
+
+        public override TimeZoneInfo TimeZoneInfo => TimeZoneInfo.Utc;
+
+        public override HttpMessageInvoker GetHttpInvoker(ConnectorFunction function)
+        {
+            if (_httpInvokers.TryGetValue(function.Namespace, out var client))
+            {
+                return client;
+            }
+
+            throw new NotImplementedException("Unknown namespace");
+        }
+
+        public override FunctionInvoker GetInvoker(ConnectorFunction function, bool returnRawResults = false)
+        {
+            if (_customInvokers.TryGetValue(function.Namespace, out var getInvoker))
+            {
+                return getInvoker(function);
+            }
+
+            // If not custom, probably HTTP invoker
+            return base.GetInvoker(function, returnRawResults);
         }
     }
 
