4024e197c5d533065a5ed0f70cc7b37dff621fab
Hoa Nguyen (from Dev Box) <nguhoa@microsoft.com>
2023-10-31 09:29:01 -0700

    remove uncomment

--- src/libraries/Microsoft.PowerFx.Core/Utils/TextFormatUtils.cs
+++ src/libraries/Microsoft.PowerFx.Core/Utils/TextFormatUtils.cs
@@ -8,7 +8,6 @@ using System.Globalization;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Xml;
-using Microsoft.CodeAnalysis;
 using Microsoft.PowerFx.Core.Types;
 
 namespace Microsoft.PowerFx.Core.Utils
