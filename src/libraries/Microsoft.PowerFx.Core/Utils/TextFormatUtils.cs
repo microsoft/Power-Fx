6e64994adb64c636f0c6e56f6fbf73bc3b3f7bb1
Hoa Nguyen <34080196+nguhoa@users.noreply.github.com>
2023-10-31 12:28:50 -0700

    Fix handling section for DateTime format in Text (#1992)
    Fix issue: https://github.com/microsoft/Power-Fx/issues/1922

--- src/libraries/Microsoft.PowerFx.Core/Utils/TextFormatUtils.cs
+++ src/libraries/Microsoft.PowerFx.Core/Utils/TextFormatUtils.cs
@@ -35,6 +35,11 @@ namespace Microsoft.PowerFx.Core.Utils
         public string FormatArg { get; set; }
 
         /// <summary>
+        /// Numeric/date time format string.
+        /// </summary>
+        public List<string> Sections { get; set; }
+
+        /// <summary>
         /// Type of date time format.
         /// </summary>
         public DateTimeFmtType DateTimeFmt { get; set; }
@@ -102,9 +107,12 @@ namespace Microsoft.PowerFx.Core.Utils
             bool hasNumericCharacters = false;
             int decimalPointIndex = -1;
             int sectionCount = 0;
+            int lastSectionIdx = -1;
             int mCount = 0;
             bool hasColonWithNum = false;
             List<int> commaIdxList = new List<int>();
+            textFormatArgs.Sections = new List<string>();
+
             for (int i = 0; i < formatStr.Length; i++)
             {
                 if (formatStr[i] == 'm' || formatStr[i] == 'M')
@@ -225,6 +233,9 @@ namespace Microsoft.PowerFx.Core.Utils
                     {
                         return false;
                     }
+
+                    textFormatArgs.Sections.Add(formatStr.Substring(lastSectionIdx + 1, i - lastSectionIdx - 1));
+                    lastSectionIdx = i;
                 }
                 else if (textFormatArgs.HasNumericFmt && formatStr[i] == ',' && i > 0)
                 {
@@ -269,7 +280,7 @@ namespace Microsoft.PowerFx.Core.Utils
                 }
                 else if (formatStr[i] == '\\')
                 {
-                    if (textFormatArgs.HasNumericFmt || (i < formatStr.Length - 1 && formatStr[i + 1] == '\"'))
+                    if ((textFormatArgs.HasNumericFmt && textFormatArgs.DateTimeFmt != DateTimeFmtType.GeneralDateTimeFormat) || (i < formatStr.Length - 1 && formatStr[i + 1] == '\"'))
                     {
                         // Skip next character if seeing escaping character \.
                         i++;
@@ -294,6 +305,11 @@ namespace Microsoft.PowerFx.Core.Utils
                         return false;
                     }
                 }
+            }
+
+            if (lastSectionIdx != -1 && lastSectionIdx < formatStr.Length - 1)
+            {
+                textFormatArgs.Sections.Add(formatStr.Substring(lastSectionIdx + 1, formatStr.Length - lastSectionIdx - 1));
             }
 
             // Each comma after the decimal point and numeric character divides the number by 1,000.
