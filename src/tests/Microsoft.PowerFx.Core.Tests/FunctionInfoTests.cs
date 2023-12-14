8b886c728b23859608dd489226842d318ee38fd5
Mike Stall <mikestall@hotmail.com>
2023-12-14 14:52:00 -0800

    fix

--- src/tests/Microsoft.PowerFx.Core.Tests/FunctionInfoTests.cs
+++ src/tests/Microsoft.PowerFx.Core.Tests/FunctionInfoTests.cs
@@ -17,13 +17,13 @@ namespace Microsoft.PowerFx.Core.Tests
             // Get any function info. 
             var engine = new Engine();
             var infos = engine.FunctionInfos.ToArray();
-            var infoAbs = infos.Where(info => info.Name == "Mid").First();
+            var infoMid = infos.Where(info => info.Name == "Mid").First();
 
-            Assert.Equal("Returns the characters from the middle of a text value, given a starting position and length.", infoAbs.Description);
-            Assert.Equal(2, infoAbs.MinArity);
-            Assert.Equal(3, infoAbs.MaxArity);
-            Assert.Equal("Mid", infoAbs.Name);
-            Assert.Equal("https://go.microsoft.com/fwlink/?LinkId=722347#m", infoAbs.HelpLink);
+            Assert.Equal("Returns the characters from the middle of a text value, given a starting position and length.", infoMid.Description);
+            Assert.Equal(2, infoMid.MinArity);
+            Assert.Equal(3, infoMid.MaxArity);
+            Assert.Equal("Mid", infoMid.Name);
+            Assert.Equal("https://go.microsoft.com/fwlink/?LinkId=722347#m", infoMid.HelpLink);
         }
     }
 }
