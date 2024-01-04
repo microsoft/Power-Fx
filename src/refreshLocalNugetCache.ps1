e8fa643c1184b89a4a87a7020c8fb8010f831010
Carlos Figueira <carlosff@microsoft.com>
2024-01-04 09:13:37 -0800

    Increase local NuGet version to a large number (#2130)
    As we increase our official NuGet version, the version from a local build should have a higher version number. This increases it to a very large number, which should be enough until our major version update.

--- src/refreshLocalNugetCache.ps1
+++ src/refreshLocalNugetCache.ps1
@@ -1,10 +1,10 @@
 ﻿
 $localCache = $Env:USERPROFILE + "\.nuget\packages"
 
-Write-Host "Deleting Microsoft.PowerFx.*\0.3.0-local from nuget cache. This will cause the newly built packages to be downloaded."
+Write-Host "Deleting Microsoft.PowerFx.*\1.99.0-local from nuget cache. This will cause the newly built packages to be downloaded."
 
 Get-ChildItem $localCache Microsoft.PowerFx.* | ForEach-Object { 
-    $path = ($_.FullName + "\1.3.0-local")
+    $path = ($_.FullName + "\1.99.0-local")
 
     if (Test-Path $path) {
       Remove-Item $path -Recurse 
