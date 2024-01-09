03ccecad948b590dac2a7a49a303b1040706019c
anderson-joyle <joyle@outlook.com>
2024-01-08 18:00:10 -0600

    Code fix.

--- src/libraries/Microsoft.PowerFx.Interpreter/Functions/Mutation/CollectFunction.cs
+++ src/libraries/Microsoft.PowerFx.Interpreter/Functions/Mutation/CollectFunction.cs
@@ -300,8 +300,6 @@ namespace Microsoft.PowerFx.Interpreter
             var tableValue = arg0 as TableValue;
             var recordValue = arg1 as RecordValue;
 
-            //var recordValueCopy = FormulaValue.NewRecordFromFields(recordValue.Fields);
-
             cancellationToken.ThrowIfCancellationRequested();
             var result = await tableValue.AppendAsync(recordValue, cancellationToken).ConfigureAwait(false);
 
