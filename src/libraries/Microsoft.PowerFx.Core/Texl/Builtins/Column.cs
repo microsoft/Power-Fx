a037fb8ea9208577bafa46743ffe1c7cb2c8562e
Carlos Figueira <carlosff@microsoft.com>
2024-01-16 19:57:28 -0800

    New functions to retrieve column names and values for untyped objects (#2100)
    We currently have support for untyped objects, but to access their fields they need to be known at build time. This makes the support really untyped - we can access the column names of untyped records and also retrieve a column value given its name as a string.

---
+++ src/libraries/Microsoft.PowerFx.Core/Texl/Builtins/Column.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System.Collections.Generic;
+using Microsoft.PowerFx.Core.Functions;
+using Microsoft.PowerFx.Core.Localization;
+using Microsoft.PowerFx.Core.Types;
+using Microsoft.PowerFx.Core.Utils;
+
+namespace Microsoft.PowerFx.Core.Texl.Builtins
+{
+    // Column(record:O, property_name:s): O
+    internal class ColumnFunction_UO : BuiltinFunction
+    {
+        public override bool IsSelfContained => true;
+
+        public override ArgPreprocessor GetArgPreprocessor(int index, int argCount)
+        {
+            if (index == 1)
+            {
+                // Replace blank with empty string
+                return base.GetGenericArgPreprocessor(index);
+            }
+
+            return base.GetArgPreprocessor(index, argCount);
+        }
+
+        private static readonly DType _returnType =
+            DType.CreateTable(new TypedName(DType.String, new DName(ColumnName_Value)));
+
+        public ColumnFunction_UO()
+            : base("Column", TexlStrings.AboutColumn, FunctionCategories.Information, DType.UntypedObject, 0, 2, 2, DType.UntypedObject, DType.String)
+        {
+        }
+
+        public override IEnumerable<TexlStrings.StringGetter[]> GetSignatures()
+        {
+            yield return new[] { TexlStrings.ColumnArg1, TexlStrings.ColumnArg2 };
+        }
+    }
+}
