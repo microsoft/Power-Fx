2205f793190c6b01d8ab62b56972c299743d1cf4
Hoa Nguyen (from Dev Box) <nguhoa@microsoft.com>
2024-01-22 11:01:46 -0800

    Create public pretty print to use in NL2FX

---
+++ src/libraries/Microsoft.PowerFx.Interpreter/PrettyPrint.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+using Microsoft.PowerFx.Core.IR;
+using Microsoft.PowerFx.Core.Parser;
+using Microsoft.PowerFx.Syntax;
+
+namespace Microsoft.PowerFx.Interpreter
+{
+    public class PrettyPrint
+    {
+        public static string GetPrettyPrint(string input)
+        {
+            var parseResult = TexlParser.ParseScript(input);
+            var node = parseResult.Root;
+            return TexlPretty.PrettyPrint(node);
+        }
+    }
+}
---
+++ src/tests/Microsoft.PowerFx.Interpreter.Tests/PrettyPrintTests.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Microsoft.PowerFx.Interpreter.Tests
+{
+    public class PrettyPrintTests
+    {
+        [Theory]
+        [InlineData("((1 + 2) + 3)", "1 + 2 + 3")]
+        [InlineData("(((1  +  2)) + 3)", "1 + 2 + 3")]
+        [InlineData("((1  +  (2)) + (3))", "1 + 2 + 3")]
+        [InlineData("((1  +  (2)) * 3) - (4 * 5)", "(1 + 2) * 3 + -(4 * 5)")]
+        public void TestPrettyPrint(string expr, string expectedExpr)
+        {
+            var actual = PrettyPrint.GetPrettyPrint(expr);
+
+            Assert.Equal(expectedExpr, actual);
+        }
+    }
+}
