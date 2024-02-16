72028eb824cd8b541b5324c097c884fe89e386e3
anderson-joyle <joyle@outlook.com>
2024-02-16 10:37:43 -0600

    Removing UDF change.

--- src/libraries/Microsoft.PowerFx.Core/Syntax/UserDefinitions.cs
+++ src/libraries/Microsoft.PowerFx.Core/Syntax/UserDefinitions.cs
@@ -96,7 +96,7 @@ namespace Microsoft.PowerFx.Syntax
             foreach (var udf in uDFs)
             {
                 var udfName = udf.Ident.Name;
-                if (_restrictedUDFNames.Contains(udfName) || texlFunctionSet.AnyWithName(udfName) || BuiltinFunctionsCore.IsKnownPublicFunction(udfName))
+                if (_restrictedUDFNames.Contains(udfName) || texlFunctionSet.AnyWithName(udfName) || BuiltinFunctionsCore._library.AnyWithName(udfName) || BuiltinFunctionsCore.OtherKnownFunctions.Contains(udfName))
                 {
                     errors.Add(new TexlError(udf.Ident, DocumentErrorSeverity.Severe, TexlStrings.ErrUDF_FunctionAlreadyDefined, udfName));
                     continue;
