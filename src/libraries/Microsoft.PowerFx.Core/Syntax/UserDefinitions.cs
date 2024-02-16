3d624b3dc38e903642aa87f1e286673e71c1134e
anderson-joyle <joyle@outlook.com>
2024-02-16 11:31:40 -0600

    Fix.

--- src/libraries/Microsoft.PowerFx.Core/Syntax/UserDefinitions.cs
+++ src/libraries/Microsoft.PowerFx.Core/Syntax/UserDefinitions.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Text;
 using Microsoft.PowerFx.Core.Errors;
 using Microsoft.PowerFx.Core.Functions;
 using Microsoft.PowerFx.Core.Localization;
