75fdffb014e25baa08c8ce3db4c85942e010f465
Mike Stall (from Dev Box) <mikestall@hotmail.com>
2024-02-19 14:29:15 -0800

    pr feedback

--- src/libraries/Microsoft.PowerFx.Core/Public/Reflection/FunctionInfoSignature.cs
+++ src/libraries/Microsoft.PowerFx.Core/Public/Reflection/FunctionInfoSignature.cs
@@ -6,9 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.Text;
-using Microsoft.PowerFx.Core.Functions;
 using Microsoft.PowerFx.Core.Localization;
-using Microsoft.PowerFx.Core.Types;
 
 namespace Microsoft.PowerFx
 {
