3d61d37b924bacbd1db7d2f26306e817d130358c
Akshar <aksharpatel@microsoft.com>
2024-04-16 15:53:46 -0400

    Heavily Refactor LSP (Still Backwards Compatible) and Make it Async and Customizable (#2308)
    This pr significantly refactors LSP SDK to truly make it async and bit
    customizable. This pr also reorganizes existing tests into a much
    modular structure while still keeping around the old tests as a proof
    that the changes in this pr are backwards compatible.
    
    The main goal of this PR is to make LSP async in such as a way that
    hosts can run expensive operations like nl2fx network calls
    asynchronously while making sure the operations that need to run
    synchronously acquire correct host specific locks.
    
    This pr accomplishes it goals by 
    1) Refactoring a monolith language server class into small handlers
    targeting only one specific method. Each handler is async by default and
    this allows us to not let LSP block threads inside its hosts
    2) LSP in this pr identifies critical sections which are delegated to
    hosts for their executions. Host provides an environment using which LSP
    can acquire appropriate locks and run those critical sections inside
    Host environment
    3) LSP, now being async, allows hosts to run Nl2Fx or Fx2Nl network
    calls in a non blocking way

---
+++ src/libraries/Microsoft.PowerFx.LanguageServerProtocol/Helpers/NotificationHelper.cs
@@ -0,0 +1,98 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+using System.Web;
+using Microsoft.PowerFx.Core.Public;
+using Microsoft.PowerFx.LanguageServerProtocol.Protocol;
+
+namespace Microsoft.PowerFx.LanguageServerProtocol
+{
+    /// <summary>
+    /// A helper class to help with creating and writing notifications.
+    /// </summary>
+    internal static class NotificationHelper
+    {
+        /// <summary>
+        /// Create PublishTokens notification and writes it to the builder. 
+        /// Note: This is a legacy notification, and should be replaced by semantic tokens in all hosts.
+        /// </summary>
+        /// <param name="builder">Language Server Output Builder.</param>
+        /// <param name="documentUri">Document Uri.</param>
+        /// <param name="result">Check Result.</param>
+        public static void WriteTokensNotification(this LanguageServerOutputBuilder builder, string documentUri, CheckResult result)
+        {
+            var notification = CreateTokensNotification(documentUri, result);
+            if (notification != null)
+            {
+                builder.AddNotification(CustomProtocolNames.PublishTokens, notification);
+            }
+        }
+
+        /// <summary>
+        /// Create PublishTokens notification. 
+        /// Note: This is a legacy notification, and should be replaced by semantic tokens in all hosts.
+        /// </summary>
+        /// <param name="documentUri">Document Uri.</param>
+        /// <param name="result">Check Result.</param>
+        /// <returns>PublishTokensParams.</returns>
+        public static PublishTokensParams CreateTokensNotification(string documentUri, CheckResult result)
+        {
+            var uri = new Uri(documentUri);
+            var nameValueCollection = HttpUtility.ParseQueryString(uri.Query);
+            if (!uint.TryParse(nameValueCollection.Get("getTokensFlags"), out var flags))
+            {
+                return null;
+            }
+
+            var tokens = result.GetTokens((GetTokensFlags)flags);
+            if (tokens == null || tokens.Count == 0)
+            {
+                return null;
+            }
+
+            return new PublishTokensParams()
+            {
+                Uri = documentUri,
+                Tokens = tokens
+            };
+        }
+
+        /// <summary>
+        /// Create PublishExpressionType notification and writes it to the builder.
+        /// </summary>
+        /// <param name="builder">Language Server Output Builder.</param>
+        /// <param name="documentUri">Document Uri.</param>
+        /// <param name="result">Check Result.</param>
+        public static void WriteExpressionTypeNotification(this LanguageServerOutputBuilder builder, string documentUri, CheckResult result)
+        {
+            var notification = CreateExpressionTypeNotification(documentUri, result);
+            if (notification != null)
+            {
+                builder.AddNotification(CustomProtocolNames.PublishExpressionType, notification);
+            }
+        }
+
+        /// <summary>
+        /// Create PublishExpressionType notification.
+        /// </summary>
+        /// <param name="documentUri">Document Uri.</param>
+        /// <param name="result">Check Result.</param>
+        /// <returns>Expression Type Notification.</returns>
+        public static PublishExpressionTypeParams CreateExpressionTypeNotification(string documentUri, CheckResult result)
+        {
+            var uri = new Uri(documentUri);
+            var nameValueCollection = HttpUtility.ParseQueryString(uri.Query);
+            if (!bool.TryParse(nameValueCollection.Get("getExpressionType"), out var enabled) || !enabled)
+            {
+                return null;
+            }
+
+            return new PublishExpressionTypeParams()
+            {
+                Uri = documentUri,
+                Type = result.ReturnType
+            };
+        }
+    }
+}
