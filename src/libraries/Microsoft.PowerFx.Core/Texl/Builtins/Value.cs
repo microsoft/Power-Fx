e701dd5d293dff5a906ab1a498862aba104be5cb
Carlos Figueira <carlosff@microsoft.com>
2024-04-23 06:30:37 -0700

    Update Decimal function to require Power Fx 1.0

--- src/libraries/Microsoft.PowerFx.Core/Texl/Builtins/Value.cs
+++ src/libraries/Microsoft.PowerFx.Core/Texl/Builtins/Value.cs
@@ -184,5 +184,20 @@ namespace Microsoft.PowerFx.Core.Texl.Builtins
             : base(DecimalFunction.DecimalInvariantFunctionName, TexlStrings.AboutDecimal, DType.Decimal)
         {
         }
+
+        public override void CheckSemantics(TexlBinding binding, TexlNode[] args, DType[] argTypes, IErrorContainer errors)
+        {
+            Contracts.AssertValue(args);
+            Contracts.AssertAllValues(args);
+            Contracts.AssertValue(argTypes);
+            Contracts.Assert(args.Length == argTypes.Length);
+            Contracts.AssertValue(errors);
+
+            if (!binding.Features.PowerFxV1CompatibilityRules)
+            {
+                errors.EnsureError(args[0], TexlStrings.ErrDecimalRequiresPowerFxV1);
+                return;
+            }
+        }
     }
 }
