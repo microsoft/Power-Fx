e8930e9d72c046841153e39587798224eba8e714
Akshar Patel (from Dev Box) <aksharpatel@microsoft.com>
2024-05-22 13:52:51 -0400

    add diag field

--- src/libraries/Microsoft.PowerFx.LanguageServerProtocol/Protocol/BaseNLParams.cs
+++ src/libraries/Microsoft.PowerFx.LanguageServerProtocol/Protocol/BaseNLParams.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+using System.Text.Json.Serialization;
+
 namespace Microsoft.PowerFx.LanguageServerProtocol.Protocol
 {
     public class BaseNLParams
@@ -9,5 +11,16 @@ namespace Microsoft.PowerFx.LanguageServerProtocol.Protocol
         /// Additional context for NL operation. Usually, a stringified JSON object.
         /// </summary>
         public string Context { get; set; } = null;
+    }
+
+    public class BaseNLResult
+    {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault | JsonIgnoreCondition.WhenWritingNull)]
+        public BaseNLDiagnosticInfo Diagnostics { get; set; } = null;
+    }
+
+    public class BaseNLDiagnosticInfo
+    {
+        public string ModelPrompt { get; set; }
     }
 }
--- src/tests/Microsoft.PowerFx.Interpreter.Tests/LanguageServiceProtocol/PublicSurfaceTests.cs
+++ src/tests/Microsoft.PowerFx.Interpreter.Tests/LanguageServiceProtocol/PublicSurfaceTests.cs
@@ -71,6 +71,8 @@ namespace Microsoft.PowerFx.Tests.LanguageServiceProtocol
                 "Microsoft.PowerFx.LanguageServerProtocol.Protocol.CustomGetCapabilitiesParams",
                 "Microsoft.PowerFx.LanguageServerProtocol.Protocol.CustomGetCapabilitiesResult",
                 "Microsoft.PowerFx.LanguageServerProtocol.Protocol.BaseNLParams",
+                "Microsoft.PowerFx.LanguageServerProtocol.Protocol.BaseNLResult",
+                "Microsoft.PowerFx.LanguageServerProtocol.Protocol.BaseNLDiagnosticInfo",
                 "Microsoft.PowerFx.LanguageServerProtocol.Protocol.CustomFx2NLParams",
                 "Microsoft.PowerFx.LanguageServerProtocol.Protocol.CustomFx2NLResult",
                 "Microsoft.PowerFx.LanguageServerProtocol.Protocol.CustomNL2FxParams",
