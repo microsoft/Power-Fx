10803908bbcef9c6f58585be50f388941754a147
Greg Lindhorst <gregli@microsoft.com>
2024-05-30 13:31:42 -0700

    Updates

--- src/libraries/Microsoft.PowerFx.Core/Public/Config/OptionSet.cs
+++ src/libraries/Microsoft.PowerFx.Core/Public/Config/OptionSet.cs
@@ -3,7 +3,6 @@
 
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Globalization;
 using System.Linq;
 using Microsoft.PowerFx.Core;
 using Microsoft.PowerFx.Core.Entities;
