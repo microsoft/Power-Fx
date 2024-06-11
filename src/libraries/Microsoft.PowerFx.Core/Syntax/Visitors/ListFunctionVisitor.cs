248af1fcfa4cc2a0c66174f0c14e41bb44d83e75
anderson-joyle <joyle@outlook.com>
2024-06-11 15:59:23 -0500

    Public API to get list of functions call

---
+++ src/libraries/Microsoft.PowerFx.Core/Syntax/Visitors/ListFunctionVisitor.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System.Collections.Generic;
+
+namespace Microsoft.PowerFx.Syntax
+{
+    public class ListFunctionVisitor : IdentityTexlVisitor
+    {
+        public HashSet<string> _functionNamespaces = new HashSet<string>();
+
+        // FullName --> Name 
+        // Use Fullname as key because it's unique. 
+        public Dictionary<string, string> _functionNames = new Dictionary<string, string>();
+
+        public static ListFunctionVisitor Run(CheckResult check)
+        {
+            return Run(check.Parse);
+        }
+
+        public static ListFunctionVisitor Run(string expression, ParserOptions options = null)
+        {
+            options ??= new ParserOptions();
+            var parse = options.Parse(expression);
+
+            return Run(parse);
+        }
+
+        public static ListFunctionVisitor Run(ParseResult parse)
+        {
+            var visitor = new ListFunctionVisitor();
+            parse.Root.Accept(visitor);
+            return visitor;
+        }
+
+        public override bool PreVisit(CallNode node)
+        {
+            var hasNamespace = node.Head.Namespace.Length > 0;
+
+            if (hasNamespace)
+            {
+                _functionNamespaces.Add(node.Head.Namespace.ToString());
+            }
+
+            var name = node.Head.Name;
+            var fullName = hasNamespace ?
+                    node.Head.Namespace + "." + name :
+                    name;
+
+            _functionNames[fullName] = name;
+
+            return base.PreVisit(node);
+        }
+    }
+}
---
+++ src/tests/Microsoft.PowerFx.Core.Tests.Shared/ListFunctionVisitorTests.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System.Linq;
+using Microsoft.PowerFx.Syntax;
+using Xunit;
+
+namespace Microsoft.PowerFx.Core.Tests.Shared
+{
+    public class ListFunctionVisitorTests : PowerFxTest
+    {
+        [Theory]
+        [InlineData("Abs(1)", "Abs")]
+        [InlineData("Abs(Abs(Abs(Abs(Abs(1)))))", "Abs")]
+        [InlineData("With({x:Today()}, x+1)", "With,Today")]
+        [InlineData("SomeNameSpace.Foo() + SomeNameSpace.Bar()", "SomeNameSpace.Foo,SomeNameSpace.Bar")]
+        [InlineData("true And true", "")]
+        [InlineData("If(true, Blank(),Error())", "If,Blank,Error")]
+        public void ListFuntionsFromParserTests(string expression, string expected)
+        {
+            foreach (var textFirst in new bool[] { false, true })
+            {
+                if (textFirst)
+                {
+                    expression = $"={expression}";
+                }
+
+                var options = new ParserOptions() { TextFirst = textFirst };
+
+                var engine = new Engine();
+                var parse = engine.Parse(expression, options);
+                var check = engine.Check(parse);
+
+                // Different overloads should produce the same result.
+                var functionsList1 = ListFunctionVisitor.Run(parse);
+                var functionsList2 = ListFunctionVisitor.Run(check);
+                var functionsList3 = ListFunctionVisitor.Run(expression, options);
+
+                var actualStr1 = string.Join(",", functionsList1._functionNames.Select(n => n.Key));
+                var actualStr2 = string.Join(",", functionsList2._functionNames.Select(n => n.Key));
+                var actualStr3 = string.Join(",", functionsList3._functionNames.Select(n => n.Key));
+
+                Assert.Equal(expected, actualStr1);
+                Assert.Equal(expected, actualStr2);
+                Assert.Equal(expected, actualStr3);
+            }
+        }
+    }
+}
