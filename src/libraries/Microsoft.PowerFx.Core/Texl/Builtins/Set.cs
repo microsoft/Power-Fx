8e822b46d86200c344ee18334ab6a89470975eb9
Hoa Nguyen (from Dev Box) <nguhoa@microsoft.com>
2024-07-10 08:08:33 -0700

    Add implementation of set to core

---
+++ src/libraries/Microsoft.PowerFx.Core/Texl/Builtins/Set.cs
@@ -0,0 +1,118 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.PowerFx.Core.App.ErrorContainers;
+using Microsoft.PowerFx.Core.Binding;
+using Microsoft.PowerFx.Core.Errors;
+using Microsoft.PowerFx.Core.Functions;
+using Microsoft.PowerFx.Core.Localization;
+using Microsoft.PowerFx.Core.Types;
+using Microsoft.PowerFx.Core.Utils;
+using Microsoft.PowerFx.Syntax;
+using static Microsoft.PowerFx.Core.Localization.TexlStrings;
+
+namespace Microsoft.PowerFx.Core.Texl.Builtins
+{
+    //   Set(var,newValue)
+    internal class SetFunction : TexlFunction
+    {
+        // Set() is a behavior function. 
+        public override bool IsSelfContained => false;
+
+        // Set() of a simple identifier is not a mutation through a reference (a mutate), but rather changing the reference (a true set).
+        public override bool MutatesArg(int argIndex, TexlNode arg) => argIndex == 0 && arg.Kind != NodeKind.FirstName;
+
+        public override IEnumerable<StringGetter[]> GetSignatures()
+        {
+            yield return new[] { TexlStrings.SetArg1, TexlStrings.SetArg2 };
+        }
+
+        public SetFunction()
+        : base(
+              DPath.Root,
+              "Set",
+              "Set",
+              TexlStrings.AboutSet,
+              FunctionCategories.Behavior,
+              DType.Unknown,
+              0, // no lambdas
+              2,
+              2)
+        {
+        }
+
+        public override bool CheckTypes(CheckTypesContext context, TexlNode[] args, DType[] argTypes, IErrorContainer errors, out DType returnType, out Dictionary<TexlNode, DType> nodeToCoercedTypeMap)
+        {
+            nodeToCoercedTypeMap = null;
+            returnType = context.Features.PowerFxV1CompatibilityRules ? DType.Void : DType.Boolean;
+
+            var isValid = CheckType(context, args[1], argTypes[1], argTypes[0], errors, ref nodeToCoercedTypeMap);
+
+            return isValid;
+        }
+
+        // 2nd argument should be same type as 1st argument. 
+        public override void CheckSemantics(TexlBinding binding, TexlNode[] args, DType[] argTypes, IErrorContainer errors)
+        {
+            base.CheckSemantics(binding, args, argTypes, errors);
+
+            Contracts.AssertValue(args);
+            Contracts.AssertAllValues(args);
+            Contracts.AssertValue(argTypes);
+            Contracts.AssertAllValid(argTypes);
+            Contracts.Assert(args.Length == argTypes.Length);
+            Contracts.AssertValue(errors);
+            Contracts.Assert(MinArity <= args.Length && args.Length <= MaxArity);
+
+            var arg0 = argTypes[0];
+            var arg1 = argTypes[1];
+
+            // Type check
+            if (!(arg0.Accepts(arg1, exact: true, useLegacyDateTimeAccepts: false, usePowerFxV1CompatibilityRules: binding.Features.PowerFxV1CompatibilityRules) ||
+                 (arg0.IsNumeric && arg1.IsNumeric)))
+            {
+                errors.EnsureError(DocumentErrorSeverity.Critical, args[1], ErrBadType_ExpectedType_ProvidedType, arg0.GetKindString(), arg1.GetKindString());
+                return;
+            }
+
+            if (arg1.AggregateHasExpandedType())
+            {
+                if (arg1.IsTable)
+                {
+                    errors.EnsureError(DocumentErrorSeverity.Critical, args[1], ErrSetVariableWithRelationshipNotAllowTable);
+                    return;
+                }
+
+                if (arg1.IsRecord)
+                {
+                    errors.EnsureError(DocumentErrorSeverity.Critical, args[1], ErrSetVariableWithRelationshipNotAllowRecord);
+                    return;
+                }
+            }
+
+            var firstName = args[0].AsFirstName();
+
+            if (firstName != null)
+            {
+                // Variable reference assignment, for example Set( x, 3 )
+                var info = binding.GetInfo(firstName);
+                if (info.Data is NameSymbol nameSymbol && nameSymbol.Props.CanSet)
+                {
+                    // We have a variable, success
+                    return;
+                }
+            }
+            else if (binding.Features.PowerFxV1CompatibilityRules)
+            {
+                // Deep mutation, for example Set( x.a, 4 )
+                base.ValidateArgumentIsSetMutable(binding, args[0], errors);
+                return;
+            }
+
+            errors.EnsureError(DocumentErrorSeverity.Severe, args[0], TexlStrings.ErrNeedValidVariableName_Arg, Name, args[0]);
+            return;
+        }
+    }
+}
