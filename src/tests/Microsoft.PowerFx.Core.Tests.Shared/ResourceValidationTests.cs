4f76eaaf79e46ce7a875bbf0112e6201007347df
anderson-joyle <joyle@outlook.com>
2024-08-07 13:45:35 -0500

    Fixes.

--- src/tests/Microsoft.PowerFx.Core.Tests.Shared/ResourceValidationTests.cs
+++ src/tests/Microsoft.PowerFx.Core.Tests.Shared/ResourceValidationTests.cs
@@ -158,14 +158,7 @@ namespace Microsoft.PowerFx.Tests
         [Fact]
         public void TestStringResourcesTryGetLocaleParam()
         {
-            StringResources.TryGet("AboutBooleanT_input",  out var paramDescription, "fr-FR");
-            Assert.NotEmpty(paramDescription);
-
-            StringResources.TryGet("AboutBooleanT_input", out paramDescription, string.Empty);
-            Assert.NotEmpty(paramDescription);
-
-            var test = StringResources.Get("AboutBooleanT_input", "fr-FR");
-            Assert.NotEmpty(test);
+            Assert.Throws<CultureNotFoundException>(() => StringResources.TryGet("AboutBooleanT_input", out _, "invalid-"));
         }
     }
 }
