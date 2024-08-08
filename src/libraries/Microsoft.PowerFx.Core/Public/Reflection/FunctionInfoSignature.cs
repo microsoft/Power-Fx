84c9f7243c948ff066c82f6495e0a94b416e927e
anderson-joyle <joyle@outlook.com>
2024-08-08 12:02:25 -0500

    Removing comment.

--- src/libraries/Microsoft.PowerFx.Core/Public/Reflection/FunctionInfoSignature.cs
+++ src/libraries/Microsoft.PowerFx.Core/Public/Reflection/FunctionInfoSignature.cs
@@ -58,8 +58,6 @@ namespace Microsoft.PowerFx
 
                 string invariantParamName = GetInvariantParameterName(p);
 
-                // We should allow passing in culture to get the help text. 
-                // https://github.com/microsoft/Power-Fx/issues/2216
                 _parent._fnc.TryGetParamDescription(invariantParamName, out var description, localeName);
 
                 result.Add(new ParameterInfoSignature
