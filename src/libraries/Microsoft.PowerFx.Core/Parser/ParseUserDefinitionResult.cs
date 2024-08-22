a4c293f7aacdf32c73989f8b6c0f737c596b737c
Rick Nguyen (from Dev Box) <ricknguyen@microsoft.com>
2024-08-22 09:54:15 -0700

    using

--- src/libraries/Microsoft.PowerFx.Core/Parser/ParseUserDefinitionResult.cs
+++ src/libraries/Microsoft.PowerFx.Core/Parser/ParseUserDefinitionResult.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.PowerFx.Core.Errors;
 using Microsoft.PowerFx.Syntax;
-using Microsoft.PowerFx.Syntax.SourceInformation;
 
 namespace Microsoft.PowerFx.Core.Parser
 {
