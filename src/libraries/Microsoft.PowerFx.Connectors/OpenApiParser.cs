4451f28648c7de9ed9c645d63658a6f0041dd8b9
Mark <mover@microsoft.com>
2024-08-23 13:28:09 -0700

    Fix stylecop error

--- src/libraries/Microsoft.PowerFx.Connectors/OpenApiParser.cs
+++ src/libraries/Microsoft.PowerFx.Connectors/OpenApiParser.cs
@@ -113,7 +113,7 @@ namespace Microsoft.PowerFx.Connectors
         {
             bool connectorIsSupported = true;
             string connectorNotSupportedReason = string.Empty;
-            List<ConnectorFunction> functions = new();
+            List<ConnectorFunction> functions = new ();
 
             if (connectorSettings == null)
             {
