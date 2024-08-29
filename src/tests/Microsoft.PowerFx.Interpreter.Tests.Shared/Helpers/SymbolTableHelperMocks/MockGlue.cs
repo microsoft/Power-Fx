d986ce34e0e5aa326d7dd001cdc5e079ca9dfe9e
Akshar Patel (from Dev Box) <aksharpatel@microsoft.com>
2024-08-29 15:42:29 -0400

    mark udf async

---
+++ src/tests/Microsoft.PowerFx.Interpreter.Tests.Shared/Helpers/SymbolTableHelperMocks/MockGlue.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using Microsoft.PowerFx.Core.App.Controls;
+using Microsoft.PowerFx.Core.Binding;
+using Microsoft.PowerFx.Core.Functions;
+using Microsoft.PowerFx.Core.Glue;
+
+internal class MockGlue : IBinderGlue
+{
+    public bool IsComponentDataSource(object lookupInfoData)
+    {
+        return false;
+    }
+
+    public bool IsDataComponentDefinition(object lookupInfoData)
+    {
+        return false;
+    }
+
+    public bool IsDataComponentInstance(object lookupInfoData)
+    {
+        return false;
+    }
+
+    public bool IsDynamicDataSourceInfo(object lookupInfoData)
+    {
+        return false;
+    }
+
+    bool IBinderGlue.CanControlBeUsedInComponentProperty(TexlBinding binding, IExternalControl control)
+    {
+        return true;
+    }
+
+    IExternalControl IBinderGlue.GetVariableScopedControlFromTexlBinding(TexlBinding txb)
+    {
+        return new DummyExternalControl();
+    }
+
+    bool IBinderGlue.IsComponentScopedPropertyFunction(TexlFunction infoFunction)
+    {
+        return false;
+    }
+
+    bool IBinderGlue.IsContextProperty(IExternalControlProperty externalControlProperty)
+    {
+        return false;
+    }
+
+    bool IBinderGlue.IsPrimaryCommandComponentProperty(IExternalControlProperty externalControlProperty)
+    {
+        return false;
+    }
+
+    bool IBinderGlue.TryGetCdsDataSourceByBind(object lhsInfoData, out IExternalControl o)
+    {
+        o = new DummyExternalControl();
+        return false;
+    }
+}
