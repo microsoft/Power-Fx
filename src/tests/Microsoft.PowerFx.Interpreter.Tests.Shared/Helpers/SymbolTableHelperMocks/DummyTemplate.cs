85e769e971ecafc44f0f0c958e5b7790fc4980a6
Akshar <aksharpatel@microsoft.com>
2024-08-30 20:29:59 -0400

    Mark UDFs and NFs Async If there's a reference to cross screen control (#2620)
    Marks UDF and Nf async if they refer to cross screen control and a new
    binding config to do that is enabled. There's no definite way to say
    where binder is binding nf or udf rules so config would be used to
    communicate the marking of top parse node as async upon countering those
    references. We don't need to mark those async if all they refer to is
    component definition but there's no definite way to also check that.
    ExternalTemplate.IsComponent returns true for component defs and
    instances but we need to check only for the former. However that
    shouldn't matter as code gen to lazy load component defs would be no op
    since component defs are loaded upfront.

---
+++ src/tests/Microsoft.PowerFx.Interpreter.Tests.Shared/Helpers/SymbolTableHelperMocks/DummyTemplate.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.PowerFx.Core.App.Components;
+using Microsoft.PowerFx.Core.App.Controls;
+using Microsoft.PowerFx.Core.Utils;
+
+internal class DummyTemplate : IExternalControlTemplate
+{
+    public readonly Dictionary<string, IExternalControlProperty> InputProps = new ();
+
+    public readonly Dictionary<string, IExternalControlProperty> OutputProps = new ();
+
+    public ComponentType ComponentType { get; set; } = ComponentType.FunctionComponent;
+
+    public bool IncludesThisItemInSpecificProperty { get; set; } = false;
+
+    public bool ReplicatesNestedControls { get; set; } = false;
+
+    public IEnumerable<DName> NestedAwareTableOutputs { get; set; } = Enumerable.Empty<DName>();
+
+    public bool IsComponent { get; set; } = false;
+
+    public bool HasExpandoProperties { get; set; } = false;
+
+    public IEnumerable<IExternalControlProperty> ExpandoProperties { get; set; } = Enumerable.Empty<IExternalControlProperty>();
+
+    public bool HasPropsInferringTypeFromPrimaryInProperty { get; set; } = false;
+
+    public IEnumerable<IExternalControlProperty> PropsInferringTypeFromPrimaryInProperty => Enumerable.Empty<IExternalControlProperty>();  
+
+    public string ThisItemInputInvariantName { get; set; } = string.Empty;
+
+    public IExternalControlProperty PrimaryOutputProperty { get; set; } = new DummyProperty();
+
+    public bool IsMetaLoc { get; set; } = false;
+
+    public bool IsCommandComponent { get; set; } = false;
+
+    public bool HasOutput(DName rightName)
+    {
+        return OutputProps.ContainsKey(rightName.Value);
+    }
+
+    public bool HasProperty(string currentPropertyValue, PropertyRuleCategory category)
+    {
+        return TryGetProperty(currentPropertyValue, out _);
+    }
+
+    public bool TryGetInputProperty(string resolverCurrentProperty, out IExternalControlProperty currentProperty)
+    {
+        return InputProps.TryGetValue(resolverCurrentProperty, out currentProperty);
+    }
+
+    public bool TryGetOutputProperty(string name, out IExternalControlProperty externalControlProperty)
+    {
+        return OutputProps.TryGetValue(name, out externalControlProperty);
+    }
+
+    public bool TryGetProperty(string name, out IExternalControlProperty controlProperty)
+    {
+        return TryGetInputProperty(name, out controlProperty) || TryGetOutputProperty(name, out controlProperty);
+    }
+}
