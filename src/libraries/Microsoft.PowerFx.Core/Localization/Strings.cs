6da4a775ee174f8c1fc6b44b1891c6a316491e42
anderson-joyle <joyle@outlook.com>
2024-10-16 17:27:03 -0500

    Base classes

--- src/libraries/Microsoft.PowerFx.Core/Localization/Strings.cs
+++ src/libraries/Microsoft.PowerFx.Core/Localization/Strings.cs
@@ -608,6 +608,8 @@ namespace Microsoft.PowerFx.Core.Localization
         public static StringGetter AboutClear = (b) => StringResources.Get("AboutClear", b);
         public static StringGetter ClearArg1 = (b) => StringResources.Get("SummarizeArg1", b);
 
+        public static StringGetter AboutJoin = (b) => StringResources.Get("AboutJoin", b);
+
         // Previously, errors were listed here in the form of a StringGetter, which would be evaluated to fetch
         // an error message to pass to the BaseError class constructor. We are switching to passing the message key itself
         // to the BaseError class, and the BaseError itself is responsible for fetching the resource. (This allows the
---
+++ src/libraries/Microsoft.PowerFx.Core/Texl/Builtins/Join.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.PowerFx.Core.Functions;
+using Microsoft.PowerFx.Core.Localization;
+using Microsoft.PowerFx.Core.Types;
+
+namespace Microsoft.PowerFx.Core.Texl.Builtins
+{
+    internal class JoinFunction : FilterFunctionBase
+    {
+        public JoinFunction()
+            : base("Join", TexlStrings.AboutJoin, FunctionCategories.Table, DType.EmptyTable, -2, 2, int.MaxValue, DType.EmptyTable, DType.EmptyTable)
+        {
+            ScopeInfo = new FunctionScopeInfo(this, acceptsLiteralPredicates: false);
+        }
+
+        public override IEnumerable<TexlStrings.StringGetter[]> GetSignatures()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
