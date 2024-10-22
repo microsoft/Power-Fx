8f6acc591bb83d129f5e74d0d0de82ffb9c2dbb0
Luc Genetier <lucgen@microsoft.com>
2024-10-22 14:05:43 +0200

    Fix OptionSets in symbol tables

--- src/libraries/Microsoft.PowerFx.Core/Public/Config/ReadOnlySymbolTable.cs
+++ src/libraries/Microsoft.PowerFx.Core/Public/Config/ReadOnlySymbolTable.cs
@@ -347,8 +347,8 @@ namespace Microsoft.PowerFx
 
         public IEnumerable<string> FunctionNames => this.Functions.FunctionNames;
 
-        public IEnumerable<KeyValuePair<string, OptionSet>> OptionSets => _variables.Where(kvp => kvp.Value.Kind == BindKind.OptionSet)
-                                                                                    .Select(kvp => new KeyValuePair<string, OptionSet>(kvp.Key, (OptionSet)kvp.Value.Data));
+        public virtual IEnumerable<KeyValuePair<string, OptionSet>> OptionSets => _variables.Where(kvp => kvp.Value.Kind == BindKind.OptionSet)
+                                                                                            .Select(kvp => new KeyValuePair<string, OptionSet>(kvp.Key, (OptionSet)kvp.Value.Data));
 
         // Which enums are available. 
         // These do not compose - only bottom one wins. 
