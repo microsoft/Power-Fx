5c173b35db0fc4fada4061590b84625877547615
Adithya Selvaprithiviraj <adithyaselv@gmail.com>
2024-11-13 09:21:33 -0800

    Make TypeLiteralNode visitor abstract in TexlVisitor (#2737)
    Making the `TypeLiteralNode` visitor abstract in the `TexlVisitor` to
    ensure we consider handling TypeLiteralNode in future TexlVisitor
    implementations. keeping it virtual sometimes made this go unnoticed.

--- src/libraries/Microsoft.PowerFx.Core/Texl/ViewFinderVisitor.cs
+++ src/libraries/Microsoft.PowerFx.Core/Texl/ViewFinderVisitor.cs
@@ -12,7 +12,7 @@ namespace Microsoft.PowerFx.Core.Texl
     /// <summary>
     /// This visitor is used to walkthrough the tree to check the existence of a view.
     /// </summary>
-    internal sealed class ViewFinderVisitor : TexlVisitor
+    internal sealed class ViewFinderVisitor : IdentityTexlVisitor
     {
         private readonly TexlBinding _txb;
 
@@ -41,74 +41,6 @@ namespace Microsoft.PowerFx.Core.Texl
             {
                 ContainsView = true;
             }
-        }
-
-        public override void PostVisit(CallNode node)
-        {
-        }
-
-        public override void PostVisit(VariadicOpNode node)
-        {
-        }
-
-        public override void PostVisit(StrInterpNode node)
-        {
-        }
-
-        public override void PostVisit(RecordNode node)
-        {
-        }
-
-        public override void PostVisit(ListNode node)
-        {
-        }
-
-        public override void PostVisit(BinaryOpNode node)
-        {
-        }
-
-        public override void PostVisit(UnaryOpNode node)
-        {
-        }
-
-        public override void PostVisit(TableNode node)
-        {
-        }
-
-        public override void PostVisit(AsNode node)
-        {
-        }
-
-        public override void Visit(ParentNode node)
-        {
-        }
-
-        public override void Visit(NumLitNode node)
-        {
-        }
-
-        public override void Visit(DecLitNode node)
-        {
-        }
-
-        public override void Visit(StrLitNode node)
-        {
-        }
-
-        public override void Visit(BoolLitNode node)
-        {
-        }
-
-        public override void Visit(BlankNode node)
-        {
-        }
-
-        public override void Visit(ErrorNode node)
-        {
-        }
-
-        public override void Visit(SelfNode node)
-        {
         }
     }
 }
