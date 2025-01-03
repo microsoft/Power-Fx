8b12fa214f92bd87bd15409cf27b7ebabb88199e
anderson-joyle <joyle@outlook.com>
2025-01-03 15:20:47 -0600

    Namespace removal.

--- src/libraries/Microsoft.PowerFx.Core/Texl/Builtins/Summarize.cs
+++ src/libraries/Microsoft.PowerFx.Core/Texl/Builtins/Summarize.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.PowerFx.Core.App.ErrorContainers;
 using Microsoft.PowerFx.Core.Errors;
 using Microsoft.PowerFx.Core.Functions;
