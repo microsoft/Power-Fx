7fd14827282685ba8c4073d27b32491b0f1975b8
anderson-joyle <joyle@outlook.com>
2025-01-03 15:17:27 -0600

    Summarize display name fix

--- src/libraries/Microsoft.PowerFx.Core/Texl/Builtins/Summarize.cs
+++ src/libraries/Microsoft.PowerFx.Core/Texl/Builtins/Summarize.cs
@@ -111,6 +111,7 @@ namespace Microsoft.PowerFx.Core.Texl.Builtins
                 errors.EnsureError(DocumentErrorSeverity.Severe, args[0], TexlStrings.ErrSummarizeDataSourceContainsThisGroupColumn);
             }
 
+            var newDisplayNameProvider = new Dictionary<DName, DName>();
             var atLeastOneGroupByColumn = false;
 
             for (int i = 1; i < args.Length; i++)
@@ -151,6 +152,8 @@ namespace Microsoft.PowerFx.Core.Texl.Builtins
                                 errors.EnsureError(DocumentErrorSeverity.Severe, arg, TexlStrings.ErrSummarizeThisGroupColumnName);
                                 continue;
                             }
+
+                            newDisplayNameProvider[columnName] = new DName(displayName);
                         }
 
                         atLeastOneGroupByColumn = true;
@@ -200,6 +203,8 @@ namespace Microsoft.PowerFx.Core.Texl.Builtins
                 isValid = false;
                 errors.EnsureError(DocumentErrorSeverity.Severe, args[1], TexlStrings.ErrSummarizeNoGroupBy);
             }
+
+            returnType = DType.AttachOrDisableDisplayNameProvider(returnType, DisplayNameProvider.New(newDisplayNameProvider));
 
             Contracts.Assert(returnType.IsTable);
             return isValid;
