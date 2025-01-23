8798153816c0519364abf0b8c78efda7cd2514c9
Greg Lindhorst <gregli@microsoft.com>
2025-01-22 22:49:59 -0800

    Update REPL multiline logic and improve identifier parse error messages (#2824)
    The REPL attempts to determine if a line continuation is needed to
    create a valid formula. Today, it does this by using the Power Fx parser
    and attempting to interpret the error messages, which turns out doesn't
    work so well. It also isn't very smart about operators and doesn't
    handle named formulas or UDFs.
    
    For example, these would not continue:
    - `IsMatch( "asdf", "(?x)` long regex best started on the next line
    - `NamedFormula =`     long named formula best started on the next line
    - `Func( x: Number ): Text =`   long udf, best started on the next line
    - `1 + ` long operand, best started on the next line
    
    The new logic is written by hand specifically to answer this
    continuation question and does a much better job. It is always a best
    guess and only determines when to send the formula to the parser/eval
    loop - annoyance either being too conservative or liberal is all that
    this controls. Even if a continuation is detected where there should not
    be one, the user can always enter a blank line to conclude the
    continuation. Not detecting a continuation will be annoying and should
    result an issue being raised, but the user can always remove the extra
    newline.
    
    Yes, we'll need to add logic in two places if the parser changes
    significantly, although that is not anticipated anytime soon. If this
    becomes a burden long term, we can look at enhancing the mainline parser
    to detect continuation situations, but at this point that feels like the
    REPL tail wagging the Power Fx dog.
    
    In the process of learning how identifiers were being parsed, I found
    the error messages were very poor for an identifier parser error. I
    didn't change the logic, just updated the messages and added some more
    tests.

--- src/tests/Microsoft.PowerFx.Repl.Tests.Shared/MultilineProcessorTests.cs
+++ src/tests/Microsoft.PowerFx.Repl.Tests.Shared/MultilineProcessorTests.cs
@@ -17,13 +17,145 @@ namespace Microsoft.PowerFx.Repl.Tests
     {
         private readonly MultilineProcessor _processor = new MultilineProcessor();
 
-        // After case is multiple lines. Last line finally completes.
         [Theory]
-        [InlineData("Sum(1,", "2)")]
-        [InlineData("Sum(1,", "2", ",3)")]
-        [InlineData("{x:3", ",", "y:4}")]
-        [InlineData("Mid(\"a\", 2,)")] // parse error, still completes. 
-        public void ExpectContinue(params string[] lines)
+
+        // basics
+        [InlineData(false, "Sum(1,", "2)")]
+        [InlineData(false, "Sum(1,", "")] // empty line stops continuation
+        [InlineData(false, "Sum(1,", "  ")] // empty line stops continuation
+        [InlineData(false, "Sum(1,", " \t ")] // empty line stops continuation
+        [InlineData(false, "Sum(1,", "2", ",3)")]
+        [InlineData(false, "{x:3", ",", "y:4}")]
+        [InlineData(false, "Mid(\"a\", 2,)")] // parse error, still completes. 
+        [InlineData(false, "[ 1, 2, 3", "]")]
+        [InlineData(false, "{ a: [ 1, 2, 3", "]", "}")]
+        [InlineData(false, "First( { a: [ 1, 2, 3", "]", "}", ")")]
+        [InlineData(false, "First( { a: [ 1, 2, 3", ")")] // error that returns complete for wrong delimiter
+        [InlineData(false, "First( { a: [ 1, 2, 3", "}")] // error that returns complete for wrong delimiter
+        [InlineData(false, "\"string", "\"")]
+        [InlineData(false, "/* text", "more", "end */")]
+        [InlineData(false, "/* text", "more", " \t ")] // empty line stops continuation
+        [InlineData(false, "/* text", "more", "  ")] // empty line stops continuation
+        [InlineData(false, "/* text", "more", "")] // empty line stops continuation
+        [InlineData(false, "( // ) text", ")")]
+        [InlineData(false, "( /* ) text", "*/ )")]
+        [InlineData(false, "[ // ] text", "]")]
+        [InlineData(false, "[ /* ] text", "*/ ]")]
+        [InlineData(false, "{ // } text", "}")]
+        [InlineData(false, "{ /* } text", "*/ }")]
+        [InlineData(false, "IsMatch( \"asdf\", \"\\w+", "\")")]
+
+        // errors
+        [InlineData(false, "[ ( // ) text", "}")] // incorrect close
+        [InlineData(false, "[ ( /* ) text", "*/ ]")] // incorrect close
+        [InlineData(false, "{ [ // ] text", "}")] // incorrect close
+        [InlineData(false, "{ [ /* ] text", "*/ )")] // incorrect close
+        [InlineData(false, "( { // } text", ")")] // incorrect close
+        [InlineData(false, "( { /* } text", "*/ ]")] // incorrect close
+        [InlineData(false, "{ ( 1 + 2 ) )")] // error, incorrect close
+        [InlineData(false, "( 1 + 2 ) )")] // error, too many closes
+
+        // udfs
+        [InlineData(false, "func( a: Number ) : ", "Text = ", "Text( a + 4)")]
+        [InlineData(false, "func( a: Number ) : Text = ", "Text( a + 4)")]
+        [InlineData(false, "func( a: Number ) : Text = {", "Text( a + 4) }")]
+
+        // ends with oeprator
+        [InlineData(false, "a =", "3")]
+        [InlineData(false, "a = // end of line comment", "3")]
+        [InlineData(false, "a = /* inline comment */", "3")]
+        [InlineData(false, "a >", "3")]
+        [InlineData(false, "a > // end of line comment", "3")]
+        [InlineData(false, "a > /* inline comment */", "3")]
+        [InlineData(false, "a <", "3")]
+        [InlineData(false, "a < // end of line comment", "3")]
+        [InlineData(false, "a < /* inline comment */", "3")]
+        [InlineData(false, "{ a:", "3 }")]
+        [InlineData(false, "{ a : // end of line comment", "3 }")]
+        [InlineData(false, "{ a : /* inline comment */", "3 }")]
+        [InlineData(false, "a +", "3 ")]
+        [InlineData(false, "a + // end of line comment", "3 ")]
+        [InlineData(false, "a + /* inline comment */", "3 ")]
+        [InlineData(false, "a -", "3 ")]
+        [InlineData(false, "a - // end of line comment", "3 ")]
+        [InlineData(false, "a - /* inline comment */", "3 ")]
+        [InlineData(false, "a *", "3 ")]
+        [InlineData(false, "a * // end of line comment", "3 ")]
+        [InlineData(false, "a * /* inline comment */", "3 ")]
+        [InlineData(false, "a /", "3 ")]
+        [InlineData(false, "a / // end of line comment", "3 ")]
+        [InlineData(false, "a / /* inline comment */", "3 ")]
+        [InlineData(false, "a ^", "3 ")]
+        [InlineData(false, "a ^ // end of line comment", "3 ")]
+        [InlineData(false, "a ^ /* inline comment */", "3 ")]
+        [InlineData(false, "\"happy \" & ", " \"days\"")]
+        [InlineData(false, "\"happy \" & // end of line comment", "\"days \" ")]
+        [InlineData(false, "\"happy \" & /* inline comment */", "\"days \" ")]
+        [InlineData(false, "true && ", "false ")]
+        [InlineData(false, "true && // end of line comment", "false ")]
+        [InlineData(false, "true && /* inline comment */", "false ")]
+        [InlineData(false, "true || ", "false ")]
+        [InlineData(false, "true || // end of line comment", "false ")]
+        [InlineData(false, "true || /* inline comment */", "false ")]
+        [InlineData(false, "!", "false ")]
+        [InlineData(false, "! // end of line comment", "false ")]
+        [InlineData(false, "! /* inline comment */", "false ")]
+        [InlineData(false, "a!b")]
+        [InlineData(false, "a && ! ", "b")]
+
+        // string interpolation
+        [InlineData(false, "$\" { \"hi\"", " } \"")]
+        [InlineData(false, "$\" { $\" \"\" { 1 ", " + ", "2 } ", "\"", "}", "\"")]
+        [InlineData(false, "$\" { $\" { 1 /* } ", " + } ", "2 } ", "\"", " */ }", "\"", "}", "more \"")]
+        [InlineData(false, "$\" { $\" { 1 // } ", " + ", "2 } ", "\"", " & ", "\"a\" }", "\"")] // ending inline comment ignored
+        [InlineData(false, "$\" { $\" { 1 /* } ", " + } ", "2 } ", "\"", " */ }", "\"", "")] // empty line stops continuation
+        [InlineData(false, "$\" { $\" { 1 // } ", " + ", "2 } ", "\"", " & ", "\"a\" }", "")] // empty line stops continuation
+        [InlineData(false, "[ $\" { 4 + }\"")] // error, unclosed expresion in island
+        [InlineData(false, "[ $\" { 4 * /* inline */ }\"")] // error, unclosed expresion in island
+        [InlineData(false, "[ $\" { 4 * //", "}\"")] // error, unclosed expresion in island
+
+        // comments
+        [InlineData(false, "4 /* a *//", "3")]
+        [InlineData(false, "4 / //", "3")]
+        [InlineData(false, "First( [1,2,3]", "/* ) */", "// )", ")")]
+        [InlineData(false, "( 1 /* error ) ", "*/ + 2 // error )", "/*) error", "4 error )*/", "5", "/* one */)")]
+
+        // identifiers
+        [InlineData(false, "'asdfasdf\"asdfasdf'")]
+        [InlineData(false, "'asdfasdf''asdfasdf'")]
+        [InlineData(false, "'asdfasdf\"asdfasdf' =", "4")]
+        [InlineData(false, "'asdfasdf''asdfasdf' =", "4")]
+        [InlineData(false, "'asdfasdf''asdfasdf' =", " ")] // empty line terminates
+        [InlineData(false, "Set( '\t")] // tab is an illegal character in an identifier name
+        [InlineData(false, "Set( '\n")] // newline is an illegal character in an identifier name
+        [InlineData(false, "Set( '\r")] // carriage return is an illegal character in an identifier name
+        [InlineData(false, "Set( '',", "3)")] // empty identifier is illegal but not worth catching at this level
+        [InlineData(false, "Set( 'happy /* ' */ )")] // tick closes identifier despite looking like it is in a comment
+        [InlineData(false, "'a''a'+", "'b''b'")]
+
+        // text first mode
+        [InlineData(true, "123(")]
+        [InlineData(true, "123{")]
+        [InlineData(true, "123[")]
+        [InlineData(true, "123'asdf")]
+        [InlineData(true, "${}")] // minimal
+        [InlineData(true, "${ 4 ", "}")] // at beginning
+        [InlineData(true, "${ 4 ", "} more")] // at beginning
+        [InlineData(true, "more ${", "}")] // at end
+        [InlineData(true, "more ${}")] // at end
+        [InlineData(true, "123${", "4", "}")]
+        [InlineData(true, "123$${")] // escaped $
+        [InlineData(true, "$${")] // escaped $
+        [InlineData(true, "123${", "4", "} {")]
+        [InlineData(true, "123${", "4", "} ${", "5", "}")]
+        [InlineData(true, "123${", "4", "} {{ ${", "5", "}")]
+        [InlineData(true, "123${ // }", "}")]
+        [InlineData(true, "123 ${ { a: [ 1, 2, 3 }")] // error, incorrect closing bracket
+        [InlineData(true, "123 ${ { a: [ 1, 2, 3 )")] // error, incorrect closing bracket
+        [InlineData(true, "123 ${ [ 4 + ] }")] // error, dangling operator
+        [InlineData(true, "123 ${ 1 + 2 } }")] // not an error
+
+        public void ExpectContinue(bool textFirst, params string[] lines)
         {
             StringBuilder sb = new StringBuilder();
 
@@ -35,7 +167,7 @@ namespace Microsoft.PowerFx.Repl.Tests
                 bool isFirstLine = i == 0;
                 Assert.Equal(isFirstLine, _processor.IsFirstLine);
 
-                var result = _processor.HandleLine(line, new ParserOptions());
+                var result = _processor.HandleLine(line, new ParserOptions() { TextFirst = textFirst });
                 sb.AppendLine(line);
 
                 if (!isLast)
