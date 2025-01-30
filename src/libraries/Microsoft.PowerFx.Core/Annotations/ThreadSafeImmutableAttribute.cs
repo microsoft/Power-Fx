2fe1bae8446982b07f972e725957855fe306fc19
Mike <jmstall@microsoft.com>
2025-01-29 18:15:04 -0800

    Reenable [ThreadSafeImmutable] tests. Add IRNode to list.
    #1519.

--- src/libraries/Microsoft.PowerFx.Core/Annotations/ThreadSafeImmutableAttribute.cs
+++ src/libraries/Microsoft.PowerFx.Core/Annotations/ThreadSafeImmutableAttribute.cs
@@ -7,6 +7,10 @@ namespace Microsoft.PowerFx
 {
     /// <summary>
     /// This type is thread safe because it's immutable. 
+    /// This means that:
+    /// - all of its properties/fields must also be immutable. 
+    /// - all derived classes are assumed to also be immutable, unless otherwise marked 
+    /// as <see cref="NotThreadSafeAttribute"/>.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface)]
     internal class ThreadSafeImmutableAttribute : Attribute
