1fd23c9b63acaa71655e2a8d08516533f451deb3
Jim Daly <jim.daly@microsoft.com>
2025-06-21 13:04:08 -0700

    ErrorResourceKey

--- src/libraries/Microsoft.PowerFx.Core/Localization/ErrorResourceKey.cs
+++ src/libraries/Microsoft.PowerFx.Core/Localization/ErrorResourceKey.cs
@@ -13,10 +13,17 @@ namespace Microsoft.PowerFx.Core.Localization
     [ThreadSafeImmutable]
     public struct ErrorResourceKey
     {
+        /// <summary>
+        /// Gets the key for the error resource.
+        /// </summary>
         public string Key { get; }
 
         internal IExternalStringResources ResourceManager { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ErrorResourceKey"/> struct with the specified key.
+        /// </summary>
+        /// <param name="key">The key for the error resource.</param>
         public ErrorResourceKey(string key)
         {
             Contracts.AssertNonEmpty(key);
