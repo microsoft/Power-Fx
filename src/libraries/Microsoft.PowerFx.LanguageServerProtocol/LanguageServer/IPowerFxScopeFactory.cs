2e6849d28eb6e472bd593180413f65c104555dc9
Jim Daly <jim.daly@microsoft.com>
2025-06-21 11:23:37 -0700

    IPowerFxScopeFactory

--- src/libraries/Microsoft.PowerFx.LanguageServerProtocol/LanguageServer/IPowerFxScopeFactory.cs
+++ src/libraries/Microsoft.PowerFx.LanguageServerProtocol/LanguageServer/IPowerFxScopeFactory.cs
@@ -6,8 +6,16 @@ using Microsoft.PowerFx.Intellisense;
 
 namespace Microsoft.PowerFx.Core
 {
+    /// <summary>
+    /// Factory interface for creating or retrieving <see cref="IPowerFxScope"/> instances for a given document URI.
+    /// </summary>
     public interface IPowerFxScopeFactory
     {
+        /// <summary>
+        /// Gets or creates an <see cref="IPowerFxScope"/> instance associated with the specified document URI.
+        /// </summary>
+        /// <param name="documentUri">The URI of the document for which to get or create the scope instance.</param>
+        /// <returns>An <see cref="IPowerFxScope"/> instance for the specified document URI.</returns>
         IPowerFxScope GetOrCreateInstance(string documentUri);
     }
 }
