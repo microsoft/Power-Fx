aeb76e126057b91fbcd74f712c5b7c013472afdd
Jim Daly <jim.daly@microsoft.com>
2025-06-21 14:17:56 -0700

    IIntellisenseSuggestion

--- src/libraries/Microsoft.PowerFx.Core/Texl/Intellisense/IIntellisenseSuggestion.cs
+++ src/libraries/Microsoft.PowerFx.Core/Texl/Intellisense/IIntellisenseSuggestion.cs
@@ -5,35 +5,42 @@ using System.Collections.Generic;
 
 namespace Microsoft.PowerFx.Intellisense
 {
+    /// <summary>
+    /// Represents a suggestion provided by the IntelliSense system, including its type, display text,  and additional
+    /// metadata for UI and functionality purposes.
+    /// </summary>
+    /// <remarks>This interface is used to encapsulate information about a single IntelliSense suggestion, 
+    /// such as its kind, display text, and whether it matches the expected type. It also provides  additional details
+    /// like overload suggestions and UI-specific metadata.</remarks>
     public interface IIntellisenseSuggestion
     {
         /// <summary>
-        /// The Kind of Suggestion.
+        /// Gets the Kind of Suggestion.
         /// </summary>
         SuggestionKind Kind { get; }
 
         /// <summary>
-        /// What kind of icon to display next to the suggestion.
+        /// Gets the kind of icon to display next to the suggestion.
         /// </summary>
         SuggestionIconKind IconKind { get; }
 
         /// <summary>
-        /// This is the string that will be displayed to the user.
+        /// Gets the string that will be displayed to the user.
         /// </summary>
         UIString DisplayText { get; }
 
         /// <summary>
-        /// Indicates if there are errors.
+        /// Gets whether there are errors.
         /// </summary>
         bool HasErrors { get; }
 
         /// <summary>
-        /// Description, suitable for UI consumption.
+        /// Gets the description, suitable for UI consumption.
         /// </summary>
         string FunctionParameterDescription { get; }
 
         /// <summary>
-        /// Description, suitable for UI consumption.
+        /// Gets the description, suitable for UI consumption.
         /// </summary>
         string Definition { get; }
 
@@ -49,7 +56,7 @@ namespace Microsoft.PowerFx.Intellisense
         IEnumerable<IIntellisenseSuggestion> Overloads { get; }
 
         /// <summary>
-        /// A boolean value indicating if the suggestion should be preselected by the formula bar
+        /// Gets a value indicating if the suggestion should be preselected by the formula bar
         /// In canvas, used for Primary Output properties.
         /// </summary>
         bool ShouldPreselect { get; }
