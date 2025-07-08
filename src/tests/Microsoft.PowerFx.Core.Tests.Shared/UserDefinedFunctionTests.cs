c0e7b49b5e55c2e574aa6138f49be672d67ecfc3
Adithya Selvaprithiviraj <adithyase@microsoft.com>
2025-07-08 13:47:40 -0700

    review comments

--- src/tests/Microsoft.PowerFx.Core.Tests.Shared/UserDefinedFunctionTests.cs
+++ src/tests/Microsoft.PowerFx.Core.Tests.Shared/UserDefinedFunctionTests.cs
@@ -691,6 +691,7 @@ namespace Microsoft.PowerFx.Core.Tests
         [Theory]
         [InlineData("F():Void = 5;")]
         [InlineData("F():Void = Set(x,5);")]
+        [InlineData("F(x: Text):Void = x;")]
         public void TestNonBehaviorUDFReturnsVoid(string expression)
         {
             var parserOptions = new ParserOptions();
@@ -699,7 +700,6 @@ namespace Microsoft.PowerFx.Core.Tests
                                             .SetBindingInfo(_primitiveTypes);
             var errors = checkResult.ApplyErrors();
             Assert.Contains(errors, e => e.MessageKey.Contains("ErrUDF_NonImperativeVoidType"));
-
         }
     }
 }
